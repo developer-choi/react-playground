70d1420ce73f9fe1057dae34cbdfb0d23b9c86ee
choiyujin <dbwls6840@naver.com>
2020-05-11 16:35:36 +0900

    table-layout 속성 사용해봤는대, 테이블 및 열 너비는 셀의 너비 table와 col요소 또는 첫 번째 행의 너비로 설정됩니다 << 이 내용이 잘 이해가 안됨. 그대신, 기본값이 auto라서 table의 width를 고정해놔도 안에 자식이 넘치면 그 width보다 더 늘어나는데 fixed로 하면 늘어나지않게되서 width계산할 때 예측하기 편할거같다는 의견에는 동의함.
    또한, fixed의 효과를 보려면 table의 width를 설정해야한다는것도 같이배움

--- src/route/css/propertiesRoutes.ts
+++ src/route/css/propertiesRoutes.ts
@@ -1,5 +1,6 @@
 import VerticalAlignExample from "../../containers/css/properties/VerticalAlignExample";
 import {CSS_FIRST_PATH_DIRECTORY_NAME} from "../firstPath";
+import TableLayoutExample from "../../containers/css/properties/TableLayoutExample";
 
 const path = `${CSS_FIRST_PATH_DIRECTORY_NAME}/properties`;
 
@@ -7,5 +8,9 @@ export const propertiesRoutes = [
     {
         path: `${path}/vertilcal-align`,
         component: VerticalAlignExample
+    },
+    {
+        path: `${path}/table-layout`,
+        component: TableLayoutExample
     },
 ];
