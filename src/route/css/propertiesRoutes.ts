4ea5620b3d7322a65de70a8c983a89204e2999ce
choiyujin <dbwls6840@naver.com>
2020-05-11 19:53:19 +0900

    word-break, word-wrap공부했음. word-break의 break-word, keep-all말고는 쓸일없다고 판단됨.

--- src/route/css/propertiesRoutes.ts
+++ src/route/css/propertiesRoutes.ts
@@ -1,6 +1,7 @@
 import VerticalAlignExample from "../../containers/css/properties/VerticalAlignExample";
 import {CSS_FIRST_PATH_DIRECTORY_NAME} from "../firstPath";
 import TableLayoutExample from "../../containers/css/properties/TableLayoutExample";
+import WordWrapBreakExample from "../../containers/css/properties/WordWrapBreakExample";
 
 const path = `${CSS_FIRST_PATH_DIRECTORY_NAME}/properties`;
 
@@ -12,5 +13,9 @@ export const propertiesRoutes = [
     {
         path: `${path}/table-layout`,
         component: TableLayoutExample
+    },
+    {
+        path: `${path}/word-wrap-break`,
+        component: WordWrapBreakExample
     },
 ];
