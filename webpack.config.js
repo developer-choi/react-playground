98843073c241c1fcf73ac621faadefba9154bc47
choiyujin <dbwls6840@naver.com>
2020-06-05 10:35:51 +0900

    필요없는 웹팩설정 삭제

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const port = 8365;
 
 module.exports = {
-  mode: "development",
-  output: {
-    publicPath: "/",
-    filename: "[name].[hash].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx)$/, //
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.(css|scss|sass)$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".ts",".tsx"]
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-      favicon: "static/images/icons/favicon.ico"
-    })
-  ],
-
-  devServer: {
-    port: port,
-    historyApiFallback: true,
-  },
+    mode: "development",
+    output: {
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts|tsx)$/, //
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            favicon: "static/images/icons/favicon.ico"
+        })
+    ],
+    devServer: {
+        port: port,
+        historyApiFallback: true,
+    }
 };
