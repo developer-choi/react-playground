252fc80aea0634f678770ee1b07b11b06af6d927
choiyujin <dbwls6840@naver.com>
2020-07-17 21:51:40 +0900

    array util update

--- src/utils/web-api-extend/arrayUtil.ts
+++ src/utils/web-api-extend/arrayUtil.ts
@@ -2,6 +2,6 @@
 	* @param array 중복된 요소가 포함되어있는 배열
 	* @return 중복된 요소가 제거된 배열
 	*/
-export function removeDuplicate(array: Array<any>) {
+export function removeDuplicate<T>(array: Array<T>) {
 		return Array.from(new Set(array));
 }
