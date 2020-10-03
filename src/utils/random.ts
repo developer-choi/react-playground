f80ca9e2d06b553d6580f7ef56cc5562ad395aa5
choiyujin <dbwls6840@naver.com>
2020-10-03 20:45:51 +0900

    라디오버튼 설계해봄. 기존에는 라디오버튼, 체크박스 2개의 기능적 차이도 몰랐었기에 마크업부터 잘못했음.
    그래서 props 설계부터 다시 제작했음.

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -3,10 +3,10 @@
  * @example (3, -2) --> -2이상 3이하의 랜덤한 숫자 반환
  */
 export function randomNumber(from: number, to: number) {
-    const _max = Math.max(from, to);
-    const _min = Math.min(from, to);
-    const length = Math.abs(_max - _min) + 1;
-    return Math.floor(Math.random() * length) + _min;
+  const _max = Math.max(from, to);
+  const _min = Math.min(from, to);
+  const length = Math.abs(_max - _min) + 1;
+  return Math.floor(Math.random() * length) + _min;
 }
 
 /**
@@ -17,13 +17,17 @@ export function randomNumber(from: number, to: number) {
  */
 export function makeRandomString(anagramArray: Array<string | number>, length: number): string {
 
-    let result = new Array<string>(length);
+  let result = new Array<string>(length);
 
-    for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
 
-        let randomIndex = randomNumber(0, anagramArray.length - 1);
-        result.push(String(anagramArray[randomIndex]));
-    }
+    let randomIndex = randomNumber(0, anagramArray.length - 1);
+    result.push(String(anagramArray[randomIndex]));
+  }
 
-    return result.join("");
+  return result.join('');
+}
+
+export function getRandomComponentId() {
+  return 'random-id-' + Math.floor(Math.random() * 10000000);
 }
