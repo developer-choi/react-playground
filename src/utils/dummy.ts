9aa8a0e05ce14da667e6ff010a01684531f1c26a
choeyujin <dbwls6840@naver.com>
2020-10-06 20:49:06 +0900

    문자열을 보여줄 때, 문자열 사이에 공백이 있고 없고에 따라 글자가 되게 이상하게 나오는 일이 있어서... 에러케이스를 확보했음.
    문제는 white-space를 잘 몰라서 생기는 일 같다. MDN에서 "white-space 속성은 요소가 공백 문자를 처리하는 법을 지정합니다" 라고 하니까 확실함. 근데 나머지는 잘 모르겠다. white-space 어렵다.
    
    목표는
    1. 어떠한 상황에서도 글자가 길어지면 (한줄, 여러줄) ...처리를 내 의도대로 할 수 있어야함.
    2. white-space: pre*****에 대해 이해하는것. 이거 태그사이에 엔터쳤더니 엔터친대로 나오던대

--- src/utils/dummy.ts
+++ src/utils/dummy.ts
@@ -30,3 +30,4 @@ export const BACK_IMAGE4 = 'https://www.navercorp.com/img/ko/main/img_main_slide
 export const BACK_IMAGE5 = 'https://www.navercorp.com/img/ko/main/img_main_slide5.jpg';
 
 export const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+export const LOREM_IPSUM_WITHOUT_BLANK = LOREM_IPSUM.replace(/ /g, '');
