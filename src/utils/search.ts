c4d620a031bd1d57e22b5cc18a4cee9ad1feac62
choeyujin <dbwls6840@naver.com>
2020-10-18 22:38:03 +0900

    search 조건 추가

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -13,8 +13,8 @@ export type AllValidKeys = SearchValidKeys | SortValidKeys;
  * (모든 페이지에서 searchText라는 key로 parse하고 stringify하고있었는데 백엔드개발자가 바꿔달라고 할 수 있기 때문)
  *
  *
- * 방법1. wrappingParse() : 첫 페이지 로딩시 유효한 key, value만 파싱된 결과로 처리를 시작한다.
- * 방법2. wrappingStringify() : 이후 서버로 요청보낼 시, 유효한 value만 쿼리스트링에 담아서 보낸다.
+ * 구현1. wrappingParse() : 첫 페이지 로딩시 유효한 key, value만 파싱된 결과로 처리를 시작한다.
+ * 구현2. wrappingStringify() : 이후 서버로 요청보낼 시, 유효한 value만 쿼리스트링에 담아서 보낸다.
  *
  * >> 모든 페이지에서 사용할 parse, stringify를 감싼 함수를 만들고, 여기서 타입체크 와 원하는 처리를 구현한다.
  * 1. 검색 타입이 페이지마다 모두 다르면, wrappingParse1 wrappingParse2 wrappingParse3을 여러개 만드는 방식으로 해결하되,
@@ -36,6 +36,7 @@ export type AllValidKeys = SearchValidKeys | SortValidKeys;
  * (2) 브라우저 주소창에 ?unknownKey 이렇게 지원하지않는 key이거나,
  * (3) 브라우저 주소창에 ?page[0]=1 또는 ?page=abc 이렇게 지원하는 key인데 원하지 않는 값이 들어있는 경우.
  * (4) ?direction=ascendant 혹은 ?direction=descendant 이렇게 querystring에서 유효한 값이 몇가지로 제한되는 경우에도, parse() 및 stringify()에서 원활히 타입체크가 되야함.
+ * (5) 같이 쓰이는 쿼리스트링 패러미터중 하나가 유효하지 않으면, 다른 나머지도 삭제해야함. 예를들어, searchText가 빈문자열이면, searchText도 삭제하되 searchType도 같이 삭제해야함.
  *
  * 2. 타입체크
  * 쿼리스트링에서 값을 받아와야 하는데 배열을 받아와야 하는 경우.
