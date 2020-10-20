18e6bfae0a79f9582a3747cf6c8513c36a6513d3
choeyujin <dbwls6840@naver.com>
2020-10-20 21:41:40 +0900

    Date API 나중에 뭐 적을지 메모

---
+++ src/pages/api/js/DateApiPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useSetDocumentTitle} from '../../../utils/custom-hooks/useSetDocumentTitle';
+import moment from 'moment';
+
+export default function DateApiPage() {
+
+  useSetDocumentTitle('Date');
+
+  return (
+      <Wrap>
+        date.ts로 응용해서 만들 수 있는 컴포넌트들과,
+        moment library usage를 여기 페이지에 담자.
+
+        date.ts는 주로 sundy에서 {'< >'} 누르면 막 날짜가 day로 바뀌고
+
+        달력에서는 이전월 이후월 버튼누르면 막 그거랑
+        달력 그 자체 (8월, 7월 마지막주 ~ 8월첫주 부분) 보여주는거 컴포넌트랑.
+        이런거 다 여기페이지에 쭉 보여주면 되겠다.
+      </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+`;
+
+const dateToNumber = new Date().getTime();
+
+/**
+ * usage는 딱 format하나임.
+ * 나머지는 Date API만 사용하면되고, 그게 utils/date.ts에 있음.
+ */
+const FORMAT = 'YYYY년 MM월 DD일';
+console.log(moment(dateToNumber).format(FORMAT));
+console.log(moment().valueOf(), dateToNumber);
