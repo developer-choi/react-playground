5399d35b23667b918fbabbfc37a77107f38f2022
choeyujin <dbwls6840@naver.com>
2020-11-03 22:33:23 +0900

    있으면 편할것같은 css함수 완성

--- src/utils/style/css.ts
+++ src/utils/style/css.ts
@@ -4,3 +4,26 @@ export const FlexDirectionColumn = css`
   display: flex;
   flex-direction: column;
 `;
+
+export function setLeftRadius(applyClassName: string, value = 0) {
+
+  const suffix = value === 0 ? '' : 'px';
+
+  return css`
+    &.${applyClassName} {
+      border-top-left-radius: ${value}${suffix};
+      border-bottom-left-radius: ${value}${suffix};
+    }
+`;
+}
+
+export function setRightRadius(applyClassName: string, value = 0) {
+  const suffix = value === 0 ? '' : 'px';
+
+  return css`
+    &.${applyClassName} {
+      border-top-right-radius: ${value}${suffix};
+      border-bottom-right-radius: ${value}${suffix};
+    }
+`;
+}
