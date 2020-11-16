0659c64f11712d9d6af809c5e3fa8d5384bc270b
choeyujin <dbwls6840@naver.com>
2020-11-16 20:48:30 +0900

    invalid event 알아냈음.

--- src/components/extend/InputExtend.tsx
+++ src/components/extend/InputExtend.tsx
@@ -1,11 +1,13 @@
-import React, {ChangeEvent, ComponentProps, forwardRef, KeyboardEvent, Ref, useCallback} from 'react';
+import React, {ChangeEvent, ComponentProps, FormEvent, forwardRef, KeyboardEvent, Ref, useCallback} from 'react';
 
 /**
  * 이 컴포넌트를 제작할 때 고려된 input의 type은 아래와 같습니다.
  */
 export type InputExtendType = 'text' | 'password' | 'email' | 'number' | 'tel' | 'date' | 'search' | 'url' | undefined;
 
-export interface InputExtendProp extends Omit<ComponentProps<'input'>, 'ref'> {
+type WithoutProp = 'ref' | 'onInvalid' | 'onInvalidCapture';
+
+export interface InputExtendProp extends Omit<ComponentProps<'input'>, WithoutProp> {
   onTab?: (event: KeyboardEvent<HTMLInputElement>) => void;
   onEnter?: (event: KeyboardEvent<HTMLInputElement>) => void;
   onChangeText?: (value: string) => void;
@@ -16,7 +18,7 @@ export interface InputExtendProp extends Omit<ComponentProps<'input'>, 'ref'> {
    * onCtrlV()가 호출되었을 당시에는 아직 setState가 완료되지 않음을 주의해야합니다.
    */
   onCtrlV?: (text: string) => void;
-  type: InputExtendType;
+  type?: InputExtendType;
 }
 
 export default forwardRef(function InputExtend({onCtrlV, onTab, onEnter, onChangeText, onChange, onKeyDown, ...rest}: InputExtendProp, ref: Ref<HTMLInputElement>) {
@@ -57,7 +59,12 @@ export default forwardRef(function InputExtend({onCtrlV, onTab, onEnter, onChang
     onChange?.(event);
   }, [onChange, onChangeText]);
 
+  const onInvalid = useCallback((event: FormEvent<HTMLInputElement>) => {
+    //invalid event때문에 type email했을 때 'as'만 입력하고 엔터치면 system alert이 발생했던 것. 이것은 form의 submit 이벤트를 prevent한다고 사라지지않음.
+    event.preventDefault();
+  }, []);
+
   return (
-      <input ref={ref} onKeyUp={needNotOnKeyUp ? undefined : _onKeyUp} onChange={needNotOnChange ? undefined : _onChange} onKeyDown={needNotOnKeyDown ? undefined : _onKeyDown} {...rest}/>
+      <input ref={ref} onInvalid={onInvalid} onKeyUp={needNotOnKeyUp ? undefined : _onKeyUp} onChange={needNotOnChange ? undefined : _onChange} onKeyDown={needNotOnKeyDown ? undefined : _onKeyDown} {...rest}/>
   );
 });
