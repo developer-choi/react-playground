0020560512f7cb13d8339bb76145ee575a6429bc
choeyujin <dbwls6840@naver.com>
2020-11-16 22:52:55 +0900

    예외처리 고민한내용 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -68,6 +68,13 @@ export function handleApiError({error, expected, expectedCodes = [], unexpected 
  * FrontError는 무조건 내가 예상하지못한 경우밖에 없기 때문에, 다른 2개 패러미터는 Omit했고,
  * (1) 사용자에게는 반드시 unexpected() 처리결과만 보여주고
  * (2) 나는 에러종류별 로깅에만 집중하는걸로.
+ *
+ * 그런데, TypeError는 현재 hooks에서 componentDidCatch같은 API가 없어가지고....
+ * 데이터받은걸로 렌더링할 때 TypeError발생하면 와 이건 모든 컴포넌트마다 try-catch로 잡아야하나? 아마.. 무슨컴포넌트에서 발생했는지를 꼬박 로그에 남기겠지?
+ * 에러바운더리같은거를 배워야겠어..
+ *
+ * API Call로 시작해서 TypeError가 발생하는건 그냥 response 통째로 찍으면 되서 편하긴하지만
+ * 다른회사에서는 API Call로 시작해서 TypeError가 발생할리가 없겠지. API명세를 얼마나 제대로하겠어,
  */
 export function handleFrontError({error, unexpected = dispatchOpenUnexpectedModal}: Omit<HandleErrorParameter, 'expectedCodes' | 'expected'>) {
   console.error('Not Axios Error', error);
