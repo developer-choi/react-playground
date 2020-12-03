18ad10f0891f71efe13c340594bf8158ab667b6d
choeyujin <dbwls6840@naver.com>
2020-12-03 23:21:08 +0900

    short function signature 작성

--- src/utils/web-api-extend/number.ts
+++ src/utils/web-api-extend/number.ts
@@ -79,3 +79,36 @@ export function numberIntegerDecimalSlice(value: number, maxNumberLength: number
         return Number(`${integerPart.slice(0, maxNumberLength)}.${decimalPart.slice(0, restLength)}`) * sign;
     }
 }
+
+/**
+ * value: 숫자값
+ * 단위는 최대 2개를 반환함.
+ *
+ * (12345) ==> "1만 2345"
+ * (1234) ==> "1234"
+ * (123456789) ==> "1억 2345만"
+ *
+ * shortKorea1Unit()을 굳이 안만든이유는 그거는 밑에 shortKoreaFloat으로 대응이 되기 떄문.
+ */
+declare function shortKorea2Unit(value: number): string
+
+/**
+ *
+ * @param decimalLength 소수점 길이 (default 0으로 할 생각)
+ * (12345, 2) ==> 1.23만
+ */
+declare function shortKoreaFloat(value: number, decimalLength?: number): string
+
+/**
+ * (1234567, 2) ==> {short: 1.23, unit: 'M'}
+ */
+declare function shortSIUnit(value: number, decimalLength?: number): {
+    short: number;
+    unit: '' | 'K' | 'M' | 'G' | 'T'
+}
+
+/**
+ * 대응이 안되는 케이스는,
+ * 65,432 KW 이런식으로 기본단위를 K로 설정한다거나, 길이가 5자리 정도는 자르지않는 경우. (65.432M 안쓰고 저렇게)
+ * 값이 0보다 한참 작을 때 표기할 방법이 음..
+ */
