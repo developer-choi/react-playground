4aa6aed99bc8eca1563af1c5effa0903a7c04ccc
choeyujin <dbwls6840@naver.com>
2020-12-06 20:51:51 +0900

    search.ts 다시 분석해보고 주석추가 및 구현추가. 관심사를 유효한key, 유효한 value 2개로 분리하고, 유효하지 않은 key를 제거하고 반환하지않고 거꾸로 유효한 key만 객체에 담아서 반환하고, 유효한 value를 체크할 방법 찾음.

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -22,23 +22,25 @@ export type AllValidKeys = SearchValidKeys | SortValidKeys;
  * function wrappingParse<T>(params) {}
  *
  * function page1Parse(params) {
- *   wrappingParse<SpecificType>(params)
+ *   return wrappingParse<SpecificType>(params);
  * }
  * >> 이후 page1 페이지에서는 page1Parse(params)로 아무런 Type Parameter를 넘기지않고 사용할 수 있지만 타입체크는 모두 받을 수 있다.
  *
  *
- * 미해결
- * 1. 유효성 검증 : (1), (2) 해결완료.
- * 결국 1번과 연결되는 문제인데,
- * (1) 브라우저 주소창에 ?key= 이렇게 빈문자열이거나,
- * (2) 브라우저 주소창에 ?unknownKey 이렇게 지원하지않는 key이거나,
- * (3) 브라우저 주소창에 ?page[0]=1 또는 ?page=abc 이렇게 지원하는 key인데 원하지 않는 값이 들어있는 경우.
- * (4) ?direction=ascendant 혹은 ?direction=descendant 이렇게 querystring에서 유효한 값이 몇가지로 제한되는 경우에도, parse() 및 stringify()에서 원활히 타입체크가 되야함.
- * (5) 같이 쓰이는 쿼리스트링 패러미터중 하나가 유효하지 않으면, 다른 나머지도 삭제해야함. 예를들어, searchText가 빈문자열이면, searchText도 삭제하되 searchType도 같이 삭제해야함.
+ * 1. 상세 구현목록 (parse)
+ * (1) 유효하지 않은 key가 있으면 삭제하고 parse
+ * 예시) ?unknownKey=3
+ *
+ * (2) 유효하지않은 value가 있으면 삭제하고 parse
+ * 브라우저 주소창에 ?page=1 또는 ?page=abc 이렇게 지원하는 key인데 원하지 않는 값이 들어있는 경우.
+ * ?page= 이렇게 빈문자열인경우 혹은 배열인 경우.
+ * ?direction=ascendant 혹은 ?direction=descendant 이렇게 querystring에서 유효한 값이 몇가지로 제한되는 경우에도
+ *
+ * (3) 같이 쓰이는 쿼리스트링 패러미터중 하나가 유효하지 않으면, 다른 나머지도 삭제해야함.
+ * searchText가 빈문자열이면, searchText도 삭제하되 searchType도 같이 삭제해야함.
  *
  * 2. 타입체크
  * 쿼리스트링에서 값을 받아와야 하는데 배열을 받아와야 하는 경우.
- * 이 경우는 1(3)와 정확히 반대되는 상황이며, 양쪽의 경우의 타입체크를 모두 원활히 만족하는 코드를 작성해야함.
  *
  * 모든 코드에서,
  * const result = parse(location.search)하고
@@ -54,40 +56,50 @@ export type AllValidKeys = SearchValidKeys | SortValidKeys;
  */
 
 /**
- * ?sec 이렇게 =이 없으면 parse에서 결과가 null이므로 null도 순회돌면서 제거해야함.
- */
-
-/**
  * 1. location.search에서 validKeys에 해당하는 key의 value만 객체로 만들어서 반환함.
  * 2. 이 때 value가 빈문자열이면 유효하지않은 querystring인것으로 판단하고 해당 프로퍼티 삭제하고 반환함.
  */
-export function rootSafeParse<K extends AllValidKeys>(search: string, validKeys: K[]): Partial<Record<K, string>> {
-  const parsed = parse(search);
-  const removedKeysParsed = removeInvalidProperty(parsed, validKeys);
-  const removeValuesParsed = removeEmptyValueFromObject(removedKeysParsed);
-  return removeValuesParsed as Partial<Record<K, string>>;
+
+export type SafeParseResult<K extends AllValidKeys> = Partial<Record<K, string>>;
+
+export function rootSafeParse<K extends AllValidKeys>(search: string, validKeys: K[]): SafeParseResult<K> {
+  const parsed = parse(search) as SafeParseResult<K>;
+  return getValidPropertyAndValueObject(parsed, validKeys);
 }
 
 declare function rootSafeStringify<K extends AllValidKeys>(object: Record<K, string>): string
   // return stringify(object);
 
 /**
- * object에 key는 string, value도 string이라는 타입을 지정하고싶은데 어떻게 해야할지 모르겠다..
- * object의 key중에서 allowKeys에 없는 key는 삭제해서 반환하며, 반환한 객체는 object를 얇게 복사한 객체다.
+ * 유효한 key만 따로 추려서 반환한다.
+ * @example ({a: 1, page: 1}, ['page']) => {page: 1}
  */
-function removeInvalidProperty(object: any, allowKeys: string[]) {
+type IsValidValueCallback = (value: string) => boolean;
 
-  const cloneObject = Object.assign({}, object);
-  const objectKeys = Object.keys(cloneObject);
+function defaultValidValue(value: string) {
+  return value !== '';
+}
 
-  objectKeys.forEach(key => {
+function getValidPropertyAndValueObject<K extends AllValidKeys>(object: SafeParseResult<K>, allowKeys: K[], validValueCallback: IsValidValueCallback = defaultValidValue): SafeParseResult<K> {
 
-    if (!allowKeys.includes(key)) {
-      delete cloneObject[key];
+  const objectKeys = Object.keys(object);
+  return objectKeys.reduce((a, b) => {
+
+    //@ts-ignore
+    if (!allowKeys.includes(b)) {
+      return a;
     }
 
-  });
-  return cloneObject;
+    const value = object[b as K] as string;
+
+    if (!validValueCallback(value)) {
+      return a;
+    }
+
+    a[b as K] = value;
+    return a;
+
+  }, {} as SafeParseResult<K>)
 }
 
 /**
@@ -113,6 +125,6 @@ function removeEmptyValueFromObject(object: any) {
  * directionValue에 가능한 값은 ascendant, descendant 2개인데,
  * 현재 타입체크를 받지못하고 아무런 타입에러가 나고있지 않음.
  */
-const directionType = '123';
-const directionValue = '1234';
-const res = rootSafeStringify({directionType, directionValue});
+// const directionType = '123';
+// const directionValue = '1234';
+// const res = rootSafeStringify({directionType, directionValue});
