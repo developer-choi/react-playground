3f564c160ec2e0c53de1bbf62c2dc1eafd63abd5
choeyujin <dbwls6840@naver.com>
2020-12-06 18:50:35 +0900

    validation에 onChange는 필요없다. onEnter하나만있으면 된다. 그래서 ValidateResult에 value property도 필요없다. 그리고 ValidateResult에서 errorMessage property가 message property로 변경된만큼, MessageConfig 역시도 에러메시지, 성공메시지를 둘 다 의미할 수 있도록 property 접미사로 error를 붙이던가 하자. 기존에 EmptyErrorMessageConfig에 whenEmpty보다는 EmptyMessageConfig에 emptyError가 좀 더 낫다.

--- src/utils/validate/common.ts
+++ src/utils/validate/common.ts
@@ -1,104 +1,78 @@
 /**
- * 입력 / 제출시 뭔가 잘못된 경우 에러메시지를 바꾸고싶을 때 사용하는 property 입니다.
- * 혹은, 기본적으로 제공되는 에러메시지를 구분할 때 사용하는 property 입니다.
+ * 잘못됬거나, 잘됬거나 했을 때 메시지를 표기하기 위한 property입니다.
+ * 아래의 경우는, 빈 문자열일 경우에 대해 에러메시지를 뜻하는 property입니다.
  */
-export interface EmptyErrorMessageConfig {
-  whenEmpty?: string;
+export interface EmptyMessageConfig {
+  emptyError: string;
 }
 
-export interface NumberErrorMessageConfig extends EmptyErrorMessageConfig {
-  notNumber?: string;
-}
-
-export const ERROR_MESSAGE: Record<keyof NumberErrorMessageConfig, string> = {
-  whenEmpty: '값을 입력해주세요.',
-  notNumber: '숫자만 입력이 가능합니다.'
+export const EMPTY_ERROR_MESSAGE_CONFIG: Record<keyof EmptyMessageConfig, string> = {
+  emptyError: '값을 입력해주세요.',
 };
 
-/**
- * onSubmit할 때는 일반적으로 사용자가 입력했던 값을 바꾸지 않습니다. 입력값이 잘못되면 에러메시지로 표현합니다.
- * 그래서 value property는 OnChange interface에만 존재해야합니다.
- */
-export interface OnSubmitValidateResult {
+export interface ValidateResult {
 
   /**
    * 유효성 검증을 통과한 여부를 나타냅니다.
    * 활용1. onChange(입력시) 에서 true일 경우, submit버튼을 활성화하거나, 입력박스가 조건을 통과했다는 의미로 초록색 테두리 표시한다거나 할 떄 쓸 수 있습니다.
-   * 활용2. onSubmitEditing(입력박스에서 키보드 엔터), onSubmit(폼 제출) 에서, 잘못됬을 경우 포커스를 주거나, 포커스를 취소하는데 사용할 수 있습니다.
+   * 활용2. onEnter(입력박스에서 키보드 엔터), onSubmit(폼 제출) 에서, 잘못됬을 경우 포커스를 주거나, 포커스를 취소하는데 사용할 수 있습니다.
    */
   validated: boolean;
 
   /**
-   * 에러메시지는 비어있거나 혹은 존재할 수 있습니다.
-   * 에러문자열이 비어있지 않으면(= 존재하면), 에러메시지 혹은 입력박스의 테두리를 빨간색으로 표시한다거나 하는 에러효과를 나타낼 떄 응용합니다.
-   * 에러문자열이 비어있다면 에러효과를 지우고 원상태로 표기하는데 사용됩니다.
-   *
-   * 나타나는 에러 효과가 없다고 해서, 반드시 유효성검증이 통과됬다는 것을 의미하지는 않습니다.
-   * 입력하는 도중에(onChange) 입력값을 지워서 입력박스에 아무것도 없는 경우, 에러메시지를 표시하지 않을 뿐 유효하지는 않습니다.
+   * validated가 true일 경우 message는 성공메시지를 의미할 수 있고,
+   * validated가 false일 경우 message는 에러메시지를 의미할 수 있습니다.
    */
-  errorMessage: string;
+  message: string;
 }
 
-/**
- * 사용자가 값을 입력하던 와중에 그 값이 잘못되면, 올바른값으로 정정하기 위한 작업이 필요할 수 있습니다.
- * 그러므로 value property가 interface에 필요합니다.
- */
-export interface OnChangeValidateResult extends OnSubmitValidateResult {
-
-  /**
-   * 사용자가 입력한 값을 나타냅니다.
-   * 일반적으로 사용자가 입력한 값과 동일하지만,
-   * 입력될 수 있는 값의 종류가 제한된 경우, (ex : 숫자만) 입력한 값과 이 값이 다를 수 있습니다.
-   */
-  value: string;
-}
-
-export type OnChangeValidatorType = (value: string) => OnChangeValidateResult;
-export type OnSubmitValidatorType = (value: string) => OnSubmitValidateResult;
-
-export const VALIDATE_SUCCESS_RESULT: OnSubmitValidateResult = {
-  errorMessage: '',
+export const VALIDATE_SUCCESS_RESULT: ValidateResult = {
+  message: '',
   validated: true
 };
 
-export function onChangeValidateWhenEmpty(value: string, validator?: OnChangeValidatorType): OnChangeValidateResult {
+export function onEnterValidateWhenEmpty(value: string, config: EmptyMessageConfig = EMPTY_ERROR_MESSAGE_CONFIG): ValidateResult {
 
   if (value.length === 0) {
-
     return {
-      value,
-      errorMessage: '',
+      message: config?.emptyError,
       validated: false
-    };
-
-  } else if (validator !== undefined) {
-
-    return validator(value);
-
-  } else {
-
-    return {
-      value,
-      ...VALIDATE_SUCCESS_RESULT
     };
   }
+
+  return VALIDATE_SUCCESS_RESULT;
 }
 
-export function onSubmitValidateWhenEmpty(value: string, validator?: OnSubmitValidatorType, config?: EmptyErrorMessageConfig): OnSubmitValidateResult {
+/**********************************************
+ * 활용예시
+**********************************************/
 
-  if (value.length === 0) {
+/**
+ * 주로 실패메시지를 쓸 때 이 Config를 많이 응용하겠지만,
+ * 성공메시지를 쓸 때도 사용이 가능할 수 있습니다.
+ */
+export interface WalletNameUpMessageConfig extends EmptyMessageConfig {
+  verifiedName: string;
+  symbolError: string;
+}
 
+export function onEnterValidateWhenEmailForSignUp(value: string, config?: WalletNameUpMessageConfig) {
+
+  const emptyResult = onEnterValidateWhenEmpty(value, config);
+  if (!emptyResult.validated) {
+    return emptyResult;
+  }
+
+  if (true) {
     return {
-      errorMessage: config?.whenEmpty ?? ERROR_MESSAGE.whenEmpty,
-      validated: false
+      validated: true,
+      message: config?.verifiedName ?? '사용 가능한 지갑이름입니다.'
     };
 
-  } else if (validator !== undefined) {
-
-    return validator(value);
-
   } else {
-
-    return VALIDATE_SUCCESS_RESULT;
+    return {
+      validated: false,
+      message: config?.symbolError ?? '지갑이름에 특수문자가 있으면 안됩니다.'
+    };
   }
 }
