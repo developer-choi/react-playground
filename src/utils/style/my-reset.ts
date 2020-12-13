b0ad9d3ad8aaff046650d0ff69f68027e97ce4a4
choiyujin <dbwls6840@naver.com>
2020-12-13 23:31:08 +0900

    font-size초기화에 button추가

--- src/utils/style/my-reset.ts
+++ src/utils/style/my-reset.ts
@@ -22,7 +22,7 @@ const InputTypeNumberHidden = css`
 `;
 
 const Font = css`
-  body, input, textarea {
+  body, input, textarea, button {
     font-size: 13px;
   }
 `;
@@ -30,32 +30,32 @@ const Font = css`
 export const MyReset = css`
   ${InputTypeSearchHidden};
   ${InputTypeNumberHidden};
-  ${Font}
-  
+  ${Font};
+
   * {
     padding: 0; //styled-reset의 button에 빠져있음
     border: none;
-    background-color: transparent;  
+    background-color: transparent;
     box-sizing: border-box;
-    
+
     //font-size: inherit; 어떤 이유로 추가헀었는지 기억이 안나서 주석처리했음. 버그상황 확보되면 그 때 필기.
     //margin: 0; 어떤 이유로 추가헀었는지 기억이 안나서 주석처리했음. 버그상황 확보되면 그 때 필기.
   }
-  
+
   table {
-    min-width: 100%; //최대가 100%까지밖에 안늘어나서, 페이지 일부에 가로로 긴 테이블이 이거때문에 가로스크롤이 안생겼음.
+    width: 100%;
     table-layout: fixed;
-    
+
     //td { 직접 셀마다 길어짐 대비를 하는것이 맞다. 오히려 셀 안에서 position absolute 자식이 안보이는 경우가 있더라. (툴팁 등)
     //  overflow: hidden;
     //}
   }
-  
+
   td {
     //styled-reset에 td가 vertical-align baseline 설정되있는데, 테이블 셀은 middle이 더 이쁘고 오히려 baseline이 이상하다.
     vertical-align: middle;
   }
-  
+
   b, h1, h2, h3, h4, h5, h6 {
     font-weight: bold;
   }
@@ -64,17 +64,17 @@ export const MyReset = css`
     text-decoration: none;
     color: inherit; //이거 안하면 크롬에서 방문한링크 보라색나오고 방문안한거 파란색으로 나옴 
   }
-  
+
   button {
     cursor: pointer;
   }
-  
+
   input, button {
     &:disabled {
       cursor: not-allowed;
     }
   }
-  
+
   textarea {
     resize: none;
   }
