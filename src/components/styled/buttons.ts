8a6d08bfc640c547a0f3f021bd8148c2a5be09a3
choiyujin <dbwls6840@naver.com>
2020-12-13 23:32:33 +0900

    BasicButton에 Link로 변환해도 동일한 스타일이 되도록 코드추가

--- src/components/styled/buttons.ts
+++ src/components/styled/buttons.ts
@@ -1,23 +1,42 @@
-import { ButtonProp } from 'react';
 import styled from 'styled-components';
 
-export const BasicButton = styled.button.attrs<ButtonProp>(() => ({type: 'button'}))`
+export const BasicButton = styled.button.attrs(() => ({type: 'button'}))`
+
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  color: white;
-  font-size: 14px;
-  
-  text-decoration: none;
-  
+
+  //button은 이 값이 기본값이지만, button을 a로 쓰는 경우에 필요해서 추가.
+  text-align: center;
+
+  //button은 이 값이 기본값이지만, button을 a로 쓰는경우 서로 디자인이 통일되야해서 추가.
+  line-height: normal;
+
   &:hover {
     opacity: 0.7;
   }
-  &:active {
-    opacity: 1;
-  }
+`;
 
-  &:disabled {
-    opacity: 0.3;
+export const BasicSizeButton = styled(BasicButton)`
+  padding: 10px 20px;
+  min-width: 100px;
+`;
+
+export const BottomButtonWrap = styled.div`
+  margin-top: 10px;
+  text-align: right;
+
+  > :first-child {
+    margin-right: 10px;
   }
+`;
+
+export const SubmitButton = styled(BasicSizeButton)`
+  background-color: ${props => props.theme.main};
+  color: white;
+`;
+
+export const CancelButton = styled(BasicSizeButton)`
+  background-color: ${props => props.theme.cancel};
+  color: white;
 `;
