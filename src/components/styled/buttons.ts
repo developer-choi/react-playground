aa1314aef46f21e9082273deba330678075c909f
choeyujin <dbwls6840@naver.com>
2020-12-22 11:54:23 +0900

    color 하나만 바꾸기 위해서는 class이름으로 컬러변경이 가능하도록 BasicButton에서 클래스에 따른 컬러속성을 적용할 수 있어야하며, 동시에 매번 컬러에 대한 클래스이름을 쓰는 것도 방지하기위해 기존 css button color 적용구조를 바꿨음.

--- src/components/styled/buttons.ts
+++ src/components/styled/buttons.ts
@@ -1,5 +1,35 @@
 import styled, {css} from 'styled-components';
 
+//colors (color, background-color)
+export const ButtonColor1 = css`
+  color: ${props => props.theme.textColor1};
+  background-color: ${props => props.theme.backColor1};
+`;
+
+export const ButtonColor2 = css`
+  color: ${props => props.theme.textColor2};
+  background-color: ${props => props.theme.color2};
+
+  &:disabled {
+    background-color: ${props => props.theme.gray1};
+  }
+
+  &:active {
+    //color: some-color;
+    //background-color: some-color;
+  }
+
+  &:visited {
+    //color: some-color;
+    //background-color: some-color;
+  }
+
+  &:focus {
+    //color: some-color;
+    //background-color: some-color;
+  }
+`;
+
 export const BasicButton = styled.button.attrs(() => ({type: 'button'}))`
 
   //이 값들은, 이미지를 button으로 감쌀 때, 이미지를 버튼기준으로 수직수평중앙정렬할 때 필요함.
@@ -12,50 +42,25 @@ export const BasicButton = styled.button.attrs(() => ({type: 'button'}))`
 
   //button은 이 값이 기본값이지만, button을 a로 쓰는경우 서로 디자인이 통일되야해서 추가.
   line-height: normal;
-
+  
   //general styling
   &:hover {
     opacity: 0.7;
+  }
+  
+  //colors
+  &.color1 {
+    ${ButtonColor1};
+  }
+  
+  &.color2 {
+    ${ButtonColor2};
   }
 
   &:disabled {
     cursor: not-allowed;
     background-color: lightgray;
     color: white;
-  }
-`;
-
-//colors (color, background-color)
-export const ButtonColor1 = css`
-  &.color1 {
-    color: ${props => props.theme.textColor1};
-    background-color: ${props => props.theme.backColor1};
-  }
-`;
-
-export const ButtonColor2 = css`
-  &.color1 {
-    color: ${props => props.theme.textColor2};
-    background-color: ${props => props.theme.color2};
-
-    &:disabled {
-      background-color: ${props => props.theme.gray1};
-    }
-    
-    &:active {
-      //color: some-color;
-      //background-color: some-color;
-    }
-    
-    &:visited {
-      //color: some-color;
-      //background-color: some-color;
-    }
-    
-    &:focus {
-      //color: some-color;
-      //background-color: some-color;
-    }
   }
 `;
 
@@ -66,13 +71,21 @@ export const BasicSizeButton = styled(BasicButton)`
   font-size: 13px;
 `;
 
+export const SmallSizeButton = styled(BasicButton)`
+  font-size: 15px;
+  padding: 10px 20px;
+  min-width: 100px;
+  border-radius: 5px; //size에는 border-radius도 포함되고,
+  flex-shrink: 0; //버튼들 중에는 길이가 줄어들면 안되는 경우도 있어서 여기에 선언한다.
+`;
+
 //ready-made (select color and size)
 export const SubmitButton = styled(BasicSizeButton)`
   ${ButtonColor1};
   ${BasicSizeButton};
 `;
 
-export const CancelButton = styled(BasicSizeButton)`
-  ${ButtonColor2};
-  ${BasicSizeButton};
+export const TableBottomButton = styled(SmallSizeButton)`
+  align-self: flex-end;
+  ${ButtonColor1};
 `;
