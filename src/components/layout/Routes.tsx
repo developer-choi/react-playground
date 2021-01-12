ec11d0771176caaf4f6d6ac392b4acebefa6f030
choeyujin <dbwls6840@naver.com>
2021-01-12 16:04:52 +0900

    material-ui/core 코드분석하기위한 환경 조성

--- src/components/layout/Routes.tsx
+++ src/components/layout/Routes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import WithoutAsideLayout from './WithoutAsideLayout';
 import Home from '../../pages/without-aside/Home';
 
 export default function Routes() {
@@ -8,9 +7,7 @@ export default function Routes() {
   return (
       <Switch>
         <Route>
-          <WithoutAsideLayout>
             <Home/>
-          </WithoutAsideLayout>
         </Route>
       </Switch>
   );
--- src/pages/without-aside/Home.tsx
+++ src/pages/without-aside/Home.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 import styled from 'styled-components';
+import MenuItem from '../../material-ui/src/MenuItem';
+import Select from '../../material-ui/src/Select';
 
 export default function Home() {
 
+  const [age, setAge] = useState(10);
+
+  const onChangeAge = useCallback((event: ChangeEvent<{ value: number }>) => {
+    setAge(event.target.value);
+  }, []);
+
   return (
       <HomeStyle>
-
+        <Select value={age} onChange={onChangeAge as any}>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+          <MenuItem value={30}>30</MenuItem>
+        </Select>
       </HomeStyle>
   );
 }
 
 const HomeStyle = styled.div`
+  padding: 30px;
 `;
