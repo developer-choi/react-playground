7aac2e7a9e8cae6f66739f510e455046aea97566
choeyujin <dbwls6840@naver.com>
2021-02-15 06:32:39 +0900

    Routes랑 Home 원복

--- src/components/layout/Routes.tsx
+++ src/components/layout/Routes.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from '../../pages/without-aside/Home';
+import WithoutAsideLayout from './WithoutAsideLayout';
 
 export default function Routes() {
-
+  
   return (
       <Switch>
         <Route>
+          <WithoutAsideLayout>
             <Home/>
+          </WithoutAsideLayout>
         </Route>
       </Switch>
   );
--- src/pages/without-aside/Home.tsx
+++ src/pages/without-aside/Home.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
-import {ProgressBarExample} from '../../components/progress/ProgressBar';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Home() {
   
-  return <ProgressBarExample/>;
+  return (
+      <HomeStyle>
+        <div className="top-wrap">
+          <span className="title">React</span>
+          <span className="content">사용자 인터페이스를 만들기 위한 JavaScript 라이브러리</span>
+          <Link to="/doc">시작하기</Link>
+        </div>
+      </HomeStyle>
+  );
 }
+
+const HomeStyle = styled.div`
+  width: 100%;
+  height: 400px;
+  background: ${props => props.theme.colors.lightBlack};
+  padding-top: 50px;
+  padding-bottom: 100px;
+
+  .top-wrap {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > * {
+      display: block;
+      margin-top: 25px;
+    }
+
+    .title {
+      font-size: 45px;
+      color: ${props => props.theme.colors.reactBlue};
+      font-weight: bold;
+    }
+
+    .content {
+      font-size: 25px;
+      color: white;
+    }
+
+    a {
+      padding: 10px 30px;
+      background: ${props => props.theme.colors.reactBlue};
+      font-size: 25px;
+    }
+  }
+`;
