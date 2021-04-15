4af240ef453559a2c3cb0baaebfe976981cb1f88
choeyujin <dbwls6840@naver.com>
2021-04-15 23:46:11 +0900

    src/components 하위 필요없는 파일 삭제

--- src/components/extend/FormExtend.tsx
+++ src/components/extend/FormExtend.tsx
@@ -1,14 +1,10 @@
 import React, {ComponentProps, FormEvent, useCallback} from 'react';
 
-export interface FormProp extends ComponentProps<'form'> {
-
-}
-
 /**
  * onSubmit 콜백 실행시 페이지 새로고침을 막기위해
  * event.preventDefault() 호출 후 onSubmit 콜백을 실행합니다.
  */
-export default function FormExtend({onSubmit, ...rest}: FormProp) {
+export default function FormExtend({onSubmit, ...rest}: Omit<ComponentProps<'form'>, 'ref'>) {
 
   const _onSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
 
