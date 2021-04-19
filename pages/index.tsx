773639be3f07749da3767143d8d8557b5ae63182
choeyujin <dbwls6840@naver.com>
2021-04-19 09:30:19 +0900

    next로 전환

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function index() {
+  
+  return (
+      <Wrap>
+        index.tsx Page
+      </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  background-color: red;
+`;
