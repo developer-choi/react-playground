c76b6e98badd61b324bc017c90961fea5f5578db
choeyujin <dbwls6840@naver.com>
2021-04-20 23:10:20 +0900

    develop Header Component

--- src/components/layouts/components/Header.tsx
+++ src/components/layouts/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 export interface HeaderProp {
 
@@ -9,11 +10,18 @@ export default function Header({...rest}: HeaderProp) {
   
   return (
       <Wrap {...rest}>
-        <NaverText width={52} src="/images/naver-text-logo.png" alt="naver-text-logo"/>
-        <MainDoor>
-          <CafeName>react-library</CafeName>
-          <img width="100%" src="/images/cafe-main-door.png" alt="cafe-main-door"/>
-        </MainDoor>
+        <Link href="/">
+          <a>
+            <NaverText width={52} src="/images/naver-text-logo.png" alt="naver-text-logo"/>
+            <MainDoor>
+              <TextWrap>
+                <CafeName>react-library</CafeName>
+                <CafeUrl>https://react-library-eta.vercel.app/</CafeUrl>
+              </TextWrap>
+              <img width="100%" src="/images/cafe-main-door.png" alt="cafe-main-door"/>
+            </MainDoor>
+          </a>
+        </Link>
       </Wrap>
   );
 }
@@ -25,14 +33,24 @@ const NaverText = styled.img`
   margin: 15px 0;
 `;
 
+const TextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  bottom: 24px;
+  left: 24px;
+`;
+
 const CafeName = styled.span`
   font-weight: bold;
-  font-size: 30px;
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
+  font-size: 40px;
 `;
 
 const MainDoor = styled.div`
   position: relative;
+`;
+
+const CafeUrl = styled.span`
+  color: #666;
+  margin-top: 10px;
 `;
