f49f0c81e8cb25d9df26703f1110ab51a9fd3b73
choeyujin <dbwls6840@naver.com>
2021-04-23 01:05:17 +0900

    I found a bug case for input.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 import MainLayout from '../src/components/layouts/MainLayout';
-import InputNumber from '@components/extend/InputNumber';
 
 export default function Page() {
-  const [value, setValue] = useState('');
-  const onChangeText = useCallback((value: string) => {
-    setValue(value);
-  }, []);
-  console.log(value);
   return (
       <MainLayout>
-        <InputNumber value={value} onChangeText={onChangeText}/>
       </MainLayout>
   );
 };
