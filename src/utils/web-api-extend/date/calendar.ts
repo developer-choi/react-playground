22a0516dd83fd0b5bbbfba9eb09b1c002479629e
choeyujin <dbwls6840@naver.com>
2021-04-25 20:51:19 +0900

    Clean up the date module.

--- src/utils/web-api-extend/date/calendar.ts
+++ src/utils/web-api-extend/date/calendar.ts
@@ -1,18 +1,34 @@
-import {dateToDetail} from './date';
+import {destructDate} from './date-util';
+
+export interface MonthDetail {
+  firstDateOfTheMonth: Date; //해당 Date객체의 month달의 첫번 째 일의 Date객체. 9월이면 9월1일에 대한 Date객체.
+  lastDateOfTheMonth: Date; //해당 Date객체의 month달의 마지막 일의 Date객체. 9월이면 9월30일에 대한 Date객체.
+}
+
+export function dateToMonthDetail(targetDate = new Date()): MonthDetail {
+  const {year, month} = destructDate(targetDate);
+  const firstDate = new Date(year, month, 1);
+  const lastDate = new Date(year, month + 1, 0);
+  
+  return {
+    lastDateOfTheMonth: lastDate, firstDateOfTheMonth: firstDate
+  };
+}
 
 export function getCalendarDates(targetDate = new Date()): Date[] {
   
-  const {year, month, lastDate, firstDate} = dateToDetail(targetDate);
+  const {year, month} = destructDate(targetDate);
+  const {lastDateOfTheMonth, firstDateOfTheMonth} = dateToMonthDetail(targetDate);
   
-  const prevMonthDayCount = firstDate.getDay();
+  const prevMonthDayCount = firstDateOfTheMonth.getDay();
   
   const prevMonthInfos = new Array(prevMonthDayCount)
       .fill('').map((value, index) => new Date(year, month, index - prevMonthDayCount));
   
-  const currentMonthInfos = new Array(lastDate.getDate())
+  const currentMonthInfos = new Array(lastDateOfTheMonth.getDate())
       .fill('').map((value, index) => new Date(year, month, index + 1));
   
-  const nextMonthDayCount = 7 - lastDate.getDay() - 1;
+  const nextMonthDayCount = 7 - lastDateOfTheMonth.getDay() - 1;
   
   const nextMonthInfos = new Array(nextMonthDayCount)
       .fill('').map((value, index) => new Date(year, month + 1, index + 1));
