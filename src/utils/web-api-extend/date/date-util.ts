c1200e663dbba6211582e006da3b4f8f56169db5
choeyujin <dbwls6840@naver.com>
2021-04-26 10:07:36 +0900

    Develop getSlicedDate

--- src/utils/web-api-extend/date/date-util.ts
+++ src/utils/web-api-extend/date/date-util.ts
@@ -18,3 +18,26 @@ export function destructDate(target = new Date()): DestructedDate {
   const milliSecond = target.getMilliseconds();
   return { year, month, date, hour, minute, second, milliSecond };
 }
+
+/**
+ * @param target 지정한 Date객체의 (기본값 : 함수가 호출된 현재에 대한 Date객체)
+ * @return year, month, date, hour, minute, second, milliSecond 순서대로 저장된 배열을 반환합니다.
+ * @example (new Date('2021-04-03 12:34:56')) => [2021, 4, 3, 12, 34, 56, 0]
+ * @example new Date(getOrderedDateProperty('2021-04-03 12:34:56').slice(0, 3)) => 2021-04-03에 대한 Date객체가 반환됩니다.
+ */
+export function getDatePropertyArray(target = new Date()): [number, number, number, number, number, number, number] {
+  const {year, month, date, hour, minute, second, milliSecond} = destructDate(target);
+  return [year, month, date, hour, minute, second, milliSecond];
+}
+
+/**
+ * @example sliceIndex (2, new Date('2021-01-02 12:34:56')) => new Date('2021-01-01 00:00:00')
+ * @example sliceIndex (3, new Date('2021-01-02 12:34:56')) => new Date('2021-01-02 00:00:00')
+ * @example sliceIndex (4, new Date('2021-01-02 12:34:56')) => new Date('2021-01-02 12:00:00')
+ * @example sliceIndex (5, new Date('2021-01-02 12:34:56')) => new Date('2021-01-02 12:34:00')
+ * @example sliceIndex (6, new Date('2021-01-02 12:34:56')) => new Date('2021-01-02 12:34:56')
+ */
+export function getSlicedDate(sliceIndex: 2 | 3 | 4 | 5 | 6, target = new Date): Date {
+  const dateProperties = getDatePropertyArray(target).slice(0, sliceIndex);
+  return new Date(...dateProperties as [any]);
+}
