75df47c7901e7ee2ad6d6d8084fbb58128c698de
choeyujin <dbwls6840@naver.com>
2021-05-06 14:31:40 +0900

    Test input type date3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, {PropsWithChildren} from 'react';
+import styled, {css} from 'styled-components';
 
 export default function Page() {
+  
   return (
-      <input style={{width: 100, height: 100, backgroundColor: 'red'}} type="date"/>
+      <>
+        <CalendarButton>
+          <CustomButton>버튼</CustomButton>
+        </CalendarButton>
+        <CalendarButton>
+          <img src="/calendar.svg" alt=""/>
+        </CalendarButton>
+      </>
   );
 };
+
+function CalendarButton({children}: PropsWithChildren<{}>) {
+  return (
+      <Label>
+        {children}
+        <Input type="date"/>
+      </Label>
+  );
+}
+
+const Label = styled.label`
+  position: relative;
+  display: inline-flex;
+`;
+
+const absoluteFull = css`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Input = styled.input`
+  font-size: 0;
+  ${absoluteFull};
+  
+  ::-webkit-calendar-picker-indicator {
+    ${absoluteFull};
+    margin: 0;
+    background-image: none;
+  }
+`;
+
+const CustomButton = styled.button`
+  background: red;
+  width: 100px;
+  height: 50px;
+  color: white;
+`;
