6967b620b72fea68d7b8a5f70a771535fb1c1c74
choeyujin <dbwls6840@naver.com>
2021-05-20 17:40:11 +0900

    Added a bug case for when setState is not merged.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
-import SendSvg from '@components/atom/svg/SendSvg';
-import { Button } from '@components/atom/button/button-presets';
 import styled from 'styled-components';
 
 export default function Page() {
   
-  const [active, setActive] = React.useState(false);
+  const [value1, setValue1] = React.useState(0);
+  const [value2, setValue2] = React.useState(2);
+  const [value3, setValue3] = React.useState(4);
   
-  const toggle = React.useCallback(() => {
-    setActive(prevState => !prevState);
+  const onClick = React.useCallback(() => {
+    setValue1(prevState => prevState + 1);
+    setValue2(prevState => prevState + 2);
+    setValue3(prevState => prevState + 3);
   }, []);
   
+  React.useEffect(() => {
+    document.body.onclick = onClick;
+  }, [onClick]);
+  
+  console.log(value1, value2, value3);
+  
   return (
-      <div>
-        <SendTransition className={active ? 'active' : ''}/>
-        <Button onClick={toggle}>Toggle</Button>
-      </div>
+      <Wrap>
+        <Text>Click Html Body</Text>
+      </Wrap>
   );
 };
 
-const SendTransition = styled(SendSvg)`
-  path {
-    fill: gray;
-    transition: fill 1s;
-  }
-  
-  &.active path {
-    fill: red;
-  }
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const Text = styled.span`
+  font-weight: bold;
+  font-size: 20px;
 `;
