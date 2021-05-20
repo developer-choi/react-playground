b86c0eb676e8a5dd994152c3b9199848503885e5
choeyujin <dbwls6840@naver.com>
2021-05-20 09:33:33 +0900

    Update queryStringValueConvertString

--- src/utils/extend/query-string.ts
+++ src/utils/extend/query-string.ts
@@ -10,9 +10,11 @@ export type ParsedUrlQueryValue = ParsedUrlQuery['any-key'];
  *
  * query string에 담을 수 있는 값의 타입은 undefined, string, string[] 총 3가지인데, 개발자가 허용하는 값의 타입은 string인 경우가 많았습니다.
  * 그러므로 개발자입장에서는 undefined도 string[]도 둘 다 유효하지 않은 값이기 때문에, 둘 다 걸러낼 필요성이 있었습니다.
+ *
+ * 또한, 빈문자열인 경우에도 유효하지 않은 값이라고 판단되어 똑같이 걸러낼 필요성이 있습니다.
  */
 export function queryStringValueConvertString(value: ParsedUrlQueryValue): string | undefined {
-  if (typeof value !== 'string') {
+  if (!value || Array.isArray(value)) {
     return undefined;
   } else {
     return value;
