acb370acd63e92e965a80c15c507a9a68d87ba52
choeyujin <dbwls6840@naver.com>
2021-05-24 09:28:42 +0900

    Add Animation, Transition Example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,58 @@
+import { Button } from '@components/atom/button/button-presets';
 import React from 'react';
-import 'video.js/dist/video-js.css';
-import ReactPlayer from 'react-player';
+import styled, {css, keyframes} from 'styled-components';
 
-const src = 'https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8';
-
+/**
+ * transition이랑 다르게 animation은 css속성이 사라질 때(= active class가 사라져서 active class로 선언했던 스타일들이 적용되지 않을 때)
+ * 애니메이션이 작동되지않는다.
+ */
 export default function Page() {
+  const [active, setActive] = React.useState(false);
+  const toggle = React.useCallback(() => {
+    setActive(prevState => !prevState);
+  }, []);
   return (
-      <CustomPlayer muted={false}/>
+      <Wrap>
+        <Button onClick={toggle}>토글</Button>
+        <TransitionBox className={active ? 'active' : ''}/>
+        <AnimationBox className={active ? 'active' : ''}/>
+      </Wrap>
   );
 }
 
-function CustomPlayer({muted}: {muted: boolean}) {
-  
-  return (
-      <ReactPlayer
-          url={src}
-          controls
-          muted={muted}
-          playing
-          config={{
-            file: { forceHLS: true },
-          }}
-      />
-  );
-}
+const Wrap = styled.div`
+  * {
+    margin-bottom: 20px;
+  }
+`;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+`;
+
+const DURATION = 2;
+const commonCss = css`
+  background-color: blue;
+`;
+
+const TransitionBox = styled(Box)`
+  ${commonCss};
+  transition: background-color ${DURATION}s;
+  &.active {
+    background-color: red;
+  }
+`;
+
+const animate = keyframes`
+  to {
+    background-color: red;
+  }
+`;
+
+const AnimationBox = styled(Box)`
+  ${commonCss};
+  &.active {
+    animation: ${animate} ${DURATION}s forwards;
+  }
+`;
