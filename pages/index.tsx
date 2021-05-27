e1b92b36123c6dcd42759fa7065512c9e6d1cc30
choeyujin <dbwls6840@naver.com>
2021-05-27 09:52:38 +0900

    Add input type date example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 
 export default function Page() {
+  const [date, setDate] = useState('');
+  
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    alert(event.target.value);
+    setDate(event.target.value);
+  }, []);
+  
   return (
       <div>
-        hello world
+        <input type="date" value={date} onChange={onChange}/>
       </div>
   );
 }
