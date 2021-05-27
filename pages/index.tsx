c3b8b95402d4757396f1268b356079e977917f6b
choeyujin <dbwls6840@naver.com>
2021-05-27 10:02:34 +0900

    Update input type date example

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ export default function Page() {
   const [date, setDate] = useState('');
   
   const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    alert(event.target.value);
     setDate(event.target.value);
   }, []);
   
   return (
       <div>
         <input style={{width: 100, height: 100, backgroundColor: 'red'}} type="date" value={date} onChange={onChange}/>
+        <br/>
+        result={date}
+        <br/>
+        typeof={typeof date}
       </div>
   );
 }
