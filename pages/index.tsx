ddf1699ab886f86b84b3a33ea968f11d73254f1f
choeyujin <dbwls6840@naver.com>
2021-06-03 15:12:59 +0900

    Add mobile camera or galery example

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ export default function Page() {
   
   return (
       <div>
-        Hello World
+        <input type="file" accept=".jpg,.png"/> {/* 겔러리 */}
+        <input type="file" capture style={{marginTop: 10}}/> {/* 카메라 */}
       </div>
   );
 }
