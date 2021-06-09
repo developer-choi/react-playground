caa50e506d37ee407b8c7af1c16c4fc2c0400801
choeyujin <dbwls6840@naver.com>
2021-06-09 20:08:15 +0900

    Add debounce example

---
+++ pages/debounce.tsx
@@ -0,0 +1,22 @@
+import React, {useCallback} from 'react';
+import Head from 'next/head';
+import {Button} from '@components/atom/button/button-presets';
+import useDebouncedCallback from '../src/utils/custom-hooks/useDebouncedCallback';
+
+export default function DebouncePage() {
+  
+  const onClick = useDebouncedCallback(useCallback(() => {
+    console.log('clicked');
+  }, []));
+  
+  return (
+      <>
+        <Head>
+          <title>debounce</title>
+        </Head>
+        <div>
+          <Button onClick={onClick}>광클버튼</Button>
+        </div>
+      </>
+  );
+}
