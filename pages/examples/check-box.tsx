ef649ae259b023c19e39df067a65ebf1178f4d4f
choeyujin <dbwls6840@naver.com>
2021-06-23 14:47:10 +0900

    Update check-box example

---
+++ pages/examples/check-box.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import Head from 'next/head';
+import CheckBox, {CheckBoxLabelLeftProps, CheckBoxProps} from '@components/atom/CheckBox';
+import NaverCheckBoxSvg from '@components/svgs/NaverCheckBoxSvg';
+import styled from 'styled-components';
+
+export default function CheckboxPage() {
+  
+  const [someValue, setSomeValue] = useState(false);
+  
+  return (
+      <>
+        <Head>
+          <title>check-box</title>
+        </Head>
+        <Wrap>
+          <CheckBox checked={someValue} onChangeChecked={setSomeValue} label="default checkbox"/>
+          <NaverCheckBox checked={someValue} onChangeChecked={setSomeValue} label="naver checkbox"/>
+        </Wrap>
+      </>
+  );
+};
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+function NaverCheckBox(props: Omit<CheckBoxProps, 'labelLeft'>) {
+  
+  const labelLeftRender = React.useCallback(({checked}: CheckBoxLabelLeftProps) => {
+    return <NaverCheckBoxSvg color={checked ? '#03C75A' : '#707070'}/>
+  }, []);
+  
+  return (
+      <NaverStyledCheckBox labelLeft={labelLeftRender} {...props}/>
+  );
+}
+
+const NaverStyledCheckBox = styled(CheckBox)`
+  font-size: 18px;
+  
+  input {
+    appearance: none;
+    margin-right: 5px;
+  }
+`;
---
+++ src/components/atom/CheckBox.tsx
@@ -0,0 +1,36 @@
+import React, {ChangeEvent, ComponentProps, ComponentType, useCallback} from 'react';
+import styled from 'styled-components';
+import classNames from 'classnames';
+
+export interface CheckBoxLabelLeftProps {
+  checked: boolean;
+  disabled?: boolean;
+}
+
+export interface CheckBoxProps extends Omit<ComponentProps<'label'>, 'ref'>, Pick<ComponentProps<'input'>, 'disabled' | 'checked'> {
+  onChangeChecked: (checked: boolean) => void;
+  label: string;
+  checked: boolean;
+  labelLeft?: ComponentType<CheckBoxLabelLeftProps>;
+}
+
+export default function CheckBox({onChangeChecked, label, checked, className, disabled, labelLeft: LabelLeft, ...rest}: CheckBoxProps) {
+  
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    onChangeChecked(event.target.checked);
+  }, [onChangeChecked]);
+  
+  return (
+      <Label className={classNames({checked: disabled ? false : checked, disabled}, className)} {...rest}>
+        {LabelLeft && <LabelLeft checked={checked} disabled={disabled}/>}
+        <input type="checkbox" onChange={onChange} checked={checked} disabled={disabled}/>
+        {label}
+      </Label>
+  );
+}
+
+const Label = styled.label`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+`;
