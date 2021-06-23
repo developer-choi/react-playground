d552cb214cc51f55e97154086c794c65b98304eb
choeyujin <dbwls6840@naver.com>
2021-06-23 11:20:51 +0900

    Rename FormExtend to Form

--- src/components/extend/Form.tsx
+++ src/components/extend/Form.tsx
@@ -4,7 +4,7 @@ import React, {ComponentProps, FormEvent, useCallback} from 'react';
  * onSubmit 콜백 실행시 페이지 새로고침을 막기위해
  * event.preventDefault() 호출 후 onSubmit 콜백을 실행합니다.
  */
-export default function FormExtend({onSubmit, ...rest}: Omit<ComponentProps<'form'>, 'ref'>) {
+export default function Form({onSubmit, ...rest}: Omit<ComponentProps<'form'>, 'ref'>) {
 
   const _onSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
 
