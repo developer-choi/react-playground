5cb06344b2899a39473e1a5d34ed14a14d56873b
choeyujin <dbwls6840@naver.com>
2021-06-25 20:09:41 +0900

    Remove unused files

--- src/components/layouts/components/LayoutWrap.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const AllLayoutWrap = styled.div`
-  max-width: 1080px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-`;
--- src/types/declarations/react.d.ts
+++
@@ -1,8 +0,0 @@
-import 'react';
-import {ComponentProps} from 'react';
-
-type OmitRefComponentType<T> = Omit<ComponentProps<T>, 'span'>
-
-declare module 'react' {
-  export type SpanProp = OmitRefComponentType<'span'>;
-}
