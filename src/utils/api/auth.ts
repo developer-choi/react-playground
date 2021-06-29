006561db6d68b294cee45d9929fc76c05c1b59e5
choeyujin <dbwls6840@naver.com>
2021-06-29 15:06:26 +0900

    Update auth module

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,6 +1,8 @@
 import Router from 'next/router';
 import {GetServerSidePropsContext, GetServerSidePropsResult} from 'next';
 import {urlStringify} from '../extend/query-string';
+import {useEffect} from 'react';
+import {ParsedError} from './axios-config';
 
 /**
  * 이 모듈은, 사용자가 로그인 / 로그아웃 상태가 변경되었을 때 즉시 웹페이지를 새로 렌더링하지 않는 웹사이트에서 사용하기위한 모듈.
@@ -11,7 +13,6 @@ import {urlStringify} from '../extend/query-string';
  * 그래서, 다른 로그인된 모든 기기 즉시 로그아웃까지는 안되지만
  * 다른 로그인된 기기가 어떤 private 요청을 보냈을 때 로그아웃을 시키는것은 대응이 가능함.
  */
-
 
 /**
  * 로그인이 되어있는 유저라면 반드시 갖고있어야하는 값의 타입.
@@ -155,5 +156,38 @@ export function privateGerServerSideProps<PageProp>(option: PrivateGerServerSide
     };
   } else {
     return callback(currentlyUserInfo);
+  }
+}
+
+/**
+ * 로그인페이지에서 사용하려고 만들었고,
+ * 로그인페이지를 벗어나는 아래의 2가지 케이스에 대해 고려가 되었음.
+ * case1. [로그인 후 이용이 가능합니다] 팝업을 만난 사용자가 로그인안하고 그냥 다른페이지를 간다거나
+ * case2. 로그인을 성공하여 다른페이지로 간 사용자의 경우
+ *
+ * 다시 초기화를 해서 이후에 또 [로그인 해야 이용가능] 성격의 팝업을 만났을 때 이 팝업을 보여주도록 하기위해/
+ */
+export function useResetIgnoreForceLogin() {
+  useEffect(() => {
+    return () => {
+      /**
+       * ignoreLoginProcess는 store가 아니라 window가 제일 합리적임.
+       * 1. 창 하나 (또는 탭하나)에서 유일한 값이고 유지되는 값이어야 하니까.
+       * 2. 이 값이 바뀌어도 다시 컴포넌트들이 리렌더링되야하는건 아니니까.
+       */
+      window.ignoreForceLogin = false;
+    };
+  }, []);
+}
+
+const LOGIN_PAGE_URL_START_PATH = '/login';
+
+export function handleAuthError(data: ParsedError) {
+  const {message} = data;
+  
+  if (!window.ignoreForceLogin && !location.href.startsWith(LOGIN_PAGE_URL_START_PATH)) {
+    window.ignoreForceLogin = true;
+    alert(message);
+    window.location.href = getLoginRedirectUrl(location.href);
   }
 }
