50128aaadbae7f5a31661ce989c755efc244f532
choeyujin <dbwls6840@naver.com>
2021-07-07 23:30:21 +0900

    InputNumber 구현중

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import InputNumber from '@components/extend/InputNumber';
 
 export default function Page() {
+  const [number, setNumber] = useState('');
+  const [negative, setNegative] = useState('');
   return (
-      <div>
-        Hello World
-      </div>
+      <Wrap>
+        <InputNumber value={number} onChangeText={setNumber}/>
+        <InputNumber value={negative} onChangeText={setNegative} enableComma enableNegative maxIntegerLength={7} maxDecimalLength={7}/>
+      </Wrap>
   );
-}
+};
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  > input {
+    border: 1px solid black;
+    padding: 5px;
+    width: 200px;
+    
+    :not(:first-child) {
+      margin-top: 20px;
+    }
+  }
+`;
--- src/components/extend/InputNumber.tsx
+++ src/components/extend/InputNumber.tsx
@@ -1,50 +1,141 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import InputText, {InputTextProp} from '@components/extend/InputText';
 import {numberWithComma} from '../../utils/extend/number';
 
-export interface InputNumberProp extends InputTextProp {
+/**
+ * Overview : input component with "input type number" and only numbers.
+ *
+ * Feature 1. "type password" correspondence; apparently masked, but in reality only numbers are entered.
+ *
+ * Feature 2.
+ * Negative income response. What the majority of input type number is often rather should not be a negative input. (input prices, etc.)
+ * So, basically negative input from, but should function well in this. if it is a negative input to allow this.
+ *
+ * Feature 2-1. Must be inputted when only one '-' is entered for negative import power. (The string '-' itself is not a number, so it becomes NaN.)
+ * Feature 2-2. Must fit well with other props functions. It still negative, even when other functions shall be operated by well. such as maxDecimalLength
+ *
+ * Feature 3. Decimal input function.
+ * When a decimal number is entered up to 12345," the response must be made even if the end ends with a . but is not a valid Number type.
+ *
+ * Feature 3. Support for various additional functions (such as commas being entered between them, maximal integer decimal places, etc.)
+ *
+ * Feature 4. All of the above functions must also work when you do Control V.
+ */
+
+export interface InputNumberOption {
   /**
-   * 입력값의 소수 최대길이를 지정할 수 있습니다.
+   * You can specify a decimal maximum length for the input value.
    */
   maxDecimalLength?: number;
   
   /**
-   * 입력값의 정수 최대길이를 지정할 수 있습니다.
+   * You can specify an integer maximum length for the input value.
    */
   maxIntegerLength?: number;
   
   enableComma?: boolean;
-  
+  enableNegative?: boolean;
+  enableDecimal?: boolean;
+}
+
+export interface InputNumberProp extends InputTextProp, InputNumberOption {
   type?: 'number' | 'password';
 }
 
-export default function InputNumber({maxDecimalLength, maxIntegerLength, onChangeText, type = 'number', ignoreEventKeys = DEFAULT_IGNORE_EVENT_KEYS, enableComma, value, ...rest}: InputNumberProp) {
+export default function InputNumber({maxDecimalLength, maxIntegerLength, onChangeText, type = 'number', enableNegative = false, enableComma, value, ignoreEventKeys = EMPTY_ARRAY, enableDecimal = true, ...rest}: InputNumberProp) {
   
   const _onChangeText = useCallback((text: string) => {
-    const _text = enableComma ? text.replace(/,/g, '') : text;
+    onChangeText?.(parseText(text, value, {enableNegative, enableComma, maxDecimalLength, maxIntegerLength}));
+  }, [maxDecimalLength, maxIntegerLength, onChangeText, enableComma, enableNegative, value]);
   
-    if (!isValidNumberLength(_text, {maxIntegerLength, maxDecimalLength})) {
-      return;
+  const _ignoreEventKeys = useMemo(() => {
+    const keys = [...ignoreEventKeys];
+    
+    if (!enableNegative) {
+      keys.push('-');
     }
   
-    onChangeText?.(_text);
-  }, [maxDecimalLength, maxIntegerLength, onChangeText, enableComma]);
+    if (!enableDecimal) {
+      keys.push('.');
+    }
+  
+    return keys;
+  }, [ignoreEventKeys, enableNegative, enableDecimal]);
   
   return (
       <InputText
           onChangeText={_onChangeText}
           type={enableComma ? undefined : type}
-          ignoreEventKeys={ignoreEventKeys}
+          ignoreEventKeys={_ignoreEventKeys}
           value={enableComma ? numberWithComma(value) : value}
           {...rest}
       />
   );
 }
 
-const DEFAULT_IGNORE_EVENT_KEYS = ['-'];
+const EMPTY_ARRAY = [] as string[];
+
+function parseText(text: string, prevValue: string, {enableComma, enableNegative, maxIntegerLength, maxDecimalLength}: InputNumberOption ) {
+  /**
+   * In all cases with InputNumber components,
+   * we will remove spaces before and after strings because we believe that space entry is not required.
+   */
+  const trimmedText = text.trim();
+  
+  /**
+   * If enableNegative is true, ignoreEventKeys is already blocking the input itself.
+   * Nevertheless, the reason for this check is to respond to Control V.
+   */
+  if (trimmedText === '-') {
+    return enableNegative ? '-' : '';
+  }
+  
+  /**
+   * In most cases, the first text is - for negative input, since the user enters letters on the keyboard.
+   * These cases are easy to implement by skipping all validation.
+   */
+  if (trimmedText === '') {
+    return '';
+  }
+  
+  const commaNextText = enableComma ? trimmedText.replace(/,/g, '') : trimmedText;
+  
+  /**
+   * There are two reasons for converting to parseInt() instead of Number Constructor.
+   *
+   * First, if a user enters a letter by keyboard, the text is entered from left to right, so it can be prevented when a number is entered and not a number is entered.
+   * This is the default action of parseInt() until a string other than a number appears.
+   *
+   * Secondly, if you do Control V, only the places where you can parse with numbers will be saved.
+   */
+  const parsedNumber = parseInt(commaNextText);
+  
+  /**
+   * Since the text above is all crossed out for empty or '-' cases, if NaN occurs here, the user can unconditionally conclude that a non-numeric value has been entered.
+   */
+  if (Number.isNaN(parsedNumber)) {
+    return prevValue;
+  }
+  
+  /**
+   * 123456. 이런식으로 뭔가 소수값입력하려고 .딱하나찍었을 때 위에 parseInt()의 특성때문에 .입력을못해..
+   * 그런데 이거랑 정수자리수 소수자리수 체크랑 같이 잘 작동해야하는데 아 머리터지네..
+   *
+   * if(parsedNumber && enableDecimal && )
+   */
+  
+  const parsedString = String(parsedNumber);
+  
+  if (!isValidNumberLength(parsedString, {maxIntegerLength, maxDecimalLength})) {
+    return prevValue;
+  }
+  
+  return parsedString;
+}
 
 function isValidNumberLength(value: string, {maxDecimalLength, maxIntegerLength}: Pick<InputNumberProp, 'maxDecimalLength' | 'maxIntegerLength'>) {
   const {integer, decimal} = splitNumberDot(value);
+  console.log(integer, decimal);
   
   if (maxDecimalLength !== undefined && decimal.length > maxDecimalLength) {
     return false;
@@ -58,7 +149,7 @@ function isValidNumberLength(value: string, {maxDecimalLength, maxIntegerLength}
 }
 
 function splitNumberDot(value: string): {integer: string, decimal: string} {
-  const integer = Math.floor(Number(value)).toString();
+  const integer = Math.floor(Math.abs(Number(value))).toString();
   const dotIndex = value.indexOf('.');
   const decimal = dotIndex === -1 ? '' : value.slice(dotIndex + 1, value.length);
   return {
