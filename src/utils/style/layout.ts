0bf9fe861f7b554b7bf1eefb81a2a57288b6765a
choeyujin <dbwls6840@naver.com>
2021-07-24 15:16:00 +0900

    Remove unused codes.

--- src/utils/style/layout.ts
+++ src/utils/style/layout.ts
@@ -1,12 +1,3 @@
-export const HEIGHTS = Object.freeze({
-  header: 60,
-  headerDiffSection: 80
-});
-
-export const WIDTHS = Object.freeze({
-  aside: 350
-});
-
 export const ZINDEXS = Object.freeze({
   header: 10,
   modal: 20
