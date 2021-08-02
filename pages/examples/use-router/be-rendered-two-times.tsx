1ecc4c2dd6000011fc3694bebf7dd66666f507f3
choeyujin <dbwls6840@naver.com>
2021-08-02 20:11:56 +0900

    Added the example that be rendered two times.

---
+++ pages/examples/use-router/be-rendered-two-times.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Head from 'next/head';
+import {useRouter} from 'next/router';
+
+export default function BeRenderedTwoTimesPage() {
+  const { query } = useRouter();
+  
+  /**
+   * Case 1. If I declare getStaticProps() or nothing,
+   * Case 2. and If I enter into this page with query-string,
+   *
+   * This component will be rendered two times.
+   * To avoid this case, You can declare getServerSideProps().
+   */
+  console.log('render', query);
+  
+  return (
+      <>
+        <Head>
+          <title>be-rendered-two-times</title>
+        </Head>
+        <div>
+          be-rendered-two-times Page
+        </div>
+      </>
+  );
+}
+
+// export async function getStaticProps() {
+//   return {
+//     props: {}
+//   };
+// }
+
+// export async function getServerSideProps() {
+//   return {
+//     props: {}
+//   };
+// }
