1eb80ba870bc011217713023d231d0a69ecb687f
choeyujin <dbwls6840@naver.com>
2021-08-09 07:31:14 +0900

    Revert "Apply all: unset; property."
    This reverts commit f270c9185cb59cc4f946bd89feacc094e42835f7.

--- src/utils/style/global.ts
+++ src/utils/style/global.ts
@@ -1,5 +1,11 @@
 import {createGlobalStyle, css} from 'styled-components';
-import {reset} from './my-reset';
+import StyledReset from 'styled-reset';
+import {MyReset} from './my-reset';
+
+const reset = css`
+  ${StyledReset};
+  ${MyReset}; //styled-reset보다 my-reset이 우선 적용되야하므로 밑에와야한다.
+`;
 
 const font = css`
 
@@ -15,17 +21,22 @@ const font = css`
 `;
 
 const layout = css`
+  
   html, body, #__next {
     height: 100%;
   }
 `;
 
 export const GlobalStyle = createGlobalStyle`
+
   ${reset};
   ${font};
   ${layout};
   
   body {
     background-color: white;
+    //background-color: #04caf4; 전체 배경색 지정할일이 있거나
+    //color: black; 기본 색상을 지정할 일이 있거나,
+    //font-size: 14px; 기본 폰트 크기를 좀 줄이고싶을 때.
   }
 `;
--- src/utils/style/my-reset.ts
+++ src/utils/style/my-reset.ts
@@ -3,9 +3,7 @@ import {css} from 'styled-components';
 const inputTypeSearchHidden = css`
   input::-ms-clear,
   input::-ms-reveal {
-    display:none;
-    width:0;
-    height:0;
+    display:none;width:0;height:0;
   }
   input::-webkit-search-decoration,
   input::-webkit-search-cancel-button,
@@ -23,16 +21,27 @@ const inputTypeNumberHidden = css`
   }
 `;
 
-export const reset = css`
-  *:not(style, script, head) {
-    all: unset;
+const font = css`
+  //body에서 font-size를 지정하더라도, 나머지 태그들은 상속이 아니라 태그 자체에 font-size속성이 선언되어있기 때문에, 이렇게 지정해야 모든 엘레먼트의 폰트사이즈가 의도대로 초기화된다.
+  body, input, textarea, button {
+    font-size: 13px;
   }
-  
+`;
+
+export const MyReset = css`
   ${inputTypeSearchHidden};
   ${inputTypeNumberHidden};
+  ${font};
   
   * {
+    padding: 0; //styled-reset의 button에 빠져있음
+    border: none;
+    background-color: transparent;
     box-sizing: border-box;
+    margin: 0; // input type radio에 적용되어있는 기본 마진값을 reset하는 코드가 styled-reset에 빠져있음.
+
+    //font-size: inherit; 어떤 이유로 추가헀었는지 기억이 안나서 주석처리했음. 버그상황 확보되면 그 때 필기.
+    //margin: 0; 어떤 이유로 추가헀었는지 기억이 안나서 주석처리했음. 버그상황 확보되면 그 때 필기.
   }
   
   a {
@@ -52,5 +61,28 @@ export const reset = css`
   td {
     //styled-reset에 td가 vertical-align baseline 설정되있는데, 테이블 셀은 middle이 더 이쁘고 오히려 baseline이 이상하다.
     vertical-align: middle;
+  }
+
+  b, h1, h2, h3, h4, h5, h6 {
+    font-weight: bold;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit; //이거 안하면 크롬에서 방문한링크 보라색나오고 방문안한거 파란색으로 나옴
+  }
+
+  button, select {
+    cursor: pointer;
+  }
+
+  input, button {
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+
+  textarea {
+    resize: none;
   }
 `;
