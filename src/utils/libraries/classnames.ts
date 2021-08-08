d45a8f683ea16c15152e28c59e1fc67c2072b202
choeyujin <dbwls6840@naver.com>
2021-08-09 08:12:59 +0900

    Implemented the classnames directly.

---
+++ src/utils/libraries/classnames.ts
@@ -0,0 +1,70 @@
+var hasOwn = {}.hasOwnProperty;
+
+export type Value = string | number | boolean | undefined | null;
+export type Mapping = Record<string, unknown>;
+export interface ArgumentArray extends Array<Argument> {}
+export type Argument = Value | Mapping | ArgumentArray;
+
+export default function libraryClassName(...args: ArgumentArray) {
+  var classes = [];
+
+  for (var i = 0; i < args.length; i++) {
+    var arg = args[i];
+    if (!arg) continue;
+
+    var argType = typeof arg;
+
+    if (argType === 'string' || argType === 'number') {
+      classes.push(arg);
+    } else if (Array.isArray(arg)) {
+      if (arg.length) {
+        var inner: any = libraryClassName.apply(null, arg);
+        if (inner) {
+          classes.push(inner);
+        }
+      }
+    } else if (argType === 'object') {
+      if (arg.toString === Object.prototype.toString) {
+        //@ts-ignore
+        for (var key in arg) {
+          //@ts-ignore
+          if (hasOwn.call(arg, key) && arg[key]) {
+            classes.push(key);
+          }
+        }
+      } else {
+        classes.push(arg.toString());
+      }
+    }
+  }
+
+  return classes.join(' ');
+}
+
+/********************************************************
+ *
+ ********************************************************/
+
+export type ClassnamesArgument = string | Object;
+
+export function myClassName(...args: ClassnamesArgument[]) {
+  const array = args.reduce<string[]>((a, b) => {
+
+    if (typeof b === 'string') {
+      a.push(b);
+    } else {
+      const classNames = Object.entries(b).reduce<string[]>((a, [key, value]) => {
+        if (value) {
+          a.push(key);
+        }
+
+        return a;
+      }, []);
+      a.push(...classNames);
+    }
+
+    return a;
+  }, []);
+
+  return array.join(' ');
+}
