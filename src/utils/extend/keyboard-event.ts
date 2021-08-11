89006757122af9b73c639a066878b1141954a0bf
choeyujin <dbwls6840@naver.com>
2021-08-11 13:30:42 +0900

    Update the KeyboardEvent util.

--- src/utils/extend/keyboard-event.ts
+++ src/utils/extend/keyboard-event.ts
@@ -11,12 +11,14 @@ export type MinimumKeyboardEvent = Pick<KeyboardEvent, 'key' | SpecialKey>;
  * @param event KeyboardEvent (ex: React.KeyboardEvent or KeyboardEvent (typescript lib)
  * @param matchTarget 사용자가 누른 키와 특수키
  * @example (event, {key: 'A', matchKeys: ['ctrlKey']}) ==> 사용자가 Control Key를 누른 상태로 (= 다른 metaKey, altKey, shiftKey는 누르지않은상태로) 같은 key를 눌러야만 true가 반환됩니다.
+ * @example (event, {key: 'Delete'}) ==> 사용자가 어떠한 특수키 (ctrlKey, altKey, metaKey, shiftKey)를 동시에 같이 누르지 않은 상태에서 Delete키 딱 하나만 눌렀을 떄에만 true가 반환됩니다.
  */
-export function isMatchKeyboardEvent(event: MinimumKeyboardEvent, matchTarget: { key: string, matchKeys: SpecialKey[] }) {
+export function isMatchKeyboardEvent(event: MinimumKeyboardEvent, matchTarget: { key: string, matchKeys?: SpecialKey[] }) {
   if (event.key.toLowerCase() !== matchTarget.key.toLowerCase()) {
     return false;
   }
   
-  const restKeys = SPECIAL_KEYS.filter(key => !matchTarget.matchKeys.includes(key));
-  return matchTarget.matchKeys.every(key => event[key]) && restKeys.every(key => !event[key]);
+  const _matchKeys = matchTarget.matchKeys ?? [];
+  const restKeys = SPECIAL_KEYS.filter(key => !_matchKeys.includes(key));
+  return _matchKeys.every(key => event[key]) && restKeys.every(key => !event[key]);
 }
