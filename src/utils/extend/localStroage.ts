fe3f510305de7a4cf3bb611c9b64ec2275ba25c9
choeyujin <dbwls6840@naver.com>
2021-08-17 15:44:17 +0900

    Add localStorage util.

---
+++ src/utils/extend/localStroage.ts
@@ -0,0 +1,68 @@
+export class LocalStorageManager {
+  readonly key: string;
+  
+  constructor(key: string) {
+    this.key = key;
+  }
+  
+  setItem(value: string) {
+    localStorage.setItem(this.key, value);
+  }
+  
+  getItem() {
+    return localStorage.getItem(this.key);
+  }
+  
+  removeItem () {
+    localStorage.removeItem(this.key);
+  }
+}
+
+export class LocalStorageObjectManager<V> extends LocalStorageManager{
+  setStringifyItem(value: V) {
+    localStorage.setItem(this.key, JSON.stringify(value));
+  }
+  
+  parseItem() {
+    const item = localStorage.getItem(this.key);
+    return item ? JSON.parse(item) as V : null;
+  }
+}
+
+export class LocalStorageArrayManager<E, P> extends LocalStorageObjectManager<E[]> {
+  readonly pkExtractor: (element: E) => P;
+  
+  constructor(key: string, pkExtractor: (element: E) => P) {
+    super(key);
+    this.pkExtractor = pkExtractor;
+  }
+  
+  parseItem(): E[] {
+    const array = super.parseItem();
+  
+    if (array === null) {
+      return [];
+      
+    } else {
+      return array;
+    }
+  }
+  
+  removeElementByPk(pk: P) {
+    const list = this.parseItem().filter(prev => this.pkExtractor(prev) !== pk);
+    this.setStringifyItem(list);
+    return list;
+  }
+  
+  removeElement(element: E) {
+    return this.removeElementByPk(this.pkExtractor(element));
+  }
+}
+
+interface Board {
+  pk: number;
+  title: string;
+}
+
+// Make your preset managers.
+export const boardManager = new LocalStorageArrayManager('text', (board: Board) => board.pk);
