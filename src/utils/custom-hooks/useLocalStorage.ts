a5be20015eec72d4bdac48414454aefd7d94129e
developer-choi <forworkchoe@gmail.com>
2021-08-18 23:26:00 +0900

    Added custom-hooks for LocalStorageManager and its derived classes.

--- src/utils/custom-hooks/useLocalStorage.ts
+++ src/utils/custom-hooks/useLocalStorage.ts
@@ -1,15 +1,43 @@
 import {Dispatch, SetStateAction, useEffect, useState} from 'react';
-import type {LocalStorageObjectManager} from '../extend/localStroage';
+import type {LocalStorageArrayManager, LocalStorageManager, LocalStorageObjectManager} from '../extend/localStroage';
 
-export function useLocalStorage<V extends Object>(manager: LocalStorageObjectManager<V>) {
-  const [value, setValue] = useState<V | null>(() => manager.parseItem());
+/**
+ * I wanted to synchronize localStorage and state.
+ * If the state is changed, localStorage must be also changed.
+ * So I make this hooks.
+ * But I don't know well whether this hooks is useful for LocalStorageManager and its derived classes.
+ * Furthermore If I make custom-hooks for LocalStorageManager, Most of the code is duplicated.
+ */
+export function useLocalStorageManager(manager: LocalStorageManager) {
+  const [state, setState] = useState<string | null>(() => manager.getItem());
   
   useEffect(() => {
-    if (value !== null) {
-      console.log('setStringifyItem', value);
-      manager.setStringifyItem(value);
+    if (state !== null) {
+      manager.setItem(state);
     }
-  }, [value, manager]);
+  }, [state, manager]);
   
-  return [value, setValue] as [V | null, Dispatch<SetStateAction<V>>];
+  return [state, setState] as [string | null, Dispatch<SetStateAction<string>>];
+}
+
+export function useLocalStorageObjectManager<V extends Object>(manager: LocalStorageObjectManager<V>) {
+  const [state, setState] = useState<V | null>(() => manager.parseItem());
+  
+  useEffect(() => {
+    if (state !== null) {
+      manager.setStringifyItem(state);
+    }
+  }, [state, manager]);
+  
+  return [state, setState] as [V | null, Dispatch<SetStateAction<V>>];
+}
+
+export function useLocalStorageArrayManager<E, P>(manager: LocalStorageArrayManager<E, P>) {
+  const [state, setState] = useLocalStorageObjectManager(manager);
+  
+  /**
+   * If the localStorage's key is empty, this will return an empty array.
+   * Because of LocalStorageArrayManager's parseItem() is overridden.
+   */
+  return [state, setState] as [E[], Dispatch<SetStateAction<E[]>>];
 }
