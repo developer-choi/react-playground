dc7ef25b2cc19546ae10b7fb767f97e3938efa15
developer-choi <forworkchoe@gmail.com>
2021-08-19 18:08:21 +0900

    Added the hydration misunderstanding example.

---
+++ pages/examples/api/hydration.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+
+export default function HydrationPage() {
+  const [value] = useState(() => {
+    try {
+      console.log(localStorage); // ReferenceError is occurred here when rendering in Server.
+      return 'success';
+    } catch (error) {
+      return 'error';
+    }
+  });
+  
+  return (
+    <>
+      <Head>
+        <title>drag-and-drop page</title>
+      </Head>
+      <div>{value}</div>
+    </>
+  );
+}
