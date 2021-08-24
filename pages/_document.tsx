6a035b715ae959523314884121a2d0a4b2b6e3c7
Yu Jin Choe <forworkchoe@gmail.com>
2021-08-24 13:59:09 +0900

    Set up the styled-components.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
+import Document, {DocumentContext} from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
 
-class MyDocument extends Document {
+// https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {...initialProps};
-  }
-  
-  render() {
-    return (
-        <Html>
-          <Head>
-            <meta name="description" content="This is NextJS's Playground."/>
-          </Head>
-          <body>
-          <Main/>
-          <NextScript/>
-          </body>
-        </Html>
-    );
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default MyDocument;
