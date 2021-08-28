e388993abd7d8c72b822ed4db0f92d485860d85e
Yu Jin Choe <forworkchoe@gmail.com>
2021-08-28 14:36:38 +0900

    Update the next's api template.

--- pages/api/upload.tsx
+++ pages/api/upload.tsx
@@ -1,5 +1,5 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function upload(req: NextApiRequest, res: NextApiResponse) {
   res.send('hello');
 }
