5c8b4d213bf2dabe554d9163dc33efae9a0803b7
Yu Jin Choe <forworkchoe@gmail.com>
2021-09-08 14:48:21 +0900

    Renamed the .env.loc into .env.localhost and Added the .env.local file.

--- pages/examples/api/env.tsx
+++ pages/examples/api/env.tsx
@@ -4,13 +4,15 @@ import type { GetServerSideProps } from 'next';
 export interface PageProp {
   publicEnv: string;
   privateEnv: string;
+  commonEnv: string;
 }
 
-export default function Page({publicEnv, privateEnv}: PageProp) {
+export default function Page({publicEnv, privateEnv, commonEnv}: PageProp) {
   return (
     <div>
       <div>publicEnv=<b>{publicEnv}</b></div>
       <div>privateEnv=<b>{privateEnv}</b></div>
+      <div>commonEnv=<b>{commonEnv}</b></div>
     </div>
   );
 }
@@ -25,7 +27,8 @@ export const getServerSideProps: GetServerSideProps<PageProp> = async () => {
   return {
     props: {
       privateEnv: process.env.NEXT_PUBLIC_PUBLIC as string,
-      publicEnv: process.env.PRIVATE as string
+      publicEnv: process.env.PRIVATE as string,
+      commonEnv: process.env.COMMON_VALUE as string
     }
   };
 };
