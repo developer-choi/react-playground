a3704b54639d91e8ad553009bd8c12a812585081
Yu Jin Choe <forworkchoe@gmail.com>
2021-09-13 20:49:57 +0900

    Added react-toastify example.

---
+++ pages/libraries/react-toastify.tsx
@@ -0,0 +1,19 @@
+import React, {useCallback} from 'react';
+import { Button } from '@components/atom/button/button-presets';
+import {toast} from 'react-toastify';
+
+export default function ReactToastifyPage() {
+  
+  const openDefaultToast = useCallback(() => {
+    toast.info('Default Toast');
+  }, []);
+  const openErrorToast = useCallback(() => {
+    toast.error('Error Toast');
+  }, []);
+  return (
+    <>
+      <Button onClick={openDefaultToast}>Default Toast</Button>
+      <Button onClick={openErrorToast}>Error Toast</Button>
+    </>
+  );
+}
