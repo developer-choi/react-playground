ca09e7ec21387a59f55b2b5b40efa47439fdaa22
Yu Jin Choe <forworkchoe@gmail.com>
2021-09-14 16:25:05 +0900

    Added Twitter Meta Test2.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import OgMeta from '@components/atom/OgMeta';
+import TwitterMeta from '@components/atom/TwitterMeta';
 
 export default function Page() {
   return (
     <>
       <OgMeta
-        title="Custom Title"
         image="/images/next-logo.png"
+        title="Custom Title"
+        description="Custom Description"
+      />
+      <TwitterMeta
+        image="/images/react-logo.png"
+        title="Custom Title"
         description="Custom Description"
       />
       <div>
