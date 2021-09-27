49bab32a6bfc164762776477041dcada9ca77a0e
Yu Jin Choe <forworkchoe@gmail.com>
2021-09-27 22:36:12 +0900

    Removed unused files.

--- src/utils/extend/query-string.ts
+++ src/utils/extend/query-string.ts
@@ -1,5 +1,4 @@
 import type {ParsedUrlQuery} from 'querystring';
-import moment from 'moment';
 import { stringify } from 'querystring';
 
 export type ParsedUrlQueryValue = ParsedUrlQuery['any-key'];
@@ -18,25 +17,6 @@ export function queryStringValueConvertString(value: ParsedUrlQueryValue): strin
     return undefined;
   } else {
     return value;
-  }
-}
-
-/**
- * @param value 쿼리스트링에 들어있던 date문자열 (형식 : yyyymmdd)
- * @return 입력값이 유효하면 그대로 반환, 유효하지 않으면 오늘 날짜를 yyyymmdd 형식으로 변환하여 반환
- */
-export function getYyyymmddOrDefault(value: ParsedUrlQueryValue) {
-  const defaultValue = moment().format('YYYYMMDD');
-  const _value = queryStringValueConvertString(value) ?? '';
-  
-  try {
-    if (moment(_value, 'YYYYMMDD', true).isValid()) {
-      return moment(_value, 'YYYYMMDD').format('YYYYMMDD');
-    } else {
-      return defaultValue;
-    }
-  } catch (error) {
-    return defaultValue;
   }
 }
 
