a71b1cc86bde241516f4af790182f4b0ccfb2f98
최유진 <choe@lioncco.com>
2021-10-07 15:09:25 +0900

    Added the example mounting between pages.

---
+++ pages/examples/api/mounting-between-pages/three.tsx
@@ -0,0 +1,11 @@
+import {MountTestHeader} from '@pages/examples/api/mounting-between-pages/one';
+
+export default function ThreePage() {
+  
+  return (
+    <>
+      <MountTestHeader/>
+      ThreePage
+    </>
+  );
+}
---
+++ pages/examples/api/mounting-between-pages/two.tsx
@@ -0,0 +1,11 @@
+import {MountTestHeader} from '@pages/examples/api/mounting-between-pages/one';
+
+export default function TwoPage() {
+  
+  return (
+    <>
+      <MountTestHeader/>
+      TwoPage
+    </>
+  );
+}
