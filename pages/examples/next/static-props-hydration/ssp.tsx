2292562e5da32ff3795c338238bbb25fd5e70245
Yu Jin Choe <forworkchoe@gmail.com>
2022-06-01 18:15:08 +0900

    Move some pages into another directory.

--- pages/examples/next/static-props-hydration/ssp.tsx
+++ pages/examples/next/static-props-hydration/ssp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Counter, TabMenus, Timer} from '@pages/examples/api/static-props-hydration/sp';
+import {Counter, TabMenus, Timer} from '@pages/examples/next/static-props-hydration/sp';
 
 export default function SSRPage() {
   return (
