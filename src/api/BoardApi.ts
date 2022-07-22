3a73b5e95a3d86279dfd52d373160559622a328c
Yu Jin Choe <forworkchoe@gmail.com>
2022-07-22 18:07:49 +0900

    Add the example of BoardApi

--- src/api/BoardApi.ts
+++ src/api/BoardApi.ts
@@ -1,7 +1,8 @@
 import BaseApi from '@api/BaseApi';
-import type { AxiosResponse } from 'axios';
-import type { BoardListResponse } from '@type/response/board';
-import { getCurrentlyLoginUserInfo } from '@util/auth/auth';
+import type {AxiosResponse} from 'axios';
+import type {BoardListResponse} from '@type/response/board';
+import {getCurrentlyLoginUserInfo} from '@util/auth/auth';
+import type {Board} from '@type/response-sub/board-sub';
 
 export default class BoardApi extends BaseApi {
   constructor() {
@@ -12,8 +13,28 @@ export default class BoardApi extends BaseApi {
     return this.axios.get('/list');
   }
   
-  postSome() {
-    // I have to add a session to the header just before calling the API.
-    return this.axios.post('/some', {}, {headers: getCurrentlyLoginUserInfo()});
+  createBoard({img, boardType, title, content}: CreateBoardParam) {
+    const formData = new FormData();
+    
+    if (img) {
+      formData.append('file', img);
+    }
+    
+    const blob = new Blob([JSON.stringify({
+      boardType,
+      title,
+      content,
+    })], {type: 'application/json'});
+    
+    formData.append('json', blob);
+    
+    return this.axios.post('/create', formData, {headers: {'Content-Type': 'multipart/form-data', ...getCurrentlyLoginUserInfo()}});
   }
+}
+
+/** Naming Rule
+ * [method name] + Param
+ */
+export interface CreateBoardParam extends Pick<Board, 'title' | 'content' | 'boardType'> {
+  img?: File;
 }
