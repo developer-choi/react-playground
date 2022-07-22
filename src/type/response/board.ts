91ad9c4904dc324f210e1d44d54c877da97846ec
Yu Jin Choe <forworkchoe@gmail.com>
2022-07-22 17:31:56 +0900

    Update comments of Board API Type

--- src/type/response/board.ts
+++ src/type/response/board.ts
@@ -1,16 +1,27 @@
 import type {PagingResponse} from '@type/response/common';
-import type {Board, BoardAnother} from '@type/response-sub/board-sub';
+import type {Board, BoardAnother, BoardType} from '@type/response-sub/board-sub';
 
 /** Naming Rule
  * Change path names into Pascal Case + "-Response"
  */
 
+/**
+ * Extracted from the contents of the API document on the BoardController.
+ */
+
 // GET /board/list
 export interface BoardListResponse extends PagingResponse {
-  list: Board[];
+  list: Board[]; //The server responds to an empty array even if there are no posts.
 }
 
 // GET /board/another
 export interface BoardAnothertResponse {
   another: BoardAnother;
+}
+
+export interface BoardResponse {
+  pk: number;
+  boardType: BoardType;
+  dateString: string; //The format is yyyy-mm-dd.
+  img?: string; //If the type has an optional, additional comments are recommended. (ex: An image in a post is not a required value.)
 }
