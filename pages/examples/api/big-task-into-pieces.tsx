7203aadadc7089231eb0b91396bb437c14458558
Yu Jin Choe <forworkchoe@gmail.com>
2022-10-21 20:16:53 +0900

    Add example of Event loop

---
+++ pages/examples/api/big-task-into-pieces.tsx
@@ -0,0 +1,41 @@
+import {Button} from '@component/atom/button/button-presets';
+import {useEffect, useRef} from 'react';
+
+export default function Page() {
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    let i = 0;
+
+    function loop() {
+      if (i >= TOTAL_TASK) {
+        return;
+      }
+
+      setTimeout(loop);
+
+      i++;
+
+      while (i % (TOTAL_TASK / 100) !== 0) {
+        i++;
+      }
+
+      console.log(i);
+
+      if (spanRef.current) {
+        spanRef.current.innerHTML = String(i * 100 / TOTAL_TASK) + '%';
+      }
+    }
+
+    loop();
+  }, []);
+
+  return (
+    <>
+      <span ref={spanRef}></span>
+      <Button onClick={() => alert('Clicked')}>Click Me</Button>
+    </>
+  );
+}
+
+const TOTAL_TASK = 1e9;
