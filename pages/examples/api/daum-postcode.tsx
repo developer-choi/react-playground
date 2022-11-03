07dc63be371e43cc4404c68704ac856519114e68
Yu Jin Choe <forworkchoe@gmail.com>
2022-11-03 19:39:25 +0900

    Add the example of react-daum-postcode

---
+++ pages/examples/api/daum-postcode.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {Button} from '@component/atom/button/button-presets';
+import type {PostcodeSummary} from '@util/custom-hooks/usePostcodePopup';
+import usePostcodePopup from '@util/custom-hooks/usePostcodePopup';
+
+export default function Page() {
+  const [summary, setSummary] = useState<PostcodeSummary>();
+  const open = usePostcodePopup(setSummary);
+
+  return (
+    <>
+      <Button onClick={open}>주소 찾기</Button>
+      {!summary ? null : (
+        <span>({summary.zonecode}) {summary.fullAddress}</span>
+      )}
+    </>
+  );
+}
