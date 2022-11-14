420ff8ef460837256f1cc02c55847525489d7c9f
Yu Jin Choe <forworkchoe@gmail.com>
2022-11-14 20:52:19 +0900

    Add example of react-google-recaptcha

---
+++ pages/examples/libraries/google-recaptcha.tsx
@@ -0,0 +1,30 @@
+import {Button} from '@component/atom/button/button-presets';
+import React, {useState} from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+// https://console.cloud.google.com/security/recaptcha?referrer=search&project=react-playground-1668411260723
+export default function Page() {
+  const [visible, setVisible] = useState(false);
+
+  const onChange = (value: any) => {
+    console.log(value);
+  };
+
+  const toggle = () => {
+    setVisible(prevState => !prevState);
+  };
+
+  return (
+    <>
+      <Button onClick={toggle}>Visible</Button>
+      {!visible ? null :
+        //@ts-ignore
+        <ReCAPTCHA
+          sitekey="6LeMpAUjAAAAAJKRCk6f5kiR4yhNRmNPU4lu5WQM"
+          onChange={onChange}
+          size="normal"
+        />
+      }
+    </>
+  );
+}
