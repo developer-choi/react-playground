6660c161eb6895bb20f94e6db7a477c5dfe91e18
Yu Jin Choe <forworkchoe@gmail.com>
2022-11-19 16:27:38 +0900

    Add example of nested collapsible

---
+++ pages/examples/components/nested-collapsible.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Collapsible, {CollapsibleContentProp, CollapsibleHeaderProp} from '@component/atom/Collapsible';
+import {Button} from '@component/atom/button/button-presets';
+import styled from 'styled-components';
+
+export default function Page() {
+  return (
+    <Collapsible renderHeader={Header} renderContent={Content}>
+      내용<br/>
+      내용<br/>
+      내용<br/>
+      내용<br/>
+      내용<br/>
+      내용<br/>
+      내용<br/>
+      <Collapsible renderHeader={Header2} renderContent={Content}>
+        내용<br/>
+        내용<br/>
+        내용<br/>
+        내용<br/>
+        내용<br/>
+        내용<br/>
+        내용<br/>
+        <Collapsible renderHeader={Header3} renderContent={Content}>
+          내용<br/>
+          내용<br/>
+          내용<br/>
+          내용<br/>
+          내용<br/>
+          내용<br/>
+          내용<br/>
+        </Collapsible>
+      </Collapsible>
+    </Collapsible>
+  );
+}
+
+function Header({onClick, collapsed}: CollapsibleHeaderProp) {
+  return (
+    <Button onClick={onClick}>{collapsed ? '펼치기' : '접기'}</Button>
+  );
+}
+
+function Header2({onClick, collapsed}: CollapsibleHeaderProp) {
+  return (
+    <Button onClick={onClick}>{collapsed ? '2단으로 펼치기' : '접기'}</Button>
+  );
+}
+
+function Header3({onClick, collapsed}: CollapsibleHeaderProp) {
+  return (
+    <Button onClick={onClick}>{collapsed ? '3단으로 펼치기' : '접기'}</Button>
+  );
+}
+
+function Content({style, children}: CollapsibleContentProp) {
+  return (
+    <ContentWrap style={style}>
+      {children}
+    </ContentWrap>
+  );
+}
+
+const ContentWrap = styled.div`
+  font-size: 16px;
+  font-style: italic;
+  background-color: lightblue;
+`;
---
+++ src/component/atom/Collapsible.tsx
@@ -0,0 +1,36 @@
+import type {CSSProperties, MouseEvent, PropsWithChildren, ReactNode} from 'react';
+import React, {useCallback, useState} from 'react';
+
+export interface CollapsibleProp extends PropsWithChildren<{}> {
+  children: ReactNode;
+  renderHeader: (props: CollapsibleHeaderProp) => ReactNode;
+  renderContent: (props: CollapsibleContentProp) => ReactNode;
+  initialCollapsed?: boolean;
+}
+
+export default function Collapsible({children, renderHeader, renderContent, initialCollapsed = true}: CollapsibleProp) {
+  const [collapsed, setCollapsed] = useState(initialCollapsed);
+
+  const toggle = useCallback((event: MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    setCollapsed(prevState => !prevState);
+  }, []);
+
+  return (
+    <>
+      {renderHeader({collapsed, onClick: toggle})}
+      {renderContent({children, collapsed, style: {overflow: 'hidden', height: collapsed ? 0 : 'auto'}})}
+    </>
+  );
+}
+
+export interface CollapsibleHeaderProp {
+  onClick: (event: MouseEvent<HTMLElement>) => void;
+  collapsed: boolean;
+}
+
+export interface CollapsibleContentProp {
+  collapsed: boolean;
+  children: ReactNode;
+  style: CSSProperties;
+}
