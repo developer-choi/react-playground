c37f47a602f0910916ba122eca712bfe2f1ba8b5
Yu Jin Choe <forworkchoe@gmail.com>
2022-11-28 22:40:41 +0900

    resolver 예제 보강

--- pages/examples/libraries/rhf/resolver/resolver-1.tsx
+++ pages/examples/libraries/rhf/resolver/resolver-1.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback} from 'react';
-import {useForm, Resolver, SubmitHandler} from 'react-hook-form';
+import {useForm, Resolver, SubmitHandler, RegisterOptions, SubmitErrorHandler} from 'react-hook-form';
 import Button from '@component/atom/button/Button';
 import {numberWithComma} from '@util/extend/number';
 
@@ -11,12 +11,16 @@ export default function Page() {
    * 2. watch(): 하지만 watch()로 접근할 경우 resolver에서 return한 values가 아니라, 입력된 값 그대로 들어옴. (123456789)
    */
   const onSubmit: SubmitHandler<FormValues> = useCallback((data) => {
-    console.log(data);
+    console.log('SUBMIT', data);
+  }, []);
+
+  const onError: SubmitErrorHandler<FormValues> = useCallback((errors) => {
+    alert(errors.price?.message as string);
   }, []);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('price')}/>
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
+      <input {...register('price', OPTIONS)}/>
       <span>{watch('price')}</span>
       <Button type="submit">제출</Button>
     </form>
@@ -26,6 +30,24 @@ export default function Page() {
 interface FormValues {
   price: string;
 }
+
+/**
+ * resolver를 useForm에 전달한 순간부터 이 OPTIONS는 무시됨.
+ * 넣으나 빼나 의미가없음 동작을 안함.
+ * 입력 아무것도 안하고 제출 해도 제출이 되고,
+ * 최대길이 넘겨서 제출해도 제출 다됨.
+ * 쓰는 순간부터 모든 유효성검증 직접해야함.
+ */
+const OPTIONS: RegisterOptions = {
+  required: {
+    value: true,
+    message: '필수로 입력해야함'
+  },
+  maxLength: {
+    value: 5,
+    message: '5자리 이상 입력하면안됨'
+  }
+};
 
 /**
  * 1. 제출했을 때 1회 호출되고,
