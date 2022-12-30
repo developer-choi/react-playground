e020898a8adc76f07facc6b3ecbb682411c84db9
Yu Jin Choe <forworkchoe@gmail.com>
2022-12-30 21:20:18 +0900

    Add example of fetchQuery()

---
+++ pages/examples/libraries/rq/fetch-query.tsx
@@ -0,0 +1,31 @@
+import Button from '@component/atom/element/Button';
+import {useCallback} from 'react';
+import {useQueryClient} from '@tanstack/react-query';
+import {timeoutPromise} from '@util/extend/promise';
+
+export default function Page() {
+  const queryClient = useQueryClient();
+
+  const callApi = useCallback(async () => {
+    await queryClient.fetchQuery({
+      queryKey: ['some-api'],
+      queryFn: () => api(1),
+      staleTime: 1000
+    });
+  }, [queryClient]);
+
+  return (
+    <div>
+      <Button onClick={callApi}>광클해보기</Button>
+    </div>
+  );
+}
+
+async function api(parameter: number) {
+  console.log('call API', parameter);
+  timeoutPromise(200);
+
+  return {
+    data: 'value'
+  };
+}
