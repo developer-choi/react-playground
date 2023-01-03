d55903123b56648e123b9db4ef3de34b42431720
Yu Jin Choe <forworkchoe@gmail.com>
2023-01-04 08:55:07 +0900

    카톡 공유하기 기능 테스트 2회차

--- pages/libraries/kakao/share.tsx
+++ pages/libraries/kakao/share.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useCallback} from "react";
 import Button from "@component/atom/element/Button";
 import Script from "next/script";
 
 const Page = () => {
   const onClick = () => {
     //@ts-ignore
-    console.log("onClick", window.Kakao);
+    console.log('initialized', window.Kakao.isInitialized());
+  };
+
+  const onLoad = useCallback(() => {
+    //@ts-ignore
+    if (!window.Kakao) {
+      console.error("Kakao not download");
+      return;
+    }
+
+    //@ts-ignore
+    if (!window.Kakao.isInitialized()) {
+      console.log('kakao not initialized');
+      //@ts-ignore
+      window.Kakao.init("04f4aa5c974f27e7a29316e8b5188c0f");
+    }
+
+    //@ts-ignore
+    console.log('initialized', window.Kakao.isInitialized());
+
     //@ts-ignore
     window.Kakao.Share.createDefaultButton({
       container: "#kakaotalk-sharing-btn",
@@ -43,7 +62,7 @@ const Page = () => {
         }
       ]
     });
-  };
+  }, []);
 
   return (
     <div>
@@ -51,10 +70,7 @@ const Page = () => {
       <Script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
         integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
-        crossOrigin="anonymous" onLoad={() => {
-        //@ts-ignore
-        window.Kakao.init("04f4aa5c974f27e7a29316e8b5188c0f");
-      }} />
+        crossOrigin="anonymous" onLoad={onLoad} />
     </div>
   );
 };
