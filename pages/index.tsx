0e17c2c21c07b131225b55061c86529dd79cf67b
Yu Jin Choe <forworkchoe@gmail.com>
2023-02-23 18:59:29 +0900

    event flow 예제 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,5 @@
-import React, {useEffect} from 'react';
-import ImageMap from 'image-map';
-import styled from 'styled-components';
+import React from 'react';
 
 export default function Page() {
-  useEffect(() => {
-    ImageMap('#target', 500);
-  }, []);
-
-  return (
-    <>
-      <Img id="target" useMap="#image-map" src="/images/map.png"/>
-
-      <map name="image-map">
-        <area onClick={() => console.log("1")} coords="131,29,481,145" shape="rect"/>
-        <area onClick={() => console.log("2")} coords="493,28,771,143" shape="rect"/>
-        <area onClick={() => console.log("3")} coords="29,172,260,287" shape="rect"/>
-        <area onClick={() => console.log("4")} coords="270,172,503,288" shape="rect"/>
-        <area onClick={() => console.log("5")} coords="511,173,875,288" shape="rect"/>
-        <area onClick={() => console.log("6")} coords="106,316,496,432" shape="rect"/>
-        <area onClick={() => console.log("7")} coords="503,316,773,432" shape="rect"/>
-      </map>
-    </>
-  );
+  return null;
 }
-
-const Img = styled.img`
-  width: 100%;
-`;
