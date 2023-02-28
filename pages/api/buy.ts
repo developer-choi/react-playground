883c5c9e60f34d7b4a6fefce663786ded38f0a85
Yu Jin Choe <forworkchoe@gmail.com>
2023-02-28 17:39:10 +0900

    Revert "테스트용 API 추가"
    This reverts commit 122073f7baa458114ffb902b39cc6c8f79b2e692.

--- pages/api/buy.ts
+++
@@ -1,7 +0,0 @@
-import type {NextApiRequest, NextApiResponse} from "next";
-
-export default function buySomething(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    headers: req.headers
-  });
-}
