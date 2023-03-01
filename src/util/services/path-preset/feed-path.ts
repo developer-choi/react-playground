4b38b1a1eabb1622de9510f5242f5783c8466320
Yu Jin Choe <forworkchoe@gmail.com>
2023-03-01 21:47:15 +0900

    feed-path 삭제

--- src/util/services/path-preset/feed-path.ts
+++
@@ -1,9 +0,0 @@
-const upperPath = '/feed';
-
-const FEED_PATH = {
-  library: upperPath + '/library',
-  history: upperPath + '/history',
-  subscriptions: upperPath + '/subscription'
-};
-
-export default FEED_PATH;
--- src/util/services/path-preset/root-path.ts
+++ src/util/services/path-preset/root-path.ts
@@ -1,10 +1,8 @@
 import CHANNEL_PATH from '@util/services/path-preset/channel-path';
-import FEED_PATH from '@util/services/path-preset/feed-path';
 import BOARD_PATH from '@util/services/path-preset/board-path';
 
 const ROOT_PATH = {
   channel: CHANNEL_PATH,
-  feed: FEED_PATH,
   board: BOARD_PATH
 };
 
