a87b53df64abf7c6f9da085dfe2f37e802d78646
Yu Jin Choe <forworkchoe@gmail.com>
2023-03-07 22:08:57 +0900

    Add loading button solution example

--- src/component/atom/element/Button.tsx
+++ src/component/atom/element/Button.tsx
@@ -1,6 +1,75 @@
-import React from 'react';
+import React, {
+  ComponentPropsWithoutRef,
+  forwardRef,
+  MouseEvent,
+  MouseEventHandler,
+  Ref,
+  useCallback,
+  useState
+} from 'react';
 import {COLORS} from '@util/services/style/theme';
 import styled, {css} from 'styled-components';
+import {myClassName} from '@util/libraries/classnames';
+
+interface ButtonProp extends ComponentPropsWithoutRef<'button'> {
+  onClickWithLoading?: (event: MouseEvent<HTMLButtonElement>) => Promise<void> | void;
+  loading?: boolean;
+}
+
+export default forwardRef(function Button(props: ButtonProp, ref: Ref<HTMLButtonElement>) {
+  const {type = 'button', className, loading, onClick, onClickCapture, onClickWithLoading, ...rest} = props;
+
+  const [loadingState, setLoadingState] = useState(false);
+  const haveLoadingHandler = !!onClickWithLoading;
+  const isLoadingResult = haveLoadingHandler ? loadingState : loading;
+
+  const customClickHandler = useCallback((handler: MouseEventHandler<HTMLButtonElement> | undefined) => {
+    if (isLoadingResult) {
+      return undefined;
+    }
+
+    return async (event: MouseEvent<HTMLButtonElement>) => {
+      //Case1. loading props, onClickWithLoading 둘 다 있을 수는 없음.
+      if (loading === true && haveLoadingHandler) {
+        throw Error('Cannot use onClickWithLoading props and loading props. Use only one of the two.');
+      }
+
+      //Case2. onClickWithLoading 이 없으면 loading에 의한 동작
+      if (!haveLoadingHandler) {
+        if (loading === true) {
+          return;
+        }
+
+        handler?.(event);
+        return;
+      }
+
+      //Case3. onClickLoading이 있으면, custom loading state로 동작해야함.
+      setLoadingState(true);
+
+      try {
+        await onClickWithLoading(event);
+
+      } finally {
+        setLoadingState(false);
+      }
+    };
+  }, [haveLoadingHandler, isLoadingResult, loading, onClickWithLoading]);
+
+  const clickHandler = customClickHandler(onClick);
+  const clickCaptureHandler = customClickHandler(onClickCapture);
+
+  return (
+    <StyledButton
+      ref={ref}
+      type={type}
+      onClick={clickHandler}
+      onClickCapture={clickCaptureHandler}
+      className={myClassName({loading: isLoadingResult}, className)}
+      {...rest}
+    />
+  );
+});
 
 const buttonHoverEffect = css`
   //hover, active, disabled는 이 순서로 선언이 되야 opacity가 직관적으로 작동함.
@@ -31,7 +100,7 @@ const grayColorTemplate = css`
   background: ${COLORS.gray1};
 `;
 
-const Button = styled.button`
+const StyledButton = styled.button`
   padding: 8px 15px;
   min-width: 90px;
   border-radius: 5px; //size에는 border-radius도 포함되고,
@@ -73,6 +142,8 @@ const Button = styled.button`
   &.full {
     width: 100%;
   }
+  
+  &.loading {
+    cursor: progress;
+  }
 `;
-
-export default Button;
