e5071b7bf4f0019e39c342a30a127041b9e79fa5
Yu Jin Choe <forworkchoe@gmail.com>
2023-03-22 09:02:35 +0900

    Click Outside 예제 추가

--- pages/experimental/click-outside/bug2.tsx
+++ pages/experimental/click-outside/bug2.tsx
@@ -19,12 +19,18 @@ export default function Page() {
     console.log('submitted.');
   }, []);
 
-  /**
+  /** 버그1
    * 1. 인풋창에 포커스 주면 박스가 생김
    * 2. 이 때 인풋창에 커서둔상태로 엔터치면
    * 3. 박스가 사라짐.
    *
    * 원인: 인풋박스에 엔터치면, 제출버튼이 클릭됨. (서브밋버튼이라)
+   */
+
+  /** 버그2
+   * 1. 인풋창에 포커스 주면 박스가 생김
+   * 2. 박스 안에 Click Me 버튼을 클릭
+   * 3. 박스가 사라짐.
    */
   return (
     <>
@@ -33,9 +39,29 @@ export default function Page() {
         <Button type="submit">제출</Button>
       </form>
       {!visible ? null : (
-        <Box ref={wrapperRef}/>
+        <Box ref={wrapperRef}>
+          <SelfHiddenButton/>
+        </Box>
       )}
     </>
+  );
+}
+
+/**
+ * 주로 삭제버튼을 생각하며 예제를 만들었음.
+ * 최근검색어 목록을 보여주면서, 항목마다 X버튼을 보여주고,
+ * X버튼을 누르면 그 항목이 사라지는 예제를 가정하고 만들었음.
+ * 실제로는 삭제콜백함수를 onClick props로 받음.
+ */
+function SelfHiddenButton() {
+  const {value: visible, setFalse: closeButton} = useToggle(true);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <button type="button" onClick={closeButton}>Click Me</button>
   );
 }
 
