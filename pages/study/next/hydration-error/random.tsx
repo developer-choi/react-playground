348f8a74f423abebb526a6b9965a5980969302c8
Yu Jin Choe <forworkchoe@gmail.com>
2023-03-28 21:42:59 +0900

    hydration error 예제페이지 중 하나를 해결한 예제페이지 추가

--- pages/study/next/hydration-error/random.tsx
+++ pages/study/next/hydration-error/random.tsx
@@ -2,6 +2,12 @@ import React, {useMemo} from 'react';
 import type {GetServerSideProps} from 'next';
 import {shuffleArray} from '@util/extend/random';
 
+/**
+ * 관리자에서 랜덤노출을 설정하면,
+ * 화면에서 새로고침 할 때마다 랜덤하게 나열순서가 변경되야하는기능을 구현하다가
+ * Hydration error를 겪어서 예제를 확보했음.
+ */
+
 // URL: http://localhost:3000/study/next/hydration-error/random
 interface PageProp {
   nameList: string[];
