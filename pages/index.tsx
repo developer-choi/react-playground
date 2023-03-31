35a25af78320ddfdc9c20106f4b4260d88269360
Yu Jin Choe <forworkchoe@gmail.com>
2023-03-31 16:40:19 +0900

    fill은 wrapper에 가로세로크기있어야됨 안그러면 안나옴

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Image from "next/image";
+import styled from "styled-components";
 
 export default function Page() {
-  return null;
+  return (
+    <Wrapper>
+      <Image
+        src="https://oksite.kr/storage/images/banner/ebppI8N5VV8AzTabdVQdKBnPUj2qcYrIMDTmKDqa.jpg"
+        alt="배너이미지"
+        layout="fill"
+      />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  
+  > * {
+    border: 5px solid red;
+  }
+`;
