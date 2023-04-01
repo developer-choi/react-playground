efb6a28bb4e2648b25aa830350980c674f3c7c47
Yu Jin Choe <forworkchoe@gmail.com>
2023-04-01 22:46:39 +0900

    Revert "fill은 wrapper에 가로세로크기있어야됨 안그러면 안나옴"
    This reverts commit 35a25af78320ddfdc9c20106f4b4260d88269360.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
 import React from 'react';
-import Image from "next/image";
-import styled from "styled-components";
 
 export default function Page() {
-  return (
-    <Wrapper>
-      <Image
-        src="https://oksite.kr/storage/images/banner/ebppI8N5VV8AzTabdVQdKBnPUj2qcYrIMDTmKDqa.jpg"
-        alt="배너이미지"
-        layout="fill"
-      />
-    </Wrapper>
-  );
+  return null;
 }
-
-const Wrapper = styled.div`
-  position: relative;
-  
-  > * {
-    border: 5px solid red;
-  }
-`;
