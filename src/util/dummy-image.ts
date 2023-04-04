1a24bfa4e56ba64d1f65787eecd78e046811c97d
Yu Jin Choe <forworkchoe@gmail.com>
2023-04-04 09:42:31 +0900

    Image priority 예제 추가

--- src/util/dummy-image.ts
+++ src/util/dummy-image.ts
@@ -10,5 +10,16 @@ export const IMAGE_LIST = {
     'https://oksite.kr/storage/images/banner/YoTmEhHQYaC5COBi0DIe5gI3ctAsCUkNNdI9vvpz.jpg',
     'https://oksite.kr/storage/images/banner/9uPUPcW3PLZLpWGPlhcIlNUzBEMq4LPElBpWeyur.jpg',
     'https://oksite.kr/storage/images/banner/D1YZ9XAOyDACo0T14DzlrWfo1fHVDNgJm48pRE2Q.jpg',
+  ],
+
+  //923 x 383 정도 되는 큰 메인배너 이미지
+  bigRectangular: [
+    'https://oksite.kr/storage/images/banner/ebppI8N5VV8AzTabdVQdKBnPUj2qcYrIMDTmKDqa.jpg',
+    'https://oksite.kr/storage/images/banner/SsL18aWY4ZFsdNersfTxO0wbTSM6gJkrIjr38gP3.jpg',
+    'https://oksite.kr/storage/images/banner/Vo1rSbo7fJVdDPMYYgms6H978yeAu57HlL0ffO6W.jpg',
+    'https://oksite.kr/storage/images/banner/RCNKAaSYNMwBKz673pX8WPl9GAoaOgIGmnx70QLw.jpg',
+    'https://oksite.kr/storage/images/banner/Wn7SaBqT9v7q76kljD0M4k8dHJbkTLKfUHal91kx.jpg',
+    'https://oksite.kr/storage/images/banner/XaEEeLibLWGMe74z2REOzzB3DvsrgwUUVUMiVnSD.jpg',
+    'https://oksite.kr/storage/images/banner/Vlj3FpzdflLhZD2hAdFC2amOsrPoDfgnTvsOIwCc.jpg',
   ]
 };
