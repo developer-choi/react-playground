c3eaca950269326ec77ff4adcab651a955fcd4c3
Yu Jin Choe <forworkchoe@gmail.com>
2023-04-11 20:08:01 +0900

    client side에서도 예상하지못한 API가 호출되는 버그 예제 추가

---
+++ pages/study/rq/with-server/both-side.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import type {GetServerSideProps} from 'next';
+import {dehydrate, DehydratedPageProps, QueryClient, useQuery} from '@tanstack/react-query';
+
+/**
+ * server side에서 받아온 데이터를
+ * client side에서 재사용하기위해 useQuery로 가져오려는데,
+ * client side에서 getApi()를 또 호출하는 잘못된 예제를 남김.
+ */
+
+// URL: http://localhost:3000/study/rq/with-server/both-side
+export default function Page() {
+  const {data} = useQuery(queryOption);
+
+  return (
+    <div>
+      {data}
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<DehydratedPageProps> = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(queryOption);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient)
+    }
+  };
+};
+
+const queryOption = {
+  queryKey: ['both-side'],
+  queryFn: getApi,
+  // staleTime: 1000
+  // staleTime: 100
+};
+
+async function getApi() {
+  console.log('Api called.');
+  return new Date().getTime();
+}
