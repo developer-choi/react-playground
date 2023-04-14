e4098ef0ee0ae9b26ebc2180e4c40d8c49782b3d
Yu Jin Choe <forworkchoe@gmail.com>
2023-04-14 17:47:35 +0900

    scroll-restoration 코드 정리

--- src/util/extend/scroll-restoration.ts
+++ src/util/extend/scroll-restoration.ts
@@ -1,18 +1,53 @@
 import {useRouter} from 'next/router';
 import {useEffect} from 'react';
 
-const scrollRestorationCache: Record<string, {left: number, top: number}> = {
+interface ScrollPosition {
+  top: number;
+  left: number;
+}
 
-};
+function getCacheManager() {
+  const positionHistoryCache: Record<string, ScrollPosition> = {
 
-let beforePopStateAsPath: string | undefined;
+  };
+
+  let beforePopStateAsPath: string | undefined;
+
+  return {
+    //뒤로가기 또는 앞으로가기 했을 때 실행될 콜백함수
+    onBeforePopState: (asPath: string) => {
+      beforePopStateAsPath = asPath;
+    },
+
+    //현재 입장한 페이지가 뒤로가기 / 앞으로가기 해서 온 페이지인지 아니면 링크눌러서 온 페이지인지
+    isMatchBackOrForward: (currentAsPath: string) => {
+      return currentAsPath === beforePopStateAsPath;
+    },
+
+    onRouteChangeComplete: () => {
+      beforePopStateAsPath = undefined;
+    },
+
+    setCacheInCurrentAsPath: (currentAsPath: string, position: ScrollPosition) => {
+      positionHistoryCache[currentAsPath] = position;
+    },
+
+    popCacheInCurrentAsPath: (currentAsPath: string) => {
+      const cache = positionHistoryCache[currentAsPath];
+      delete positionHistoryCache[currentAsPath];
+      return cache;
+    }
+  };
+}
+
+const cacheManager = getCacheManager();
 
 export function useScrollRestoration() {
   const router = useRouter();
 
   useEffect(() => {
     router.beforePopState(({as}) => {
-      beforePopStateAsPath = as;
+      cacheManager.onBeforePopState(as);
       return true;
     });
   }, [router]);
@@ -28,20 +63,21 @@ export function useScrollRestoration() {
        * 뒤로가기해서 나가든, 그냥 눌러서 나가든, 무조건 set하긴해야함.
        * 그래야 추후에 다시 돌아올 때, 앞으로가기해서 돌아오든 할 때 스크롤보낼 수 있으니까.
        */
-      scrollRestorationCache[thisAsPath] = {
+      cacheManager.setCacheInCurrentAsPath(thisAsPath, {
         top: window.scrollY,
         left: window.scrollX
-      };
+      });
     };
 
     const routeChangeCompleteHandler = () => {
-      if (scrollRestorationCache[thisAsPath] && beforePopStateAsPath === thisAsPath) {
-        const {top, left} = scrollRestorationCache[thisAsPath];
+      const cache = cacheManager.popCacheInCurrentAsPath(thisAsPath);
+
+      if (cache && cacheManager.isMatchBackOrForward(thisAsPath)) {
+        const {top, left} = cache;
         setTimeout(() => window.scrollTo({top, left, behavior: 'smooth'}), 0);
-        delete scrollRestorationCache[thisAsPath]
       }
 
-      beforePopStateAsPath = undefined;
+      cacheManager.onRouteChangeComplete();
       window.history.scrollRestoration = 'manual'; //이거 안쓰면 2회차부터 뒤로갈 때 목적지에서 스크롤 움직여버리는데 명확하게 원인설명안됨.
     };
 
