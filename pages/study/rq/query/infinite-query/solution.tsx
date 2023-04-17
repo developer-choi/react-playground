169d171e1c6f7a0097e1ae20ff9c9923fab1ff4d
Yu Jin Choe <forworkchoe@gmail.com>
2023-04-17 10:22:59 +0900

    rc-virtual-list 기반으로 infinite scroll 구현하려다 스크롤복원이 동작안해서 포기한 예제 추가

--- pages/study/rq/query/infinite-query/solution.tsx
+++ pages/study/rq/query/infinite-query/solution.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {UIEvent, useCallback} from 'react';
 import {QueryKey, useInfiniteQuery} from '@tanstack/react-query';
 import type {QueryFunctionContext} from '@tanstack/query-core/build/lib/types';
 import type {Course} from '@type/response-sub/course-sub';
@@ -6,6 +6,16 @@ import CourseApi from '@api/CourseApi';
 import styled from 'styled-components';
 import {flexCenter} from '@util/services/style/css';
 import {useInfiniteScroll} from '@util/extend/event/scroll';
+import List from 'rc-virtual-list';
+
+/**
+ * rc-virtual-list 문제점
+ * 1. 커스텀 스크롤바를 사용했기 때문에, 스크롤복원관련 아무런 동작을 못시킴.
+ * 2. 반드시 height를 지정해줘야했음. 이거 지정안해주면 virtual list 동작을 안함.
+ * ㄴ 이 height는 container의 view port상에 보이는 높이인거같은데,
+ * ㄴ 상품리스트 필터 처럼 컨테이너 높이가 고정되는 경우에는 딱 쓸수있는데
+ * ㄴ 상품리스트 처럼 뷰포트만큼 늘어나는경우 높이를 고정시킬 수 없음.
+ */
 
 // URL: http://localhost:3000/study/rq/query/infinite-query/solution
 export default function Page() {
@@ -28,6 +38,18 @@ export default function Page() {
     enabled: !disabledNextPage
   });
 
+  const onScroll = useCallback((event: UIEvent<HTMLElement>) => {
+    if (disabledNextPage) {
+      return;
+    }
+
+    const {scrollHeight, clientHeight, scrollTop} = event.target as HTMLElement;
+
+    if ((scrollHeight - clientHeight - scrollTop) <= 500) {
+      fetchNextPage();
+    }
+  }, [disabledNextPage, fetchNextPage]);
+
   if (!data) {
     return null;
   }
@@ -36,9 +58,11 @@ export default function Page() {
 
   return (
     <>
-      {list.map(({pk, title}) => (
-        <Row key={pk}>{title}</Row>
-      ))}
+      <List data={list} itemHeight={200} fullHeight height={1289} itemKey="pk" onScroll={onScroll}>
+        {({title}) => (
+          <Row>{title}</Row>
+        )}
+      </List>
     </>
   );
 }
