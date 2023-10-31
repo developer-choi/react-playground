a10ae274e6829ac8092a7b909138473ea8794afc
Yu Jin Choe <forworkchoe@gmail.com>
2023-11-01 07:13:08 +0900

    Update register() example

--- pages/study/rhf/resgister/unregister.tsx
+++ pages/study/rhf/resgister/unregister.tsx
@@ -8,7 +8,7 @@ import {useDispatchOpenModal} from '@store/reducers/modal';
 // import {baseHandleErrors} from '@util/extend/react-hook-form';
 
 /**
- * http://localhost:3000/study/rhf/bug/unregister
+ * http://localhost:3000/study/rhf/register/unregister
  *
  * 버그 재현방법
  * 1. 위 URL로 접속
