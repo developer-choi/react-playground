232433eabf8133310f3574df06bcb9507b5f4288
Yu Jin Choe <forworkchoe@gmail.com>
2024-03-21 21:13:30 +0900

    날짜값 변환할 때 month가 실제 month와 1차이나는 부분을 잘못수정한 부분 원복

--- src/util/extend/date/date-convert.ts
+++ src/util/extend/date/date-convert.ts
@@ -13,6 +13,7 @@ export interface DestructedDate {
   milliSecond: number;
 }
 
+// 주의 : month는 실제 month보다 1 작습니다. 0부터 시작하기 때문입니다.
 export function destructDate(target = new Date()): DestructedDate {
   const year = target.getFullYear();
   const month = target.getMonth();
@@ -21,15 +22,17 @@ export function destructDate(target = new Date()): DestructedDate {
   const minute = target.getMinutes();
   const second = target.getSeconds();
   const milliSecond = target.getMilliseconds();
-  return { year, month, date, hour, minute, second, milliSecond };
+  return {year, month, date, hour, minute, second, milliSecond};
 }
 
 /**
  * @param target 지정한 Date객체의 (기본값 : 함수가 호출된 현재에 대한 Date객체)
  * @return year, month, date, hour, minute, second, milliSecond 순서대로 저장된 배열을 반환합니다.
- * @example (new Date('2021-04-03 12:34:56')) => [2021, 4, 3, 12, 34, 56, 0]
+ * @example (new Date('2021-04-03 12:34:56')) => [2021, 3, 3, 12, 34, 56, 0]
+ *
+ * 주의 : month는 실제 month보다 1 작습니다. 0부터 시작하기 때문입니다.
  */
 export function getDatePropertyArray(target = new Date()): [number, number, number, number, number, number, number] {
   const {year, month, date, hour, minute, second, milliSecond} = destructDate(target);
-  return [year, month + 1, date, hour, minute, second, milliSecond];
+  return [year, month, date, hour, minute, second, milliSecond];
 }
