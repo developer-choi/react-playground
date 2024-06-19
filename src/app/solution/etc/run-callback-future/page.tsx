5489162dee88ce17f52deed2ca968ad8b84dac9d
Yu Jin Choe <forworkchoe@gmail.com>
2024-06-20 08:43:12 +0900

    미래의 원하는 시점에 원하는 로직 실행하는 로직 예제 추가

---
+++ src/app/solution/etc/run-callback-future/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, {useCallback, useState} from 'react';
+import {getDiffDate} from '@/utils/extend/date/util';
+import {useRunCallbackInFuture} from '@/utils/extend/date/future';
+
+// URL: http://localhost:3000/solution/etc/run-callback-future
+export default function Page() {
+  const [message, setMessage] = useState(`The callback is scheduled to run in ${SECONDS} seconds.`);
+
+  const changeMessage = useCallback(() => {
+    setMessage('The callback is executed');
+  }, []);
+
+  // Run the callback function after {SECONDS} seconds.
+  useRunCallbackInFuture(FUTURE, changeMessage);
+
+  return (
+    <div>
+      {message}
+    </div>
+  );
+}
+
+const SECONDS = 5;
+const FUTURE = getDiffDate(new Date(), [0, 0, 0, 0, 0, SECONDS]).getTime();
---
+++ src/utils/extend/date/future.ts
@@ -0,0 +1,57 @@
+import {getDiffDate} from '@/utils/extend/date/util';
+import {useEffect} from 'react';
+
+/**
+ * @param futureTimestamp 미래의 timestamp
+ * @param callback 미래에 실행할 콜백
+ * @return NodeJS.Timeout
+ * @description 미래에, 전달한 timestamp가 되면, callback을 실행합니다.
+ * 반드시, 필요없는 경우 clearTimeout를 호출해야합니다.
+ */
+export function runCallbackInFuture(futureTimestamp: number, callback: () => void) {
+  const timeout = futureTimestamp - new Date().getTime();
+
+  if (timeout < 0) {
+    console.warn('futureTimestamp is not more future than present. The callback is not executed.');
+    return;
+  }
+
+  return setTimeout(callback, timeout);
+}
+
+/**
+ * 내일 자정이 되면 전달받은 callback을 실행합니다.
+ * 그 외 내용은 runCallbackInFuture()와 동일합니다.
+ */
+export function runCallbackInMidnight(callback: () => void) {
+  const midnight = getDiffDate(new Date(), [0, 0, 1]);
+  return runCallbackInFuture(midnight.getTime(), callback);
+}
+
+/**
+ * runCallbackInFuture() 에서 이 hooks가 호출된 이후 unmount 될 때 clearTimeout()를 추가한 함수입니다.
+ * 그 외 내용은 동일합니다.
+ */
+export function useRunCallbackInFuture(futureTimestamp: number, callback: () => void) {
+  useEffect(() => {
+    const timeoutId = runCallbackInFuture(futureTimestamp, callback);
+    
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [callback, futureTimestamp]);
+}
+
+/**
+ * runCallbackInMidnight() 에서 이 hooks가 호출된 이후 unmount 될 때 clearTimeout()를 추가한 함수입니다.
+ * 그 외 내용은 동일합니다.
+ */
+export function useRunCallbackInMidnight(callback: () => void) {
+  useEffect(() => {
+    const timeoutId = runCallbackInMidnight(callback);
+
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [callback]);
+}
