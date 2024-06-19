a38dc3cea4614d142d0468a13796115f51583e19
Yu Jin Choe <forworkchoe@gmail.com>
2024-06-20 08:51:44 +0900

    LinkOrAnchor 예제 추가

---
+++ src/app/solution/etc/link-or-anchor/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import LinkOrAnchor from '@/components/element/LinkOrAnchor';
+
+// URL: http://localhost:3000/solution/etc/link-or-anchor
+export default function Page() {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', gap: 12}}>
+      <LinkOrAnchor href="https://www.naver.com/">
+        https://www.naver.com
+      </LinkOrAnchor>
+      <LinkOrAnchor href="http://localhost:3000">
+        http://localhost:3000
+      </LinkOrAnchor>
+      <LinkOrAnchor href="http://localhost:3000/solution/etc/link-or-anchor">
+        http://localhost:3000/solution/etc/link-or-anchor
+      </LinkOrAnchor>
+      <LinkOrAnchor href="/solution/etc/link-or-anchor">
+        /solution/etc/link-or-anchor
+      </LinkOrAnchor>
+      <LinkOrAnchor href={undefined}>
+        undefined href
+      </LinkOrAnchor>
+    </div>
+  );
+}
---
+++ src/components/element/LinkOrAnchor.tsx
@@ -0,0 +1,98 @@
+'use state';
+
+import React, {ComponentPropsWithoutRef, useEffect, useState} from 'react';
+import Link, {LinkProps} from 'next/link';
+
+/**
+ * href: FULL URL (관리자페이지에서 무언가를 등록할 때 (이미지배너 등) 연결될 URL을 입력하게되는데 그 때 입력되는 FULL URL
+ * @example ("https://www.naver.com") ==> <a href="https://www.naver.com" target="입력값">으로 작동
+ * @example ("https://some.production.com/some/directory") ==> <Link href="/some/directory">으로 작동
+ * @example ("") ==> <a>으로 작동
+ */
+export interface LinkOrAnchorProps extends Omit<ComponentPropsWithoutRef<'a'>, 'href'>, Pick<LinkProps, 'prefetch'> {
+  href: string | undefined | null;
+}
+
+export default function LinkOrAnchor({prefetch, href, target, rel, ...rest}: LinkOrAnchorProps) {
+  const originLink = useOriginLink(href ?? '');
+
+  if (originLink === 'initial' || !originLink.href) {
+    return <a {...rest} />;
+  }
+
+  if (originLink.isOurOrigin) {
+    return (
+      <Link href={originLink.href} prefetch={prefetch} {...rest}/>
+    );
+  }
+
+  const innerProp = target !== undefined ?
+    {target, rel}
+    :
+    {target: '_blank', rel: 'noreferrer'};
+
+  return (
+    <a href={originLink.href} target={innerProp.target} rel={innerProp.rel} {...rest}/>
+  );
+}
+
+function useOriginLink(href: string) {
+  const [originLink, setOriginLink] = useState<'initial' | {href: string, isOurOrigin: boolean}>('initial');
+
+  useEffect(() => {
+    if(href.startsWith('/')) {
+      setOriginLink({
+        isOurOrigin: true,
+        href
+      });
+      return;
+    }
+
+    try {
+      //이 값은 항상 origin 맽 끝에 /가 없음
+      const {origin} = new URL(href);
+
+      if (origin !== location.origin) {
+        setOriginLink({
+          isOurOrigin: false,
+          href
+        });
+        return;
+      }
+
+      /**
+       * 이걸 안하면, link가 http://localhost:3000일 때
+       * 결과적으로 <Link href="" 이렇게 되서 현재 페이지로 데이터만 새로고침된다.
+       * 하지만 http://localhost:3000의 의미는 이 Origin의 root page를 가리키는 url이기 때문에,
+       * 직접 이렇게 작성했다.
+       */
+      if (origin === href || origin + '/' === href) {
+        setOriginLink({
+          isOurOrigin: true,
+          href: '/'
+        });
+        return;
+      }
+
+      setOriginLink({
+        isOurOrigin: true,
+        href: href.replaceAll(origin, '')
+      });
+
+    } catch (error) {
+      const originalMessage = (error as Error).message;
+      const message = `${originalMessage}
+Expected URL (example): https://some.domain.com/some/path
+Link(Parameter): ${href === '' ? '(empty string)' : href}
+    `;
+      console.warn(message);
+
+      setOriginLink({
+        isOurOrigin: false,
+        href: ''
+      });
+    }
+  }, [href]);
+
+  return originLink;
+}
