49bb0abaae7cb70a44125c1e35f9d7ae0f1c5faa
Yu Jin Choe <forworkchoe@gmail.com>
2024-07-22 20:27:30 +0900

    Revert "PC / Mobile 분리할 수 있는 2가지 방법 추가"
    This reverts commit 2bad9f883e7260505e35f866f1d672a8ccb6cb9b.

--- src/utils/extend/library/next.ts
+++ src/utils/extend/library/next.ts
@@ -1,19 +1,3 @@
-import UAParser from 'ua-parser-js';
-
 export function isServer() {
-  return typeof window === 'undefined';
+  return typeof window === undefined;
 }
-
-export function isMobileOnBothSide(): boolean {
-  const userAgent = isServer() ? require('next/headers').headers().get('user-agent') : navigator.userAgent;
-
-  if (!userAgent) {
-    return false;
-  }
-
-  const parseResult = new UAParser(userAgent);
-  return MOBILE_TYPE.includes(parseResult.getDevice().type as any);
-}
-
-// 태블릿은 모바일로 간주
-const MOBILE_TYPE = ['mobile', 'tablet'];
