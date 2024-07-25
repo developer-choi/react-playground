9e724d66d62e3755182abebba8464fd90d6c363e
Yu Jin Choe <forworkchoe@gmail.com>
2024-07-25 10:41:20 +0900

    trimObject() 내부로직 개선

--- src/utils/extend/data-type/object.ts
+++ src/utils/extend/data-type/object.ts
@@ -33,30 +33,104 @@ export function itemListToDataOfType<T extends string>(itemList: NameValueItem<T
 }
 
 /**
- * 자식의 자식의 자식까지 전부 뒤져서 string이면 trim()함
+ * loopRecursively()에 제공된 value값이 만약 객체일 경우, callback으로 그 객체의 키값까지 같이 전달됩니다.
+ * 객체안에 객체가 들어있는 경우, 부모 객체와 자식 객체의 key값을 .으로 구분해서 전달됩니다.
+ *
+ * loopRecursively({key1: 'a', key2: {subKey: 'b'}}) 만약 이렇게 호출한다면,
+ * callback으로는 {key: 'key1', value: 'a'}, {key: 'key2.subKey', value: 'b'} 이렇게 전달됩니다.
  */
-export function trimObject<T>(value: T): T {
-  if (Array.isArray(value)) {
-    return value.map(item => trimObject(item)) as T;
+export type ConvertCallback = (parameter: {key?: string, value: any}) => any;
 
-  } else if (typeof value === 'string') {
-    return value.trim() as T;
+/**
+ * value를 순회 돌면서, Array 또는 중첩된 Object를 계속 재귀 순회하면서 value값만 바꾸는 함수입니다.
+ * @param value 임의의 값이 올 수 있지만, object를 권장함
+ * @param convertCallback value의 키값마다 저장된 값을 변환할 콜백함수, 변환하지 않을 값이면 callback의 parameter를 그대로 반환해야함.
+ *
+ * @description convertCallback에서 전달되는 key, value로 내가 바꾸려고 하는 값인지를 체크하는 방법은,
+ * 1. typeof 연산자를 value에 사용해서 체크할 수도 있고, (하단 trimObject() 확인)
+ * 2. key값 / value값을 비교해서 체크할 수도 있습니다.
+ */
+function loopRecursively<V>(value: V, convertCallback: ConvertCallback): any {
+  function recursive(value: V, convertCallback: ConvertCallback, parentKey?: string): any {
+    if (Array.isArray(value)) {
+      return value.map(item => recursive(item, convertCallback, parentKey)) as V;
 
-  } else if (typeof value !== 'object') {
-    return value;
+    } else if (typeof value !== 'object' || value === null) {
+      return convertCallback({key: parentKey, value});
 
-  } else {
-    return Object.fromEntries(Object.entries(value as Object).map(([key, value]) => {
-      if (typeof value === 'object') {
-        return [key, trimObject(value)];
-      } else if (typeof value === 'string') {
-        return [key, value.trim()];
-      } else {
-        return [key, value];
-      }
-    })) as T;
+    } else {
+      return Object.fromEntries(Object.entries(value).map(([keyInObject, valueInObject]) => {
+        const key = parentKey ? `${parentKey}.${keyInObject}` : keyInObject;
+
+        if (typeof valueInObject === 'object' || valueInObject === null) {
+          return [keyInObject, recursive(valueInObject, convertCallback, key)];
+        } else {
+          return [keyInObject, convertCallback({key, value: valueInObject})];
+        }
+      })) as V;
+    }
   }
+
+  return recursive(value, convertCallback);
 }
+
+/**
+ * 기능 설명은 loopRecursively()로 대체합니다.
+ *
+ * 1. convertCallback은, 전달된 value와 동일한 data type을 반환해야하며
+ * 2. 1번으로 인해 이 함수의 반환타입은 전달된 value와 동일한 type임을 보장합니다. (제네릭까지 셋팅)
+ */
+export function safeLoopRecursivelyObject<O extends object>(object: O, convertCallback: ConvertCallback): O {
+  const wrappedCallback: ConvertCallback = (parameter) => {
+    const result = convertCallback(parameter);
+
+    if (typeof parameter.value !== typeof result) {
+      throw new Error('The converted value type is different than before.');
+    }
+
+    return result;
+  }
+
+  return loopRecursively(object, wrappedCallback);
+}
+
+/**
+ * 자식의 자식의 자식까지 전부 뒤져서 string이면 trim()함
+ * @example ({key1: ' 1 ', key2: [' 2 '], key3: {subKey: ' 3 '}}) ==> {key1: '1', key2: ['2'], key3: {subKey: '3'}}
+ */
+export function trimObject<O extends object>(value: O): O {
+  return safeLoopRecursivelyObject(value, function ({value}) {
+    if (typeof value === 'string') {
+      return value.trim();
+    } else {
+      return value;
+    }
+  });
+}
+
+/*
+const test = {
+  key1: 1,
+  key2: ' 1, 2, 3 ',
+  key3: function () {
+    console.log('hello world');
+  },
+  key4: null,
+  key5: undefined,
+  key6: [1, ' 1 2 3 ', 4],
+  key7: {
+    key1: 1,
+    key2: ' 1, 2, 3 ',
+    key3: function () {
+      console.log('hello world');
+    },
+    key4: null,
+    key5: undefined,
+    key6: [1, ' 1 2 3 ', 4],
+  }
+};
+trimObject(test);
+ */
 
 /*************************************************************************************************************
  * Exported interfaces
