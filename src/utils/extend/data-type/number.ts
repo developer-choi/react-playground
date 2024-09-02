efc980de83b1fe8b897a875c2209e2fdfc5241ed
Yu Jin Choe <forworkchoe@gmail.com>
2024-09-02 22:07:00 +0900

    number, string 모듈 추가

---
+++ src/utils/extend/data-type/number.ts
@@ -0,0 +1,45 @@
+/**
+ * @example (1234.1234) => "1,234.1234"
+ */
+export function numberWithComma(value: number | string): string {
+  const _value = typeof value === 'string' ? value : value.toString();
+  const parts = _value.split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
+}
+
+/**
+ * @return from, to 사이의 자연수를 배열로 만들어 반환. from, to의 경계도 포함.
+ * @example (-1, 4) => [-1, 0, 1, 2, 3, 4]
+ * @example (4, -1) => [4, 3, 2, 1, 0, -1]
+ */
+export function range(from: number, to: number): number[] {
+  const max = Math.max(from, to);
+  const min = Math.min(from, to);
+  const length = max - min + 1;
+
+  if (from > to)
+    return [...new Array(length).keys()].map(num => num + min).reverse();
+
+  else
+    return [...new Array(length).keys()].map(num => num + min);
+}
+
+export function calculateTotal<T>(list: T[], callback: (item: T) => number) {
+  return list.reduce((a, b) => {
+    return a + callback(b);
+  }, 0);
+}
+
+export function getAbsoluteValueWithSign(value: number) {
+  const absoluteValue = Math.abs(value);
+  const signType = value >= 0 ? 'positive' : 'negative';
+
+  return {
+    signType, // 클래스에 전달해서 양/음 스타일링 할 때 사용
+
+    // 화면에 렌더링 할 때 사용
+    absoluteValue, // 그중 숫자 부분만 따로 형식을 바꿀 때 사용 (3자리 마다 콤마를 끊거나 등)
+    sign: signType === 'positive' ? '+' : '-',
+  };
+}
--- src/utils/extend/data-type/string.ts
+++ src/utils/extend/data-type/string.ts
@@ -1,3 +1,8 @@
+// 특히 시간 표시할 때 엄청 많이 씀.
+export function padToTwoDigits(value: string | number) {
+  return (typeof value === 'number' ? value.toString() : value).padStart(2, '0');
+}
+
 // sortByNumber와 활용해서 많이나온 키워드로 정렬해서 보여줄 수 있음.
 export function classifyStrings(array: string[]): {value: string, count: number}[] {
   return Object.entries(array.reduce((a, b) => {
