96eecfd1df2dbf0f7cd6555a00d7ca1d5e46555e
Yu Jin Choe <forworkchoe@gmail.com>
2024-09-13 05:42:23 +0900

    Input에서 Success일 때 아이콘 노출추가

--- src/components/form/Input/index.tsx
+++ src/components/form/Input/index.tsx
@@ -3,13 +3,15 @@ import classNames from 'classnames';
 import {ComponentPropsWithRef, forwardRef, MouseEvent, ReactNode, Ref, useCallback} from 'react';
 import styles from './index.module.scss';
 import {isReactElement} from '@/utils/extend/library/react';
+import CheckIcon from '@/components/icon/CheckIcon';
 
 export interface InputProps extends ComponentPropsWithRef<"input">, Omit<FormElementWrapperProps, "kind"> {
   rightRender?: ReactNode; // 주로 아이콘 (패스워드 인풋의 eye 아이콘 등) 혹은 텍스트로 된 단위 (원, 달러, 포인트 등)
   hiddenErrorMessage?: boolean; // 에러테두리는 표시하고싶은데 에러메시지는 미노출하고싶은 경우 전달 (ex: 주소폼처럼 여러개의 <Input이 셋트로 나오는 경우)
+  success?: boolean; // <Input
 }
 
-export default forwardRef(function Input({label, error, rightRender, style, className, hiddenErrorMessage, disabled, ...rest}: InputProps, ref: Ref<HTMLInputElement>) {
+export default forwardRef(function Input({label, error, rightRender, style, className, hiddenErrorMessage, disabled, success, ...rest}: InputProps, ref: Ref<HTMLInputElement>) {
   /**
    * disabled일 때는, 인풋을 포함해서 테두리영역 안에있는 아이콘까지 전부다 클릭했을 때 반응이 없어야함.
    */
@@ -28,6 +30,7 @@ export default forwardRef(function Input({label, error, rightRender, style, clas
       <div className={classNames(styles.innerContainer, {[styles.error]: error})} onClickCapture={onClickCapture}>
         <input ref={ref} className={styles.input} disabled={disabled} {...rest} />
         {_rightRender}
+        {!success ? null : <CheckIcon fill="green"/>}
       </div>
     </FormElementWrapper>
   );
