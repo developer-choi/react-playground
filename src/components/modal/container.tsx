02e9baed7e8749db7313c7f8e4ad590ef9747c05
Yu Jin Choe <forworkchoe@gmail.com>
2024-09-20 07:56:14 +0900

    dialog가 떴을 때 포커스받을 수 잇는 첫 요소에 포커스 안가게 막는 기능 추가

--- src/components/modal/container.tsx
+++ src/components/modal/container.tsx
@@ -10,6 +10,7 @@ export interface ModalContainerProps extends EssentialModalProps, Omit<Component
   easilyClose?: boolean; // backdrop / esc 눌러서 모달 닫게 해주는 기능, default false
   type?: 'centerAlign' | 'bottomSheet' | 'fullScreen'; // default centerAlign
   size?: 'large' | 'medium'; // centerAlign 모달에서만 사용함
+  disableOpenFocus?: boolean; // open 시 젤 위에있는 포커스받을 수 있는 요소에 포커스 가는 기능 비활성화 default false
 
   /**
    * bottomSheet 모달에서는 boolean 으로 전달, 나머지는 전달할 필요는 없지만 만약 전달한 경우, 값에 따라 동작됨.
@@ -26,7 +27,7 @@ export interface ModalContainerProps extends EssentialModalProps, Omit<Component
 // Alert, Confirm 등 모달 확장할 때 이 타입을 확장하기
 export type ComposedModalProps = Omit<ModalContainerProps, 'type' | 'size'>;
 
-export function ModalContainer({className, size = 'medium', type = 'centerAlign', easilyClose, onClose, open, ...rest}: ModalContainerProps) {
+export function ModalContainer({className, size = 'medium', type = 'centerAlign', easilyClose, onClose, open, disableOpenFocus = false, ...rest}: ModalContainerProps) {
   const modalRef = useRef<HTMLDialogElement>(null);
   /**
    * bottomSheet면 기본값 false로 동작하고
@@ -82,6 +83,7 @@ export function ModalContainer({className, size = 'medium', type = 'centerAlign'
   // https://docs.google.com/document/d/16-Z3RmslEMvhfwOMmePYTRg4HkUjKjWSxxF2SB7NGGU/edit#heading=h.dpm2y2x8qp1i
   return createPortal((
     <dialog ref={modalRef} className={classNames(styles.rootModalContainer, styles[type + 'Container'], {[styles.open]: _open})} onClick={easilyClose ? onClose : undefined}>
+      {!disableOpenFocus ? null : (<button className={styles.disableOpenFocus}/>)}
       <div className={classNames(styles.rootModalInner, styles[type + 'Inner'], styles[size], className)} onClick={onClickBackdrop} {...rest} />
     </dialog>
   ), document.body);
