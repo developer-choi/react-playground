f689790cab71dd359639066adec05a826977b1b6
Yu Jin Choe <forworkchoe@gmail.com>
2024-09-25 20:00:06 +0900

    Period Timer의 종료일이 과거이거나, 시작일이 미래인 케이스 대응 완료

---
+++ src/app/(example)/staging/timer/diff/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import {useDiffTimer} from '@/utils/extend/timer';
+import Button from '@/components/element/Button';
+
+/**
+ * URL: http://localhost:3000/staging/timer/diff
+ * Doc: https://docs.google.com/document/d/1HJkytM33ADDc_vcZvV1AHfslX8NLdTigCGe8cSR-gE8/edit#heading=h.bxmojislumzh
+ *
+ * 요구조건 > 렌더링횟수가 합리적이어야함. 같은 state값으로 두번 따닥 리렌더링되고 그런일 없어야함.
+ */
+export default function Page() {
+  return (
+    <CertificationTimer/>
+  );
+}
+
+function CertificationTimer() {
+  const timer = useDiffTimer({
+    diffSeconds: 3,
+    beforeStart: () => {
+      // clearErrors('phone_number');
+    },
+    onTerminated: () => {
+      // setError('phone_number', {
+      //   type: 'timeout',
+      //   message: '인증 시간이 초과되었습니다. 재전송 해주세요',
+      // });
+      console.log('종료!!');
+    },
+  });
+
+  console.log(timer);
+
+  return (
+    <div>
+      <div>{timer.status}</div>
+      <Button onClick={timer.start}>인증번호 전송</Button>
+      <span>{timer.dateText}</span>
+      <Button onClick={timer.initialize}>인증번호 확인</Button>
+    </div>
+  )
+}
--- src/utils/extend/timer.ts
+++ src/utils/extend/timer.ts
@@ -1,6 +1,7 @@
 import {useCallback, useEffect, useRef, useState} from 'react';
 import dayjs from 'dayjs';
 import {usePrevious} from '@/utils/extend/library/react';
+import {runCallbackInFuture} from '@/utils/extend/date/future';
 
 export interface DiffTimerOption {
   onTerminated?: () => void;
@@ -56,12 +57,16 @@ export interface PeriodTimerParam {
     startTimestamp: number;
     endTimestamp: number;
   };
-  format?: typeof defaultPeriodTimerFormat;
+  proceedingFormat?: typeof defaultPeriodTimerProceedingFormat;
+  futureFormat?: (remain: ReturnType<typeof calculateRemainTime>, futureTimestamp: number) => string;
 }
 
 export interface PeriodTimerResult {
   isProceeding: boolean;
-  dateText: '' | string; // 진행중이 아니면 빈문자열, ex: '2일 12 : 37 : 00'
+  isFuture: boolean;
+  isTerminated: boolean; // true인 경우는 종료일이 과거인 경우임. 즉, 시작일이 미래인 경우에는 false임.
+  proceedingText: '' | string; // 진행중이 아니면 빈문자열, ex: '2일 12 : 37 : 00'
+  futureText: '' | string; // 진행중이 아니면 빈문자열, ex: '01-22 OPEN'
 }
 
 /**
@@ -71,29 +76,86 @@ export interface PeriodTimerResult {
  * @param format 일, 시, 분, 초 값으로 문자열을 반환해야하는 함수
  * @return isProceeding 진행중 여부 (아직 시작안했거나, 종료된 경우 false)
  *
+ * 예외케이스 2가지
+ * 1. 종료일이 현재보다 작은경우 (과거에 이미 종료된 케이스) ==> 계속 종료상태
+ * 2. 시작일이 현재보다 큰 경우 (미래에 시작될 케이스) ==> 시작일이 된 시점에 타이머 시작
  */
-export function usePeriodTimer({period, format = defaultPeriodTimerFormat}: PeriodTimerParam): PeriodTimerResult {
+export function usePeriodTimer(param: PeriodTimerParam): PeriodTimerResult {
+  const {
+    period,
+    proceedingFormat = defaultPeriodTimerProceedingFormat,
+    futureFormat = defaultPeriodTimerFutureFormat
+  } = param;
+  const timeoutId = useRef<NodeJS.Timeout>();
   const currentTimestamp = Date.now();
-  const enabled = !period
-    ? false
-    : currentTimestamp > period.startTimestamp && currentTimestamp < period.endTimestamp;
+  const periodState = getPeriodState(currentTimestamp, period);
+
+  // 처음부터 period가 존재하는 케이스
+  const initialEnabled = !period ? false : periodState === 'proceeding';
+  const [enabled, setEnabled] = useState(initialEnabled);
+
+  useEffect(() => {
+    if (periodState === 'past') {
+      return;
+    }
+
+    /**
+     * 처음부터 period가 존재하지않아서 (API로 부터 기간을 받아오는 케이스)
+     * 나중에 리렌더링되고 나서야 effect가 실행되는 케이스
+     */
+    if (periodState === 'proceeding') {
+      setEnabled(true);
+      return;
+    }
+
+    // future case는 최초 렌더링시점, 또는 나중에 리렌더링 시점에 미래인 경우, 미래에 도달한 시점에 타이머가 실행될 수 있도록 콜백함수 예약
+    timeoutId.current = runCallbackInFuture(period?.startTimestamp as number, function() {
+      setEnabled(true);
+    });
+
+    return () => {
+      clearTimeout(timeoutId.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [periodState]);
 
   const {snapshotTimestamp, status} = useExpiredTimer({
     expiredTimestamp: !enabled ? 0 : period!.endTimestamp,
     enabled
   });
 
-  const result = (!enabled || status !== 'proceeding' || !period) ? null : calculateRemainTime(snapshotTimestamp, period.endTimestamp);
+  const proceedingResult = (!enabled || status !== 'proceeding' || !period) ? null : calculateRemainTime(snapshotTimestamp, period.endTimestamp);
+  const futureResult = periodState !== 'future' ? null : calculateRemainTime(currentTimestamp, period?.startTimestamp as number);
 
   return {
     isProceeding: status === 'proceeding',
-    dateText: !result ? '' :  format(result)
+    isFuture: periodState === 'future',
+    isTerminated: periodState === 'future' ? false : status === 'terminated',
+    proceedingText: !proceedingResult ? '' :  proceedingFormat(proceedingResult),
+    futureText: !futureResult ? '' : futureFormat(futureResult, period?.startTimestamp as number)
   };
 }
 
 /*************************************************************************************************************
  * Non Export
  *************************************************************************************************************/
+// 오직 시작일, 종료일 기간으로만 계산하는 Period의 State
+function getPeriodState(currentTimestamp: number, period: PeriodTimerParam['period']) {
+  if (!period) {
+    return null;
+  }
+
+  if (currentTimestamp > period.endTimestamp) {
+    return 'past';
+  }
+
+  if (currentTimestamp < period.startTimestamp) {
+    return 'future';
+  }
+
+  return 'proceeding';
+}
+
 function defaultDiffTimerFormat(remainSeconds: number) {
   return `${Math.floor(remainSeconds / 60)}:${String(remainSeconds % 60).padStart(2, '0')}`;
 }
@@ -116,8 +178,17 @@ function calculateRemainTime(currentTimestamp: number, endTimestamp: number) {
   };
 }
 
-function defaultPeriodTimerFormat({days, hours, minutes, seconds}: ReturnType<typeof calculateRemainTime>) {
+function defaultPeriodTimerProceedingFormat({days, hours, minutes, seconds}: ReturnType<typeof calculateRemainTime>) {
   return `${days}일 ${hours.toString().padStart(2, '0')} : ${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
+}
+
+/**
+ * 총 2가지 스타일의 [미래 시작 전] 날짜 표기가능.
+ * 1. 잔여시간 표기 (n일 n시 n분 n초 뒤 시작된다) ==> 1st parameter
+ * 2. 날짜 표기 (MM월 DD일에 시작한다) ==> 2nd parameter
+ */
+function defaultPeriodTimerFutureFormat(_: ReturnType<typeof calculateRemainTime>, futureTimestamp: number) {
+  return dayjs(futureTimestamp).format('MM-DD') + ' START';
 }
 
 interface ExpiredTimerParam {
