29dfcc0c219233eea83ce13f102fb16ba16f37dd
Yu Jin Choe <forworkchoe@gmail.com>
2024-09-27 04:55:05 +0900

    UserFieldCountApiRequest 주석 수정

--- src/utils/service/api/user.ts
+++ src/utils/service/api/user.ts
@@ -3,10 +3,25 @@ import {customFetchOnClientSide} from '@/utils/extend/library/fetch';
 export interface UserFieldCountApiRequest {
   type: 'email' | 'nick_name';
   value: string;
-  validationMode: 'exist' | 'does-not-exist'; // exist: 있는게 정상, 그 반대는 없는게 정상.
 
   /**
-   * 차단회원 제외할지 여부.
+   * TODO 아래 2개로 가지말고 그냥 validationMode를 ‘find’ | ‘signup’ 이렇게 잡으면 안되나?
+   *
+   * find
+   * 1. 있는게 정상
+   * 2. 탈퇴회원은 미포함 (탈퇴회원으로 계정을 찾게해주면 안됨)
+   *
+   * signup
+   * 1. 없는게 정상
+   * 2. 탈퇴회원은 포함 (탈퇴한 계정의 이메일로 새로 가입이 가능하게 해야함)
+   * - 아 물론 이건 사이트마다 약간 다를 수 있긴하지만.
+   *
+   * 어쨌든 이렇게 하면, onlyActiveUser param 필요가 없어짐.
+   */
+  validationMode: 'exist' | 'does-not-exist'; // exist: 있는게 정상, 그 반대는 없는게 정상.
+
+  /** TODO Legacy
+   * 탈퇴회원 제외할지 여부.
    * 회원가입할 때는 아이디 조회하는 경우 탈퇴한 아이디로 못만들게 해야하지만, (count 계산 시 포함시켜서 count > 0인 값이 응답되게 해야)
    * 비번찾기할 때 아이디 조회하는 경우, 탈퇴한 아이디로는 못찾게 해야함. (count 계산 시 미포함시켜서 count === 0 되게)
    * 기본값 false
