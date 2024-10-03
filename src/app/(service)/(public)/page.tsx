34b378c65654e1accf271f25bf04b36bb300454a
Yu Jin Choe <forworkchoe@gmail.com>
2024-10-03 09:57:55 +0900

    라디오버튼에 generic 추가한것에 대한 예제페이지 추가

--- src/app/(service)/(public)/page.tsx
+++ src/app/(service)/(public)/page.tsx
@@ -1,3 +1,61 @@
+'use client';
+
+import Radio, {RadioGroup, RadioProps} from '@/components/form/Radio';
+import {SubmitErrorHandler, SubmitHandler, useForm} from 'react-hook-form';
+import {useCallback} from 'react';
+import designSystemStyles from '@/styles/design-system.module.scss';
+import Button from '@/components/element/Button';
+import {baseHandleErrors} from '@/utils/extend/library/react-hook-form';
+
 export default function Home() {
-  return null;
+  const {galaxyTabProps, ipadProProps, onSubmit} = useGiftForm();
+
+  return (
+    <form className={designSystemStyles.commonForm} onSubmit={onSubmit}>
+      <RadioGroup>
+        <Radio {...galaxyTabProps}/>
+        <Radio {...ipadProProps}/>
+      </RadioGroup>
+      <Button isSubmit>제출버튼</Button>
+    </form>
+  );
+}
+
+// 사은품 폼
+function useGiftForm() {
+  const {register, handleSubmit} = useForm<GiftFormData>();
+
+  const ipadProProps: RadioProps<GiftType> = {
+    ...register('type', {
+      required: '사은품 상품 선택은 필수입니다'
+    }),
+    value: 'ipad-pro',
+  };
+
+  const galaxyTabProps: RadioProps<GiftType> = {
+    ...register('type', {
+      required: '사은품 상품 선택은 필수입니다'
+    }),
+    value: 'galaxy-tab',
+  };
+
+  const onError: SubmitErrorHandler<GiftFormData> = useCallback(({type}) => {
+    baseHandleErrors([type]);
+  }, []);
+
+  const onSubmit: SubmitHandler<GiftFormData> = useCallback(async ({type}) => {
+    alert(`선택한 상품 > ${type}`);
+  }, []);
+
+  return {
+    ipadProProps,
+    galaxyTabProps,
+    onSubmit: handleSubmit(onSubmit, onError)
+  };
+}
+
+type GiftType = 'ipad-pro' | 'galaxy-tab';
+
+interface GiftFormData {
+  type: GiftType;
 }
