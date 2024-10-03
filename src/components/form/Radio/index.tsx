6f3f17f84256143c6fe88b86d88818a0d910f3ae
Yu Jin Choe <forworkchoe@gmail.com>
2024-10-03 09:46:49 +0900

    체크박스 / 라디오버튼에 generic 추가

--- src/components/form/Radio/index.tsx
+++ src/components/form/Radio/index.tsx
@@ -6,7 +6,7 @@ import {CheckboxProps} from '@/components/form/Checkbox';
 import {FormElementWrapper, FormElementWrapperProps} from '@/components/form/form-elements';
 
 // https://docs.google.com/document/d/1rqmOi11-M61mjOkk5i63SigHNN8BJYLpka8c73tSLb0/edit?usp=drivesdk
-export default forwardRef(function Radio(props: CheckboxProps, ref: ForwardedRef<HTMLInputElement>) {
+export default forwardRef(function Radio<T extends string>(props: CheckboxProps<T>, ref: ForwardedRef<HTMLInputElement>) {
   const {label, color = 'primary', style, ...rest} = props;
 
   return (
