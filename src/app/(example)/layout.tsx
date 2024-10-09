7f6f623d1cb7893fca7174e702f88daab1548020
Yu Jin Choe <forworkchoe@gmail.com>
2024-10-09 18:58:24 +0900

    example 최상위 레이아웃에 AppProvider를 추가하고 그 하위에서 Modal이나 ReactQuery의 Provider를 감싸던 layout 모두 삭제

--- src/app/(example)/layout.tsx
+++ src/app/(example)/layout.tsx
@@ -1,12 +1,10 @@
 import {PropsWithChildren} from 'react';
-import dynamic from 'next/dynamic';
-const ModalProvider = dynamic(() => import('@/components/setting/ModalProvider'), {ssr: false});
+import AppProvider from '@/components/setting/AppProvider';
 
 export default function Layout({children}: PropsWithChildren) {
-  // 원래는 이게 Service Layout에 들어가야함.
   return (
-    <ModalProvider>
+    <AppProvider>
       {children}
-    </ModalProvider>
+    </AppProvider>
   );
 }
