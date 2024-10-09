306684371536423089efe644c74a4f9a0e2f2708
Yu Jin Choe <forworkchoe@gmail.com>
2024-10-09 19:53:04 +0900

    Streaming을 위한 react-query 셋팅 변경

--- src/components/setting/ReactQueryProvider.tsx
+++ src/components/setting/ReactQueryProvider.tsx
@@ -1,41 +1,12 @@
 'use client';
 
-import {isServer, QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {PropsWithChildren} from 'react';
+import React, {PropsWithChildren} from 'react';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import {getQueryClient} from '@/utils/extend/library/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
 
-// https://tanstack.com/query/latest/docs/framework/react/guides/advanced-ssr
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
+// Doc: [RQ 셋팅방법] https://docs.google.com/document/d/1lpn4oe63VfJEbsuNp0WdgWRypdtHQWkdrJzGdFcu7Ws/edit
 export default function ReactQueryProvider({children}: PropsWithChildren) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
   const queryClient = getQueryClient();
 
   return (
