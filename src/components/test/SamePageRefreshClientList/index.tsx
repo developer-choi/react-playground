bdda8207288d7f037bfe0bb06c347c2a15a95df7
Yu Jin Choe <forworkchoe@gmail.com>
2024-10-16 12:39:35 +0900

    한 페이지 안에서 최신화가 발생해야하는 페이지 예제 추가

---
+++ src/components/test/SamePageRefreshClientList/index.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import {BoardRow} from '@/types/services/board';
+import {useCallback} from 'react';
+import {deleteBoardApi, patchBoardApi, postBoardApi} from '@/utils/service/api/board';
+import {revalidateTagOnServerAction} from '@/utils/extend/library/revalidate-tag';
+import dayjs from 'dayjs';
+import Button from '@/components/element/Button';
+
+export interface SamePageRefreshClientListProps {
+  list: BoardRow[];
+}
+
+export default function SamePageRefreshClientList({list}: SamePageRefreshClientListProps) {
+  const post = useCallback(async () => {
+    await postBoardApi({
+      title: `(created) - ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`,
+      type: 'free'
+    });
+    await revalidateTagOnServerAction('boardList');
+  }, []);
+
+  const deleteBoard = useCallback(async (pk: number) => {
+    await deleteBoardApi(pk);
+    await revalidateTagOnServerAction('boardList');
+  }, []);
+
+  const patchBoard = useCallback(async (pk: number) => {
+    await patchBoardApi({
+      pk,
+      title: `(modified) - ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`,
+      type: 'gallery'
+    });
+    await revalidateTagOnServerAction('boardList');
+  }, []);
+
+  return (
+    <>
+      <Button size="large" onClick={post}>Post</Button>
+      {list.map(({pk, title}) => (
+        <div key={pk} style={{display: 'flex', alignItems: 'center', gap: 8, marginTop: 12}}>
+          <h2>{title}</h2>
+          <Button color="secondary" size="small" onClick={() => deleteBoard(pk)}>Delete</Button>
+          <Button color="secondary" size="small" onClick={() => patchBoard(pk)}>Patch</Button>
+        </div>
+      ))}
+    </>
+  );
+}
