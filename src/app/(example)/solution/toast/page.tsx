bf0606e003da7e091839bca98ea4f9fb5be8e66d
Yu Jin Choe <forworkchoe@gmail.com>
2024-11-07 13:21:10 +0900

    토스트 1개만 뜨도록 하는 테스트 페이지 추가

--- src/app/(example)/solution/toast/page.tsx
+++ src/app/(example)/solution/toast/page.tsx
@@ -3,17 +3,24 @@
 import {useCallback} from 'react';
 import {snackBar} from '@/components/toast';
 import {snackBarProfile} from '@/components/toast/SnackBarProfile';
+import Button from '@/components/element/Button';
 
 // URL: http://localhost:3000/solution/toast
 // Doc : https://docs.google.com/document/d/1Z78cJFrCNLm1rFeMIA7y64o6E84yJ4H_hNCVCdjztCM/edit#heading=h.4h61jm8200i1
 export default function Page() {
-  const onClick = useCallback(() => {
-    snackBar('aaaaaaaaaaaaaaaaaaaaaaaaaaaa', {
+  const onClickWithClose = useCallback(() => {
+    snackBar('hello world'.repeat(5), {
       closeIcon: true
     });
-    snackBar('Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World', {
-      closeIcon: true
+  }, []);
+
+  const onClickWithoutClose = useCallback(() => {
+    snackBar('hello world'.repeat(5), {
+      closeIcon: false
     });
+  }, []);
+
+  const onClickProfile = useCallback(() => {
     snackBarProfile({
       name: 'Kevin',
       message: 'Hello, How are you?',
@@ -22,6 +29,10 @@ export default function Page() {
   }, []);
 
   return (
-    <button onClick={onClick}>Default (text)</button>
+    <>
+      <Button onClick={onClickWithClose}>Text + close</Button>
+      <Button onClick={onClickWithoutClose}>Text - close</Button>
+      <Button onClick={onClickProfile}>Profile</Button>
+    </>
   );
 }
