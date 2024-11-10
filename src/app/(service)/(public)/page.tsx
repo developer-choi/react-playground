1a6aaca76bd8415f5a5427e9d0928fd6db847ce1
Yu Jin Choe <forworkchoe@gmail.com>
2024-11-10 15:15:54 +0900

    RHF로 file 연동하면 생기는 한계 1

--- src/app/(service)/(public)/page.tsx
+++ src/app/(service)/(public)/page.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import {SubmitErrorHandler, SubmitHandler, useForm} from 'react-hook-form';
+import {ChangeEvent, useCallback} from 'react';
+import {baseHandleErrors} from '@/utils/extend/library/react-hook-form';
+import Button from '@/components/element/Button';
+
+/** 재현방법 > 파일 하나 넣고 제출해보기
+ * 한계 1. event.target.value = '' 하면, setValue 하더라도 required options에 걸린다.
+ * - https://stackoverflow.com/questions/12030686/html-input-file-selection-event-not-firing-upon-selecting-the-same-file#answer-60886920 이거 때문에 event.target.value = ''는 필수인데.
+ * - 근데 또 watch()로 값을 찍어보면 찍히긴 함...
+ */
 export default function Home() {
-  return null;
+  const {register, handleSubmit, setValue, watch} = useForm<FileFormData>();
+
+  const onError: SubmitErrorHandler<FileFormData> = useCallback(({file}) => {
+    baseHandleErrors([file]);
+  }, []);
+
+  const onSubmit: SubmitHandler<FileFormData> = useCallback(async (data) => {
+    console.log('data', data.file);
+  }, []);
+
+  const fileProps = register('file', {
+    onChange: (event: ChangeEvent<HTMLInputElement>) => {
+      setValue('file', event.target.files?.item(0) ?? undefined);
+      event.target.value = '';
+    },
+    required: '필수입니다.'
+  });
+
+  console.log('file name=', watch('file')?.name);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
+      <input type="file" {...fileProps}/>
+      <Button type="submit">Submit</Button>
+    </form>
+  );
+}
+
+interface FileFormData {
+  file: File | undefined;
 }
