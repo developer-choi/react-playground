8fe5972ad6188370ce62fbc1f0f751d9d505358a
Yu Jin Choe <forworkchoe@gmail.com>
2025-01-02 00:16:59 +0900

    dnd 로직 별도 분리

--- src/app/(example)/study/drag-sort-list/page.tsx
+++ src/app/(example)/study/drag-sort-list/page.tsx
@@ -1,53 +1,32 @@
 'use client';
 
-import {useCallback, useState, DragEvent} from 'react';
+import {useState} from 'react';
 import styles from './page.module.scss';
-import {preventDefault} from '@/utils/extend/event';
+import {useSortListWithDragAndDrop} from '@/utils/extend/useSortListWithDragAndDrop';
 
 // URL: http://localhost:3000/study/drag-sort-list
-// Blog: https://simian114.gitbook.io/blog/undefined/react/drag-and-drop
+// Doc: https://docs.google.com/document/d/1WY2nU73zxkzLqcn9NCMA9rhLrKW7049fyJD7nJioYms/edit?tab=t.0
 export default function Page() {
   const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9].map(value => ({data: value.toString().repeat(10)})));
-
-  const onDragStartCallback = useCallback((index: number) => {
-    return (event: DragEvent<HTMLDivElement>) => {
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.setData('text/plain', index.toString());
-    };
-  }, []);
-
-  const onDropCallback = useCallback((droppedIndex: number) => {
-    return (event: DragEvent<HTMLDivElement>) => {
-      const droppingIndex = Number(event.dataTransfer.getData('text/plain'));
-      const rect = (event.target as HTMLDivElement).getBoundingClientRect();
-      const isDroppedAboveMiddle = event.clientY < rect.top + rect.height / 2;
-
-      setItems(list => {
-        const droppingItem = list[droppingIndex];
-
-        return list.reduce((a, b, currentIndex) => {
-          if (currentIndex === droppedIndex) {
-            if (isDroppedAboveMiddle) {
-              return a.concat(droppingItem).concat(b);
-            } else {
-              return a.concat(b).concat(droppingItem);
-            }
-          }
-
-          if (currentIndex !== droppingIndex) {
-            return a.concat(b);
-          }
-
-          return a;
-        }, [] as {data: string}[]);
-      });
-    };
-  }, []);
+  const {onDragStartCallback, onDropCallback, ...rest} = useSortListWithDragAndDrop({
+    list: items,
+    onChange: {
+      all: setItems,
+    }
+  });
 
   return (
     <div className={styles.container}>
       {items.map((item, index) => (
-        <div key={index} className={styles.item} draggable onDragStart={onDragStartCallback(index)} onDragOver={preventDefault} onDrop={onDropCallback(index)}>{item.data}</div>
+        <div
+          key={index}
+          className={styles.item}
+          onDragStart={onDragStartCallback(index)}
+          onDrop={onDropCallback(index)}
+          {...rest}
+        >
+          {item.data}
+        </div>
       ))}
     </div>
   );
---
+++ src/utils/extend/useSortListWithDragAndDrop.ts
@@ -0,0 +1,62 @@
+import {preventDefault} from '@/utils/extend/event';
+import {HTMLAttributes, useCallback, DragEvent} from 'react';
+
+export interface SortListWithDragAndDropParam<T> {
+  list: T[];
+  onChange: {
+    all: (list: T[]) => void
+  } | {
+    moveIndex: (fromIndex: number, toIndex: number) => void // react-hook-form의 useFieldArray의 move()를 대응하기위함. 예시는 PFM routine에 있음.
+  };
+}
+
+export interface SortListWithDragAndDropResult<E> extends Pick<HTMLAttributes<E>, 'draggable' | 'onDragOver'> {
+  onDragStartCallback: (index: number) => (event: DragEvent<E>) => void;
+  onDropCallback: (index: number) => (event: DragEvent<E>) => void;
+}
+
+export function useSortListWithDragAndDrop<T, E>({onChange, list}: SortListWithDragAndDropParam<T>): SortListWithDragAndDropResult<E> {
+  const onDragStartCallback = useCallback((index: number) => {
+    return (event: DragEvent<E>) => {
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('text/plain', index.toString());
+    };
+  }, []);
+
+  const onDropCallback = useCallback((droppedIndex: number) => {
+    return (event: DragEvent<E>) => {
+      const droppingIndex = Number(event.dataTransfer.getData('text/plain'));
+      const rect = (event.target as HTMLDivElement).getBoundingClientRect();
+      const isDroppedAboveMiddle = event.clientY < rect.top + rect.height / 2;
+
+      const droppingItem = list[droppingIndex];
+
+      if ('all' in onChange) {
+        onChange.all(list.reduce((a, b, currentIndex) => {
+          if (currentIndex === droppedIndex) {
+            if (isDroppedAboveMiddle) {
+              return a.concat(droppingItem).concat(b);
+            } else {
+              return a.concat(b).concat(droppingItem);
+            }
+          }
+
+          if (currentIndex !== droppingIndex) {
+            return a.concat(b);
+          }
+
+          return a;
+        }, [] as T[]));
+      } else if('moveIndex' in onChange) {
+        onChange.moveIndex(droppingIndex, droppedIndex);
+      }
+    };
+  }, [list, onChange]);
+
+  return {
+    draggable: true,
+    onDragOver: preventDefault,
+    onDragStartCallback,
+    onDropCallback,
+  };
+}
