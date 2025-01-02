c72b8c78d34c2e0baa72ef24556f7a61a4c389de
Yu Jin Choe <forworkchoe@gmail.com>
2025-01-03 06:54:45 +0900

    아이콘 최신화

--- src/components/icon/CloseIcon.tsx
+++ src/components/icon/CloseIcon.tsx
@@ -1,6 +1,6 @@
 import {ComponentPropsWithoutRef} from 'react';
 
-export default function CloseIcon({fill = 'white', ...rest}: ComponentPropsWithoutRef<'svg'>) {
+export default function CloseIcon({fill = '#646464', ...rest}: ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
       <path
