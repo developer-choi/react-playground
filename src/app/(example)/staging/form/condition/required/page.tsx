8f9ef27e9a892ceb872b04610fa3aa0399ebe889
Yu Jin Choe <forworkchoe@gmail.com>
2025-01-06 17:47:41 +0900

    API기준 required condition form에서 에러피드백 정확하게 되는거 예제 추가

--- src/app/(example)/staging/form/condition/required/page.tsx
+++ src/app/(example)/staging/form/condition/required/page.tsx
@@ -7,28 +7,29 @@ import Radio, {RadioGroup} from '@/components/form/Radio';
 import Button from '@/components/element/Button';
 import designSystemStyles from '@/styles/design-system.module.scss';
 import classNames from 'classnames';
+import {useMutation} from '@tanstack/react-query';
+import {timeoutPromise} from '@/utils/extend/test/promise';
 
 /**
  * URL: http://localhost:3000/staging/form/condition/required
  * Doc: https://docs.google.com/document/d/1AZ0lzPYG-m-BuQ-EVXaloZ5JDKj28cDK8vUpt9LG-V0/edit?tab=t.0#heading=h.ylvvpqoeiqfq
  *
- * 테스트 방법 > 그냥 제출버튼 누르면 됨. 셋팅 다 해놨음.
+ * 테스트 방법 1. Client Side에서 체크하는 케이스
+ * (1) defaultValues중에 아무거나 빈문자열로 셋팅하고,
+ * (2) 그 빈문자열이 셋팅된 step과 반대의 step을 선택한 후 제출 눌렀을 때
+ * (3) 빈문자열이 있는 step으로 바뀌고 & 문제가 되는 인풋으로 포커스 이동 & 스크롤 이동 & 에러메시지 표시가 되는지 확인.
  *
- * 테스트 환경)
- * step1의 1번째 항목만 입력 안시켜놓고
- * step2 보는 상태로
- * 제출을 하면
- * step1 선택되면서
- * 문제가 되는 항목으로 포커스가 이동됨.
+ * 테스트 방법 2. API에서 체크 후 특정 인풋에 문제가 있다고 응답되는 경우
+ * (1) 그냥 바로 제출 누르면 됨. 이에 맞게 기본값 미리 다 defaultValues에 셋팅해놨음.
  */
 export default function Page() {
-  const {control, register, handleSubmit, watch, setValue, formState: {errors}} = useForm<TestFormData>({
+  const {control, register, handleSubmit, watch, setValue, setError, formState: {errors}} = useForm<TestFormData>({
     defaultValues: {
       selectedStep: 'step2',
       step1: [
         {
-          title: '',
-          content: ''
+          title: '111',
+          content: '222'
         },
         {
           title: '222',
@@ -62,9 +63,28 @@ export default function Page() {
 
   const selectedStep = watch('selectedStep');
 
+  const {mutateAsync, isPending} = useMutation({
+    mutationFn: updateApi
+  });
   const onSubmit: SubmitHandler<TestFormData> = useCallback(async (data) => {
     console.log('data', data);
-  }, []);
+
+    try {
+      await mutateAsync(data);
+    } catch (error) {
+      const {stepType, index, dataType} = error as ErrorResponse;
+      setValue('selectedStep', stepType);
+
+      setTimeout(() => {
+        setError(`${stepType}.${index}.${dataType}`, {
+          type: 'api',
+          message: `${stepType} ${dataType}을 이렇게 적으면 안되요`
+        }, {
+          shouldFocus: true
+        });
+      });
+    }
+  }, [mutateAsync, setError, setValue]);
 
   const onError: SubmitErrorHandler<TestFormData> = useCallback(({step1}) => {
     if (step1) {
@@ -95,9 +115,27 @@ export default function Page() {
           <Button type="button" onClick={() => step2.append({title: '', content: ''})}>Step2 추가</Button>
         )}
       </form>
-      <Button isSubmit onClick={handleSubmit(onSubmit, onError)}>제출</Button>
+      <Button isSubmit loading={isPending} onClick={handleSubmit(onSubmit, onError)}>제출</Button>
     </>
   );
+}
+
+interface ErrorResponse {
+  stepType: StepType;
+  dataType: 'title' | 'content';
+  index: number;
+}
+
+async function updateApi(data: TestFormData) {
+  await timeoutPromise(1000);
+
+  const response: ErrorResponse = {
+    stepType: 'step1',
+    dataType: 'title',
+    index: 0
+  };
+
+  return Promise.reject(response);
 }
 
 interface SubFormProps {
