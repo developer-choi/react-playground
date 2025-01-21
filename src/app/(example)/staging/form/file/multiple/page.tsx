20bd8d9f5725babbd38ef5bb4706fa82e6cfac55
Yu Jin Choe <forworkchoe@gmail.com>
2025-01-21 21:59:46 +0900

    예제 보완

--- src/app/(example)/staging/form/file/multiple/page.tsx
+++ src/app/(example)/staging/form/file/multiple/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import {fileSizeToByte} from '@/utils/extend/file/file-size';
-import React from 'react';
+import React, {useCallback} from 'react';
 import InputFile from '@/components/form/Input/inputFile';
 import Image from 'next/image';
 import Link from 'next/link';
 import {useMultipleImageFile} from '@/utils/extend/file/handle-image/state';
+import Button from '@/components/element/Button';
+import designSystemStyles from '@/styles/design-system.module.scss';
+import classNames from 'classnames';
 
 // URL: http://localhost:3000/staging/form/file/multiple
 // Doc: https://docs.google.com/document/d/1_9-Bw4SihS6DGpskF8Xor_gaahHetDfKDk6QJeZQ6Ig/edit?tab=t.0
@@ -19,13 +22,19 @@ export default function Page() {
     }
   });
 
+  const onSubmit = useCallback(() => {
+    console.log('list', list);
+  }, [list]);
+
   return (
     <>
       <div>
         <Link href="/">Unmount 테스트용 다른 페이지 가는 링크</Link>
       </div>
-      <InputFile style={{display: 'inline-block', width: 100, height: 100, border: '3px solid black'}} {...inputFileProps}>파일등록</InputFile>
-      <button style={{background: 'red', width: 100, height: 30}}>제출</button>
+      <div className={classNames(designSystemStyles.commonSubForm, 'row')}>
+        <InputFile style={{display: 'inline-block', width: 100, height: 100, border: '3px solid black'}} {...inputFileProps}>파일등록</InputFile>
+        <Button onClick={onSubmit}>제출</Button>
+      </div>
 
       {list.map((preview) => (
         <div key={preview.id} style={{position: 'relative', display: 'inline-block'}}>
