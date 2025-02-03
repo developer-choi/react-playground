b21a01b49eec3ec4d604eee87b53effd312da67d
Yu Jin Choe <forworkchoe@gmail.com>
2025-02-03 15:08:24 +0900

    prefetching 기본값을 Nextjs 공식문서에 맞게 수정하고, onMouseEnter 동작을 prefetch값이 지정되지 않았을 때에만 실행하도록 수정

--- src/components/element/link/CustomLink.tsx
+++ src/components/element/link/CustomLink.tsx
@@ -11,15 +11,21 @@ export interface CustomLinkProps extends PropsWithChildren<LinkProps> {
 
 /**
  * Doc: https://docs.google.com/document/d/1YHHOxCfMh_sxAw3AZQrsxR-2iyUH_R4A7qDGXEloQPc/edit?tab=t.0
+ *
+ * 1. 대부분의 경우 (prefetch = undefined) 마우스 오버하면 강제로 프리패칭됨. (SSR 로 만든 페이지라 할지라도)
+ * 2. prefetch = false가 유용한 경우는 리스트로 수십개의 목록을 노출 시키고 그 목록 각각이 링크로 된 경우 (ex: 게시글 리스트 페이지) 내가 원하는 게시글을 마우스로 클릭하려고 마우스 쭉 움직일 때 onMouseEnter()가 쭉 실행되면 매우 곤란해짐
  */
-export default function CustomLink({prefetch = false, href, ...rest}: CustomLinkProps) {
+export default function CustomLink({prefetch, href, ...rest}: CustomLinkProps) {
   const router = useRouter();
+  const shouldEnablePrefetch = prefetch === undefined && typeof href === 'string';
 
   const onMouseEnter = useCallback(() => {
-    router.prefetch(href as string);
+    if (typeof href === 'string') {
+      router.prefetch(href);
+    }
   }, [href, router]);
   
   return (
-    <Link prefetch={prefetch} onMouseEnter={onMouseEnter} href={href} {...rest}/>
+    <Link prefetch={prefetch} onMouseEnter={shouldEnablePrefetch ? onMouseEnter : undefined} href={href} {...rest}/>
   );
 }
