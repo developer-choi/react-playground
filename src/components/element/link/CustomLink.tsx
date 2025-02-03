541415529b8ad7e750acc624802b41e1a7960aaa
Yu Jin Choe <forworkchoe@gmail.com>
2025-02-03 12:45:38 +0900

    CustomLink에 마우스 오버 시점에 prefetching 하는 기능 추가

--- src/components/element/link/CustomLink.tsx
+++ src/components/element/link/CustomLink.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link, {LinkProps} from 'next/link';
-import React, {CSSProperties, PropsWithChildren} from 'react';
+import React, {CSSProperties, PropsWithChildren, useCallback} from 'react';
+import {useRouter} from 'next/navigation';
 
 export interface CustomLinkProps extends PropsWithChildren<LinkProps> {
   style?: CSSProperties;
@@ -7,10 +10,16 @@ export interface CustomLinkProps extends PropsWithChildren<LinkProps> {
 }
 
 /**
- * Original : https://github.com/developer-choi/react-playground/commit/80891494f84e1c93c42fb01428d1dbee98133601#diff-c5022c9c2a2b3525d5b37b200fb8fea5c4ea7b1775ac34558c227a5267fdd99f
+ * Doc: https://docs.google.com/document/d/1YHHOxCfMh_sxAw3AZQrsxR-2iyUH_R4A7qDGXEloQPc/edit?tab=t.0
  */
-export default function CustomLink({prefetch = false, ...rest}: CustomLinkProps) {
+export default function CustomLink({prefetch = false, href, ...rest}: CustomLinkProps) {
+  const router = useRouter();
+
+  const onMouseEnter = useCallback(() => {
+    router.prefetch(href as string);
+  }, [href, router]);
+  
   return (
-    <Link prefetch={prefetch} {...rest}/>
+    <Link prefetch={prefetch} onMouseEnter={onMouseEnter} href={href} {...rest}/>
   );
 }
