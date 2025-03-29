417165c454de65db183b464da783addf8ad55238
Yu Jin Choe <forworkchoe@gmail.com>
2025-03-30 01:28:48 +0900

    랜덤함수 추가

--- src/utils/extend/test/random.ts
+++ src/utils/extend/test/random.ts
@@ -11,8 +11,36 @@ export function randomNumber(from: number, to: number) {
   return Math.floor(Math.random() * length) + _min;
 }
 
-export function randomBoolean(percent: number = 50): boolean {
-  return Math.random() > (100 - percent) / 100;
+export function randomBoolean(percent = 50): boolean {
+  return Math.random() < percent / 100;
+}
+
+/**
+ * 성공할 때 까지 전달된 횟수만큼 시도해서 성공여부를 반환
+ */
+export function tryRandomUntilSuccess(percent = 50, count: number): boolean {
+  for (let i = 0; i < count; i++) {
+    if (randomBoolean(percent)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/**
+ * 전달된 확률로 성공하는데 몇회를 시도했는지 반환
+ */
+export function measureTriesToSuccess(percent = 50): number {
+  let count = 0;
+
+  while (true) {
+    count++;
+
+    if (randomBoolean(percent)) {
+      return count;
+    }
+  }
 }
 
 export function randomInArray<T>(array: T[], count = 1): T[] {
