9eb734efe53f07475aee19499d1bb48996f0e9d4
Yu Jin Choe <forworkchoe@gmail.com>
2025-05-15 15:16:54 +0900

    Revert "fetch()의 .json()이랑 .text() 강제 테스트"
    This reverts commit 9a6367ada73ba42aacd1236b629d8612ae281af7.

--- src/app/(service)/(public)/page.tsx
+++ src/app/(service)/(public)/page.tsx
@@ -1,21 +1,3 @@
-'use client';
-
-import {useCallback} from 'react';
-import {customFetchOnClientSide} from '@/utils/extend/library/fetch/client';
-import Button from '@/components/element/Button';
-
 export default function Home() {
-  const onClick = useCallback(async () => {
-    await customFetchOnClientSide('/api/test/json', {
-      method: 'GET',
-      authorize: 'none',
-      query: {
-        type: 'json-number'
-      }
-    });
-  }, []);
-
-  return (
-    <Button onClick={onClick}>Click me</Button>
-  );
+  return null;
 }
