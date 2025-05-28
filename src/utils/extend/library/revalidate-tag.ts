d01beb5640948f5bbcd0968b134a41ffa4e63a65
Yu Jin Choe <forworkchoe@gmail.com>
2025-05-28 17:08:50 +0900

    revalidatePath() 추가

--- src/utils/extend/library/revalidate-tag.ts
+++ src/utils/extend/library/revalidate-tag.ts
@@ -1,8 +1,13 @@
 'use server';
 
-import {revalidateTag} from 'next/cache';
+import {revalidatePath, revalidateTag} from 'next/cache';
 import {RevalidateTagType} from '@/utils/extend/library/fetch';
 
 export async function revalidateTagOnServerAction(tag: RevalidateTagType) {
   revalidateTag(tag);
+}
+
+// https://docs.google.com/document/d/13lDDYK9_phJENJNSVGovS3JfqDDpJpw3mfw8nnLBpEQ/edit?usp=drivesdk
+export async function revalidatePathOnServerAction(path: string) {
+  revalidatePath(path);
 }
