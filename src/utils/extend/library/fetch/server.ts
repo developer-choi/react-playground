feea7528e9bc50b1ef58692c22611a7bd20a1f3c
Yu Jin Choe <forworkchoe@gmail.com>
2025-05-30 13:50:01 +0900

    Server Side에서 fetch하다 401 에러났을 때 처리가 안되던 오류 수정

--- src/utils/extend/library/fetch/server.ts
+++ src/utils/extend/library/fetch/server.ts
@@ -1,22 +1,8 @@
 import 'server-only';
-
-import {LoginError} from '@/utils/service/error';
-import {auth, CURRENT_URL_IN_HEADER} from '@/utils/service/auth';
-import {redirect} from 'next/navigation';
+import {auth} from '@/utils/service/auth';
 import {customFetch, ExtendedCustomFetchParameter} from '@/utils/extend/library/fetch/index';
-import {headers} from 'next/headers';
 
 export async function customFetchOnServerSide<D>(input: string | URL | globalThis.Request, parameter: ExtendedCustomFetchParameter) {
-  try {
-    const session = parameter.authPolicy === 'none' ? null : await auth();
-    return customFetch<D>(input, {...parameter, session});
-  } catch (error) {
-    if (error instanceof LoginError) {
-      const currentUrl = headers().get(CURRENT_URL_IN_HEADER) ?? '/'; // middleware에서 셋팅
-      redirect(`/api/next-auth/logout?redirect=${currentUrl}`);
-
-    } else {
-      throw error;
-    }
-  }
+  const session = parameter.authPolicy === 'none' ? null : await auth();
+  return await customFetch<D>(input, {...parameter, session});
 }
--- src/utils/service/error/server.tsx
+++ src/utils/service/error/server.tsx
@@ -1,11 +1,12 @@
-import {CustomizedError, FetchError} from '@/utils/service/error/index';
-import {notFound} from 'next/navigation';
+import {CustomizedError, FetchError, LoginError} from '@/utils/service/error/index';
+import {notFound, redirect} from 'next/navigation';
 import {PageServerComponentProps} from '@/types/declaration/next';
 import {validateComputableNumber} from '@/utils/extend/browser/query-string/validate';
 import * as Sentry from '@sentry/nextjs';
 import {getErrorInfo} from '@/utils/service/error/info';
 import ErrorPageTemplate from '@/components/error/ErrorPageTemplate';
 import {isObject} from '@/utils/extend/data-type/object';
+import {headers} from 'next/headers';
 
 // Doc: [Can not catch an ServerSideError on client] https://docs.google.com/document/d/1UmDWmmGTNH_XNupQ-tegMnQwzb-m5yD2Hz_NzO2glic/edit?tab=t.0
 export function handleServerSideError(error: unknown) {
@@ -13,6 +14,17 @@ export function handleServerSideError(error: unknown) {
 
   if (!isObject(error)) {
     throw error;
+  }
+
+  /**
+   * catch 로직은 가급적 실행하는 곳과 가까운 곳에서 처리하는것이 좋기 때문에...
+   * 원래는 customFetchOnServerSide()에서 처리를 하고싶었으나,
+   * redirect()는 try문 안에서 실행할 수 없다보니 부득이하게 여기서 처리
+   * + 대신, customFetchOnClientSide() 에서 발생한 에러는 useHandleClientSideError()가 아닌 customFetchOnClientSide() 에서 처리했음.
+   */
+  if (error instanceof LoginError) {
+    const currentUrl = headers().get('current-pathname-with-search') ?? '/'; // middleware에서 셋팅
+    redirect(`/api/next-auth/logout?redirect=${currentUrl}`);
   }
 
   const { title, content } = getErrorInfo(error);
