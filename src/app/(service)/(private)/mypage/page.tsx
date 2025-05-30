f22389478c5f4dfe6c38efc3ccc47746e8657c9d
Yu Jin Choe <forworkchoe@gmail.com>
2025-05-30 14:06:49 +0900

    Server Side에서 401에러가 동시에 여러번 발생했을 때에도 문제없이 동작하는지 테스트하기 위한 예제 페이지 추가

--- src/app/(service)/(private)/mypage/page.tsx
+++ src/app/(service)/(private)/mypage/page.tsx
@@ -1,6 +1,38 @@
-// URL: http://localhost:3000/mypage
+import {timeoutPromise} from '@/utils/extend/test/promise';
+import {LoginError} from '@/utils/service/error';
+import {Suspense} from 'react';
+import {handleServerSideError} from '@/utils/service/error/server';
+
+/**
+ * URL: http://localhost:3000/mypage?hello=world
+ * Server Side에서 401에러가 동시에 여러번 발생했을 때에도 문제없이 동작하는지 테스트하기 위한 예제 페이지 
+ */
 export default function Page() {
   return (
-    <h1>My Page</h1>
+    <div>
+      <Suspense fallback="Loading...">
+        <Test/>
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <Test/>
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <Test/>
+      </Suspense>
+    </div>
   );
+}
+
+async function Test() {
+  try {
+    await timeoutPromise(1000);
+
+    if (2 > 1) {
+      throw new LoginError('Login error occurred');
+    }
+
+    return null;
+  } catch (error) {
+    return handleServerSideError(error);
+  }
 }
