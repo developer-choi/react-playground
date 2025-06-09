e4e6da5fde75c8bfdeefb761ae30d82f1e62e841
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-09 09:47:00 +0900

    beforeSend 타입 강제 추가

--- src/utils/extend/library/sentry.ts
+++ src/utils/extend/library/sentry.ts
@@ -1,7 +1,7 @@
-import type {ErrorEvent, EventHint} from '@sentry/types';
+import type {ErrorEvent, EventHint, ClientOptions} from '@sentry/types';
 import {CustomizedError} from '@/utils/service/error/class';
 
-export function beforeSend(event: ErrorEvent, hint: EventHint) {
+export const beforeSend: ClientOptions['beforeSend'] = (event: ErrorEvent, hint: EventHint) => {
   // nextjs에서 server side에서 redirect() 하면 내부적으로 이 에러를 던져서 처리하도록 되어있는데 문제는 그게 Sentry까지 날아간다는 것이었음. 이거말고 다른 해결책을 못찾음.
   const isNextRedirectError = hint.originalException && typeof hint.originalException === 'object' && 'digest' in hint.originalException && typeof hint.originalException.digest === 'string' && hint.originalException.digest.includes('NEXT_REDIRECT');
 
