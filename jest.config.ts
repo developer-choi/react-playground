9b999964ee45f9914f9aef602ac7dd849f3c5b1b
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-20 17:18:17 +0900

    jest 돌릴 때 과정 더 자세히 나오게 설정 변경

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
+  verbose: true
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
