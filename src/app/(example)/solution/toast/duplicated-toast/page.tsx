a8fc7705470756b5684d4afa86d8c73bbf4ce8c7
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-26 10:58:27 +0900

    중복 토스트 방지 예제 페이지를 solution 하위로 이동

--- src/app/(example)/solution/toast/duplicated-toast/page.tsx
+++ src/app/(example)/solution/toast/duplicated-toast/page.tsx
@@ -7,7 +7,7 @@ import {snackBarProfile} from '@/components/toast/SnackBarProfile';
 
 /**
  * 목적 > 광클 해도 토스트가 최대 1개만 뜰 수 있도록 하는 방법을 구현했음.
- * URL: http://localhost:3000/experimental/duplicated-toast
+ * URL: http://localhost:3000/solution/toast/duplicated-toast
  * Doc: https://docs.google.com/document/d/1Z78cJFrCNLm1rFeMIA7y64o6E84yJ4H_hNCVCdjztCM/edit#heading=h.4h61jm8200i1
  */
 export default function Page() {
