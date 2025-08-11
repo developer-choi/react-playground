fc86085cc65edc33d016c85a050992984efcd66a
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-11 10:54:52 +0900

    주석 추가

--- src/utils/service/common/inputs/index.ts
+++ src/utils/service/common/inputs/index.ts
@@ -1,6 +1,7 @@
 import {FieldPath, FieldValues, RegisterOptions, UseFormReturn} from 'react-hook-form';
 import {InputProps} from '@/components/form/Input';
 
+// Doc: https://docs.google.com/document/d/1vfCUikBQ-iRWqJt8-EmIhv2Gvl2hI9zzyAs-XsOpmSo/edit?tab=t.0
 export interface FormInputParam<T extends FieldValues> {
   form: {
     methods: UseFormReturn<T>;
