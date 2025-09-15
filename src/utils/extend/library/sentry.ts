be53ef122e1a5062bf3a56ca5a64b9a1c5fc62ed
Yu Jin Choe <forworkchoe@gmail.com>
2025-09-15 15:29:06 +0900

    Sentry로 보내는걸 스킵하는 코드를 별도로 분리

--- src/utils/extend/library/sentry.ts
+++ src/utils/extend/library/sentry.ts
@@ -2,10 +2,7 @@ import type {ClientOptions, ErrorEvent, EventHint} from '@sentry/types';
 import {BaseError} from '@forworkchoe/core/utils';
 
 export const beforeSend: ClientOptions['beforeSend'] = (event: ErrorEvent, hint: EventHint) => {
-  // nextjs에서 server side에서 redirect() 하면 내부적으로 이 에러를 던져서 처리하도록 되어있는데 문제는 그게 Sentry까지 날아간다는 것이었음. 이거말고 다른 해결책을 못찾음.
-  const isNextRedirectError = hint.originalException && typeof hint.originalException === 'object' && 'digest' in hint.originalException && typeof hint.originalException.digest === 'string' && hint.originalException.digest.includes('NEXT_REDIRECT');
-
-  if (isNextRedirectError) {
+  if (isSkipSentry(hint)) {
     return null;
   }
 
@@ -36,3 +33,28 @@ export const beforeSend: ClientOptions['beforeSend'] = (event: ErrorEvent, hint:
 
   return event;
 }
+
+function isSkipSentry(hint: EventHint): boolean {
+  // nextjs에서 server side에서 redirect() 하면 내부적으로 이 에러를 던져서 처리하도록 되어있는데 문제는 그게 Sentry까지 날아간다는 것이었음. 이거말고 다른 해결책을 못찾음.
+  const isNextRedirectError = hint.originalException && typeof hint.originalException === 'object' && 'digest' in hint.originalException && typeof hint.originalException.digest === 'string' && hint.originalException.digest.includes('NEXT_REDIRECT');
+
+  if (isNextRedirectError) {
+    return true;
+  }
+
+  return SKIP_ERRORS.some(
+    (error) =>
+      hint.originalException instanceof error.class &&
+      hint.originalException.message === error.message &&
+      (hint.originalException.stack && error.stackKeyword && hint.originalException.stack.includes(error.stackKeyword)),
+  );
+}
+
+// 아무리 오류가 많이 발생하더라도 대응하지 않을 오류는 Sentry로 보내지 않기 위함
+const SKIP_ERRORS = [
+  {
+    class: ReferenceError,
+    message: `Can't find variable: some_variable_name`,
+    stackKeyword: '/external/some.js',
+  },
+];
